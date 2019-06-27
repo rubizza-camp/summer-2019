410c7a40e0bc951c75d314af7d2383d6486fe9af
Anatoli Liotych <anatoli.liotych@gmail.com>
2019-06-27 15:34:02 +0300

    Squash commits!!!
    Initial commit
    
    Create README.md
    
    corrected
    
     fixed an exception.
    
    Update about_sandwich_code.rb
    
    Update about_sandwich_code.rb
    
    Update about_sandwich_code.rb
    
    Update about_sandwich_code.rb
    
    Update about_sandwich_code.rb
    
    Update about_sandwich_code.rb
    
    Update about_sandwich_code.rb
    
    Update about_sandwich_code.rb
    
    Update triangle.rb
    
    Update triangle.rb
    
    Update about_constants.rb
    
    Update about_constants.rb
    
    Update neo.rb
    
    Update neo.rb
    
    Update about_sandwich_code.rb
    
    Update about_sandwich_code.rb
    
    Delete neo.rb
    
    Create neo.rb
    
    Delete about_sandwich_code.rb
    
    Create about_sandwich_code.rb
    
    Update about_symbols.rb
    
    Update about_blocks.rb
    
    Update about_constants.rb
    
    Update about_constants.rb
    
    Update about_dice_project.rb

---
+++ 3460/0/triangle.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+# rubocop:disable CyclomaticComplexity
+# rubocop:disable PerceivedComplexity
+#
+def triangle(side_a, side_b, side_c)
+  raise TriangleError unless side_a.positive?
+  raise TriangleError unless side_a + side_b > side_c
+  return :equilateral if side_a == side_b && side_b == side_c
+  return :isosceles if side_a == side_b || side_b == side_c || side_a == side_c
+
+  :scalene
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
+# rubocop:enable CyclomaticComplexity
+# rubocop:enable PerceivedComplexity
