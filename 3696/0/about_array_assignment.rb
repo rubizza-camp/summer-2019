0263c109617cdb586a9070035e7df259baca264b
Your Name <you@example.com>
2019-07-05 23:16:15 +0300

    Rubocop autocorrect

--- 3696/0/about_array_assignment.rb
+++ 3696/0/about_array_assignment.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutArrayAssignment < Neo::Koan
   def test_non_parallel_assignment
-    names = ["John", "Smith"]
+    names = ['John', 'Smith']
     assert_equal ["John", "Smith"], names
   end
 
