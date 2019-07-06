1f69d509e77a452dbde262a5a33c22bac50304f4
Demeugaliyev <m.demeugaliev@gmail.com>
2019-07-07 01:14:14 +0600

    step 18

--- 3751/0/about_arrays.rb
+++ 3751/0/about_arrays.rb
@@ -24,12 +24,12 @@ class AboutArrays < Neo::Koan
   def test_accessing_array_elements
     array = [:peanut, :butter, :and, :jelly]
 
-    assert_equal __, array[0]
-    assert_equal __, array.first
-    assert_equal __, array[3]
-    assert_equal __, array.last
-    assert_equal __, array[-1]
-    assert_equal __, array[-3]
+    assert_equal :peanut, array[0]
+    assert_equal :peanut, array.first
+    assert_equal :jelly, array[3]
+    assert_equal :jelly, array.last
+    assert_equal :jelly, array[-1]
+    assert_equal :butter, array[-3]
   end
 
   def test_slicing_arrays
