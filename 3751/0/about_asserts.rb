73b5ace14564477ed2bfdc1b6f1880bf5fce9d34
Demeugaliyev <m.demeugaliev@gmail.com>
2019-07-06 23:27:04 +0600

    step 1

--- 3751/0/about_asserts.rb
+++ 3751/0/about_asserts.rb
@@ -7,7 +7,7 @@ class AboutAsserts < Neo::Koan
 
   # We shall contemplate truth by testing reality, via asserts.
   def test_assert_truth
-    assert false                # This should be true
+    assert true                # This should be true
   end
 
   # Enlightenment may be more easily achieved with appropriate
