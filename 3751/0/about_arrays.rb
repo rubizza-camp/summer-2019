b9957562042a444c764ce3f0dcbe61cb98b07043
Demeugaliyev <m.demeugaliev@gmail.com>
2019-07-07 01:09:50 +0600

    step 17

--- 3751/0/about_arrays.rb
+++ 3751/0/about_arrays.rb
@@ -15,10 +15,10 @@ class AboutArrays < Neo::Koan
     assert_equal [1], array
 
     array[1] = 2
-    assert_equal [1, __], array
+    assert_equal [1, 2], array
 
     array << 333
-    assert_equal __, array
+    assert_equal [1, 2, 333], array
   end
 
   def test_accessing_array_elements
