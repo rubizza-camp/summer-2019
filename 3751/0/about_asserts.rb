0800cedcdb662d04b4a2579eac3f1a7b985d4aff
Demeugaliyev <m.demeugaliev@gmail.com>
2019-07-06 23:31:02 +0600

    step 4

--- 3751/0/about_asserts.rb
+++ 3751/0/about_asserts.rb
@@ -27,7 +27,7 @@ class AboutAsserts < Neo::Koan
 
   # Some ways of asserting equality are better than others.
   def test_a_better_way_of_asserting_equality
-    expected_value = __
+    expected_value = 2
     actual_value = 1 + 1
 
     assert_equal expected_value, actual_value
