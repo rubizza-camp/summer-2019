bb4c7bae9db253b039c6133190a0cbdf38e234f8
Demeugaliyev <m.demeugaliev@gmail.com>
2019-07-06 23:54:46 +0600

    step 8

--- 3751/0/about_nil.rb
+++ 3751/0/about_nil.rb
@@ -22,9 +22,9 @@ class AboutNil < Neo::Koan
   end
 
   def test_nil_has_a_few_methods_defined_on_it
-    assert_equal __, nil.nil?
-    assert_equal __, nil.to_s
-    assert_equal __, nil.inspect
+    assert_equal true, nil.nil?
+    assert_equal "".to_s, nil.to_s
+    assert_equal "nil", nil.inspect
 
     # THINK ABOUT IT:
     #
