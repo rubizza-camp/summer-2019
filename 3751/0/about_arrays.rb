e11641f6bf72e741463e3ed02d76cb256a667917
Demeugaliyev <m.demeugaliev@gmail.com>
2019-07-07 01:02:12 +0600

    step 16

--- 3751/0/about_arrays.rb
+++ 3751/0/about_arrays.rb
@@ -3,8 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutArrays < Neo::Koan
   def test_creating_arrays
     empty_array = Array.new
-    assert_equal __, empty_array.class
-    assert_equal __, empty_array.size
+    assert_equal Array, empty_array.class
+    assert_equal 0, empty_array.size
   end
 
   def test_array_literals
