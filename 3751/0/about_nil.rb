8ffa8c6e778a61129fc93a4e34775d24db7217e2
Demeugaliyev <m.demeugaliev@gmail.com>
2019-07-06 23:39:42 +0600

    step 6

--- 3751/0/about_nil.rb
+++ 3751/0/about_nil.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutNil < Neo::Koan
   def test_nil_is_an_object
-    assert_equal __, nil.is_a?(Object), "Unlike NULL in other languages"
+    assert_equal true, nil.is_a?(Object), "Unlike NULL in other languages"
   end
 
   def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
