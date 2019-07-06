d4671468f552111df635b05e7922165a8399ab30
Demeugaliyev <m.demeugaliev@gmail.com>
2019-07-07 01:30:21 +0600

    step 19

--- 3751/0/about_arrays.rb
+++ 3751/0/about_arrays.rb
@@ -35,13 +35,13 @@ class AboutArrays < Neo::Koan
   def test_slicing_arrays
     array = [:peanut, :butter, :and, :jelly]
 
-    assert_equal __, array[0,1]
-    assert_equal __, array[0,2]
-    assert_equal __, array[2,2]
-    assert_equal __, array[2,20]
-    assert_equal __, array[4,0]
-    assert_equal __, array[4,100]
-    assert_equal __, array[5,0]
+    assert_equal [:peanut], array[0,1]
+    assert_equal [:peanut, :butter], array[0,2]
+    assert_equal [:and, :jelly], array[2,2]
+    assert_equal [:and, :jelly], array[2,20]
+    assert_equal [], array[4,0]
+    assert_equal [], array[4,100]
+    assert_equal nil, array[5,0]
   end
 
   def test_arrays_and_ranges
