f279b81ad628450cb2d6688396b3fe411e13657a
Pavel Zhachko <rammiel@yandex.ru>
2019-07-07 20:23:47 +0300

    about_dice_project.rb done

--- 3520/0/about_dice_project.rb
+++ 3520/0/about_dice_project.rb
@@ -2,9 +2,14 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_reader :values
+
+  def roll(set_size)
+    @values = []
+    set_size.times { @values.push rand(1..6) }
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
