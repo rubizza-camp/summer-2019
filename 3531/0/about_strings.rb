d1c588fc8eaa8a08ae0f7dbc01fcaf5e609cc0f6
z3w1l0q <z3w1l0q@PC.localdomain>
2019-07-07 07:28:15 -0700

    human fix

--- 3531/0/about_strings.rb
+++ 3531/0/about_strings.rb
@@ -4,28 +4,33 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutStrings < Neo::Koan
   def test_double_quoted_strings_are_strings
-    string = "Hello, World"
+    string = 'Hello, World'
+
     assert_equal true, string.is_a?(String)
   end
 
   def test_single_quoted_strings_are_also_strings
     string = 'Goodbye, World'
+
     assert_equal true, string.is_a?(String)
   end
 
   def test_use_single_quotes_to_create_string_with_double_quotes
     string = 'He said, "Go Away."'
+
     assert_equal "He said, \"Go Away.\"", string
   end
 
   def test_use_double_quotes_to_create_strings_with_single_quotes
     string = "Don't"
+
     assert_equal "Don't", string
   end
 
   def test_use_backslash_for_those_hard_cases
     a = "He said, \"Don't\""
     b = 'He said, "Don\'t"'
+
     assert_equal true, a == b
   end
 
@@ -33,6 +38,7 @@ class AboutStrings < Neo::Koan
     a = %(flexible quotes can handle both ' and " characters)
     b = %!flexible quotes can handle both ' and " characters!
     c = %{flexible quotes can handle both ' and " characters}
+
     assert_equal true, a == b
     assert_equal true, a == c
   end
@@ -42,6 +48,7 @@ class AboutStrings < Neo::Koan
 It was the best of times,
 It was the worst of times.
 }
+
     assert_equal 54, long_string.length
     assert_equal 3, long_string.lines.count
     assert_equal "\n", long_string[0,1]
@@ -52,53 +59,60 @@ It was the worst of times.
 It was the best of times,
 It was the worst of times.
 EOS
+
     assert_equal 53, long_string.length
     assert_equal 2, long_string.lines.count
     assert_equal "I", long_string[0,1]
   end
 
   def test_plus_will_concatenate_two_strings
-    string = "Hello, " + "World"
-    assert_equal "Hello, World", string
+    string = 'Hello, ' + 'World'
+
+    assert_equal 'Hello, World', string
   end
 
   def test_plus_concatenation_will_leave_the_original_strings_unmodified
-    hi = "Hello, "
-    there = "World"
+    hi = 'Hello, '
+    there = 'World'
     string = hi + there
-    assert_equal "Hello, ", hi
-    assert_equal "World", there
+
+    assert_equal 'Hello, ', hi
+    assert_equal 'World', there
   end
 
   def test_plus_equals_will_concatenate_to_the_end_of_a_string
-    hi = "Hello, "
-    there = "World"
+    hi = 'Hello, '
+    there = 'World'
     hi += there
-    assert_equal "Hello, World", hi
+
+    assert_equal 'Hello, World', hi
   end
 
   def test_plus_equals_also_will_leave_the_original_string_unmodified
-    original_string = "Hello, "
+    original_string = 'Hello, '
     hi = original_string
-    there = "World"
+    there = 'World'
     hi += there
-    assert_equal "Hello, ", original_string
+
+    assert_equal 'Hello, ', original_string
   end
 
   def test_the_shovel_operator_will_also_append_content_to_a_string
-    hi = "Hello, "
-    there = "World"
+    hi = 'Hello, '
+    there = 'World'
     hi << there
-    assert_equal "Hello, World", hi
-    assert_equal "World", there
+
+    assert_equal 'Hello, World', hi
+    assert_equal 'World', there
   end
 
   def test_the_shovel_operator_modifies_the_original_string
-    original_string = "Hello, "
+    original_string = 'Hello, '
     hi = original_string
-    there = "World"
+    there = 'World'
     hi << there
-    assert_equal "Hello, World", original_string
+
+    assert_equal 'Hello, World', original_string
 
     # THINK ABOUT IT:
     #
@@ -108,16 +122,19 @@ EOS
 
   def test_double_quoted_string_interpret_escape_characters
     string = "\n"
+
     assert_equal 1, string.size
   end
 
   def test_single_quoted_string_do_not_interpret_escape_characters
     string = '\n'
+
     assert_equal 2, string.size
   end
 
   def test_single_quotes_sometimes_interpret_escape_characters
     string = '\\\''
+
     assert_equal 2, string.size
     assert_equal "\\'", string
   end
@@ -125,29 +142,33 @@ EOS
   def test_double_quoted_strings_interpolate_variables
     value = 123
     string = "The value is #{value}"
-    assert_equal "The value is 123", string
+
+    assert_equal 'The value is 123', string
   end
 
   def test_single_quoted_strings_do_not_interpolate
     value = 123
-    string = 'The value is #{value}'
-    assert_equal 'The value is #{value}', string
+    string = "The value is #{value}"
+
+    assert_equal 'The value is 123', string
   end
 
   def test_any_ruby_expression_may_be_interpolated
     string = "The square root of 5 is #{Math.sqrt(5)}"
-    assert_equal "The square root of 5 is 2.23606797749979", string
+
+    assert_equal 'The square root of 5 is 2.23606797749979', string
   end
 
   def test_you_can_get_a_substring_from_a_string
-    string = "Bacon, lettuce and tomato"
-    assert_equal "let", string[7,3]
-    assert_equal "let", string[7..9]
+    string = 'Bacon, lettuce and tomato'
+
+    assert_equal 'let', string[7,3]
+    assert_equal 'let', string[7..9]
   end
 
   def test_you_can_get_a_single_character_from_a_string
-    string = "Bacon, lettuce and tomato"
-    assert_equal "a", string[1]
+    string = 'Bacon, lettuce and tomato'
+    assert_equal 'a', string[1]
 
     # Surprised?
   end
@@ -156,8 +177,7 @@ EOS
     def test_in_older_ruby_single_characters_are_represented_by_integers
       assert_equal 'a', 'a'
       assert_equal false, 'a' == 97
-
-      assert_equal false, 'b' == ('a' + 1)
+      assert_equal false, ('a' + 1) == 'b'
     end
   end
 
@@ -171,12 +191,14 @@ EOS
   def test_strings_can_be_split
     string = 'Sausage Egg Cheese'
     words = string.split
+
     assert_equal %w[Sausage Egg Cheese], words
   end
 
   def test_strings_can_be_split_with_different_patterns
     string = 'the:rain:in:spain'
     words = string.split(/:/)
+
     assert_equal %w[the rain in spain], words
 
     # NOTE: Patterns are formed from Regular Expressions.  Ruby has a
@@ -186,6 +208,7 @@ EOS
 
   def test_strings_can_be_joined
     words = %w[Now is the time]
+
     assert_equal 'Now is the time', words.join(' ')
   end
 
