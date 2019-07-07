308ddac72dd8829fa9d03f58392577250eb0f7ea
Alex Samets <aliaksandr.samets@gmail.com>
2019-07-08 02:35:50 +0300

    Update about_keyword_arguments.rb

--- 3600/0/about_keyword_arguments.rb
+++ 3600/0/about_keyword_arguments.rb
@@ -8,9 +8,9 @@ class AboutKeywordArguments < Neo::Koan
 
   def test_keyword_arguments
     assert_equal Array, method_with_keyword_arguments.class
-    assert_equal [1, "two"], method_with_keyword_arguments
-    assert_equal ["one", "two"], method_with_keyword_arguments(one: 'one')
-    assert_equal [1,2], method_with_keyword_arguments(two: 2)
+    assert_equal [1, 'two'], method_with_keyword_arguments
+    assert_equal %w(one two), method_with_keyword_arguments(one: 'one')
+    assert_equal [1, 2], method_with_keyword_arguments(two: 2)
   end
 
   def method_with_keyword_arguments_with_mandatory_argument(one, two: 2, three: 3)
@@ -21,11 +21,10 @@ class AboutKeywordArguments < Neo::Koan
     exception = assert_raise (ArgumentError) do
       method_with_keyword_arguments_with_mandatory_argument
     end
-    assert_match(/wrong number of arguments/, "wrong number of arguments (given 0, expected 1)")
+    assert_match(/wrong number of arguments/, 'wrong number of arguments (given 0, expected 1)')
   end
 
   # THINK ABOUT IT:
-  #
   # Keyword arguments always have a default value, making them optional to the caller
 
 end
