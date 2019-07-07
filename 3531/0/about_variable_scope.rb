54f8eeb0d4919ffe2fb8646820eb869f7a9d0467
z3w1l0q <z3w1l0q@PC.localdomain>
2019-07-07 07:41:02 -0700

    delete unnecessary empty lines and fix Style/StringLiterals symbols triangle var_scope

--- 3531/0/about_variable_scope.rb
+++ 3531/0/about_variable_scope.rb
@@ -4,17 +4,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutVariableScope < Neo::Koan
-
   def bark
     noise = 'RUFF'
   end
 
   def test_noise_is_not_available_in_the_current_scope
-
     assert_raise(NameError) do
       noise
     end
-
   end
 
   def test_we_can_get_noise_by_calling_method
@@ -26,13 +23,11 @@ class AboutVariableScope < Neo::Koan
     # defined? does not return true or false
     assert_equal nil, defined? inaccesible
   end
-
   # ------------------------------------------------------
-
   def test_blocks_can_access_variables_outside_scope 
-    test = "Hi"
+    test = 'Hi'
     2.times do
-      test = "Hey"
+      test = 'Hey'
     end
 
     assert_equal 'Hey', test    
@@ -44,31 +39,25 @@ class AboutVariableScope < Neo::Koan
     end
     assert_equal nil, defined? x
   end
-
   # ------------------------------------------------------
-
   class Mouse
     @@total = 0
     # Class variables are prefixed with two '@' characters.
-
-    def initialize(n)
-      @name = n
-      # Instance variables are prefixed with one '@' character.
+    def initialize(num)
+      @name = num
       @@total += 1 
     end
 
-    def name
-      @name
-    end
+    attr_reader :name
 
-    def Mouse.count
+    def self.count
       @@total
     end
   end
 
   def test_instance_variable 
-    oscar = Mouse.new("Oscar")
-    assert_equal __, oscar.name 
+    oscar = Mouse.new('Oscar')
+    assert_equal 'Oscar', oscar.name 
   end
 
   def test_class_variable
@@ -79,12 +68,9 @@ class AboutVariableScope < Neo::Koan
 
   # Meditate on the following: 
   # What is the difference between a class variable and instance variable?
-
   # ------------------------------------------------------
-  
   $anywhere = 'Anywhere'
   # Global variables are prefixed with the '$' character.
-
   def test_global_variables_can_be_accessed_from_any_scope
     assert_equal 'Anywhere', anywhere    
   end
--- 3531/0/triangle.rb
+++ 3531/0/triangle.rb
@@ -11,26 +11,18 @@
 # and
 #   about_triangle_project_2.rb
 #
-def triangle(a_val, b_val, c_val)
-  raise TriangleError if [a_val, b_val, c_val].min <= 0
+def triangle(a_side, b_side, c_side)
+  a_side, b_side, c_side = [a_side, b_side, c_side].sort
 
-  x_val, y_val, z_val = [a_val, b_val, c_val].sort
+  raise TriangleError unless a_side.positive?
 
-  raise TriangleError if x_val + y_val <= z_val
+  raise TriangleError unless a_side + b_side > c_side
 
-  %i[equilateral isosceles scalene].fetch([a_val, b_val, c_val].uniq.size - 1)
+  return :equilateral if a_side == c_side
 
-  define_type(a_val, b_val, c_val)
-end
+  return :isosceles if a_side == b_side || b_side == c_side
 
-def define_type(a_val, b_val, c_val)
-  if a_val.eql?(b_val) && b_val.eql?(c_val) && c_val.eql?(a_val)
-    :equilateral
-  elsif a_val.eql?(b_val) || b_val.eql?(c_val) || c_val.eql?(a_val)
-    :isosceles
-  elsif a_val != b_val && b_val != c_val && c_val != a_val
-    :scalene
-  end
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
