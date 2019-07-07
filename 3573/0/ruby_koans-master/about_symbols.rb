63bd6d416343588ed989a4a5162503ac0067d186
AS-AlStar <starovojtov.alexander@gmail.com>
2019-07-07 13:16:47 +0300

    Fix method test_constants_become_symbols

--- 3573/0/ruby_koans-master/about_symbols.rb
+++ 3573/0/ruby_koans-master/about_symbols.rb
@@ -38,7 +38,7 @@ class AboutSymbols < Neo::Koan
     def test_constants_become_symbols
       all_symbols_as_strings = Symbol.all_symbols.map { |x| x.to_s }
 
-      assert_equal true, all_symbols_as_strings.include?(RubyConstant)
+      assert_equal false, all_symbols_as_strings.include?(RubyConstant)
     end
   end
 
