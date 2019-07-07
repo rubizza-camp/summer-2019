9b68d283725ba72a3030ce1a6f1e8bd276e22c6f
Demeugaliyev <m.demeugaliev@gmail.com>
2019-07-07 18:20:35 +0600

    step 26

--- 3751/0/about_array_assignment.rb
+++ 3751/0/about_array_assignment.rb
@@ -14,8 +14,8 @@ class AboutArrayAssignment < Neo::Koan
 
   def test_parallel_assignments_with_extra_values
     first_name, last_name = ["John", "Smith", "III"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "John", first_name
+    assert_equal "Smith", last_name
   end
 
   def test_parallel_assignments_with_splat_operator
