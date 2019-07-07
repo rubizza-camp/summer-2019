991f7a3d4a130e5f7910dd1f63e140a39a562507
maksimmanzhai <bender23max@gmail.com>
2019-07-07 19:59:25 +0300

    Writing function of returns the type of triangle

--- 3532/0/triangle.rb
+++ 3532/0/triangle.rb
@@ -14,7 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  a, b, c = [a, b, c].sort
+
+  raise TriangleError unless a.positive?
+
+  raise TriangleError unless a + b > c
+
+  return :equilateral if a == c
+
+  return :isosceles if a == b || b == c
+
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
