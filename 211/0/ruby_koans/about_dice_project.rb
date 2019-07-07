8dece20bf408d9f861c3d5333ec8df68156d322c
iris-the-fox <iris-miz@yandex.ru>
2019-07-07 19:39:58 +0300

    Solve dice project

--- 211/0/ruby_koans/about_dice_project.rb
+++ 211/0/ruby_koans/about_dice_project.rb
@@ -2,9 +2,12 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_reader :values
+  def roll(num)
+    @values = Array.new(num) {|i| rand(1..6)}
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
