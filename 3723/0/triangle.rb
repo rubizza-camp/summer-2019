763062fcdeea1054d60cdaae37c5b440b1b06a14
Hawat <gmhawat@gmail.com>
2019-07-07 23:52:02 +0300

    Fix for rubocop

--- 3723/0/triangle.rb
+++ 3723/0/triangle.rb
@@ -13,12 +13,14 @@
 # and
 #   about_triangle_project_2.rb
 #
-def triangle(a, b, c)
-  raise TriangleError, "Sides must by numbers greater than zero" if (a <= 0) || (b <= 0) || (c <= 0)
-  raise TriangleError, "Sides must be right" if (a+b <= c) || (a+c <= b) || (b+c <= a)
-  return :equilateral if a == b && a == c && b == c
-  return :isosceles if a == b || a == c || b == c
-  return :scalene if a != b && a != c && b != c
+def triangle(a_s, b_s, c_s)
+  a_s, b_s, c_s = [a_s, b_s, c_s].sort
+  raise TriangleError unless a_s.positive?
+  raise TriangleError unless a_s + b_s > c_s
+  return :equilateral if a_s == c_s
+  return :isosceles if a_s == b_s || b_s == c_s
+
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
