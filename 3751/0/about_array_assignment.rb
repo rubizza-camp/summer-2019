b93b4ba922d3a6c62061c59d95e5c070a3489ca9
Demeugaliyev <m.demeugaliev@gmail.com>
2019-07-07 18:13:26 +0600

    step 24

--- 3751/0/about_array_assignment.rb
+++ 3751/0/about_array_assignment.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutArrayAssignment < Neo::Koan
   def test_non_parallel_assignment
     names = ["John", "Smith"]
-    assert_equal __, names
+    assert_equal ["John", "Smith"], names
   end
 
   def test_parallel_assignments
