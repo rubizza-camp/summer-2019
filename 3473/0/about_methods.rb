55f7bce3487519faff04afe0a91b9f0a9ce0aca0
uvlad7 <47718845+uvlad7@users.noreply.github.com>
2019-07-07 21:40:27 +0300

    Update about_methods.rb

--- 3473/0/about_methods.rb
+++ 3473/0/about_methods.rb
@@ -1,5 +1,5 @@
 # rubocop:disable Lint/MissingCopEnableDirective, Lint/Void, Lint/UnreachableCode
-# rubocop:disable Style/RedundantSelf, Lint/AmbiguousRegexpLiteral, Style/AccessModifierDeclarations
+# rubocop:disable Style/RedundantSelf, Lint/AmbiguousRegexpLiteral
 # rubocop:disable Naming/UncommunicativeMethodParamName, Style/EvalWithLocation
 
 require File.expand_path(File.dirname(__FILE__) + '/neo')
