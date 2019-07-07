4e5abe3e0cd4ca583f52cc031ad2f52cd5ee0fe2
Sufl0wer <52326018+Sufl0wer@users.noreply.github.com>
2019-07-08 00:29:54 +0300

    Update about_arrays.rb

--- 3750_0/ruby_koans/about_arrays.rb
+++ 3750_0/ruby_koans/about_arrays.rb
@@ -33,20 +33,20 @@ class AboutArrays < Neo::Koan
   end
 
   def test_slicing_arrays
-    array = [:peanut, :butter, :and, :jelly]
+    array = %i[peanut butter and jelly]
 
-    assert_equal [:peanut], array[0,1]
-    assert_equal [:peanut, :butter], array[0,2]
-    assert_equal [:and, :jelly], array[2,2]
-    assert_equal [:and, :jelly], array[2,20]
-    assert_equal [], array[4,0]
-    assert_equal [], array[4,100]
-    assert_equal nil, array[5,0]
+    assert_equal %i[peanut], array[0, 1]
+    assert_equal %i[peanut butter], array[0, 2]
+    assert_equal %i[and jelly], array[2, 2]
+    assert_equal %i[and jelly], array[2, 20]
+    assert_equal [], array[4, 0]
+    assert_equal [], array[4, 100]
+    assert_equal nil, array[5, 0]
   end
 
   def test_arrays_and_ranges
     assert_equal Range, (1..5).class
-    assert_not_equal [1,2,3,4,5], (1..5)
+    assert_not_equal [1, 2, 3, 4, 5], (1..5)
     assert_equal [1, 2, 3, 4, 5], (1..5).to_a
     assert_equal [1, 2, 3, 4], (1...5).to_a
   end
@@ -54,9 +54,9 @@ class AboutArrays < Neo::Koan
   def test_slicing_with_ranges
     array = [:peanut, :butter, :and, :jelly]
 
-    assert_equal [:peanut, :butter, :and], array[0..2]
-    assert_equal [:peanut, :butter], array[0...2]
-    assert_equal [:and, :jelly], array[2..-1]
+    assert_equal %i[peanut butter and], array[0..2]
+    assert_equal %i[peanut butter], array[0...2]
+    assert_equal %i[and jelly], array[2..-1]
   end
 
   def test_pushing_and_popping_arrays
