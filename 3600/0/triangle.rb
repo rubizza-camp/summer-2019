548a467da1d7216eb9dbb420853379b6e502bfd5
Alex Samets <aliaksandr.samets@gmail.com>
2019-07-08 00:36:46 +0300

    Update triangle.rb

--- 3600/0/triangle.rb
+++ 3600/0/triangle.rb
@@ -14,7 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  a, b, c = sides = [a, b, c].sort
+  raise TriangleError unless a > 0 and a + b > c
+  [nil, :equilateral, :isosceles, :scalene][sides.uniq.size]
 end
 
 # Error class used in part 2.  No need to change this code.
