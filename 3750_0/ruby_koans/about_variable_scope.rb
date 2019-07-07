8dcf617f54a7731b6b642e965174fbb3372c0e6a
Sufl0wer <52326018+Sufl0wer@users.noreply.github.com>
2019-07-07 23:22:21 +0300

    Update about_variable_scope.rb

--- 3750_0/ruby_koans/about_variable_scope.rb
+++ 3750_0/ruby_koans/about_variable_scope.rb
@@ -3,43 +3,43 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutVariableScope < Neo::Koan
 
   def bark
-    noise = "RUFF"
+    noise = 'RUFF'
   end
 
   def test_noise_is_not_available_in_the_current_scope
 
-    assert_raise(___) do
+    assert_raise(NameError) do
       noise
     end
 
   end
 
   def test_we_can_get_noise_by_calling_method
-    assert_equal __, bark
+    assert_equal 'RUFF', bark
   end
 
   inaccessible = "Outside our universe"
   def test_defs_cannot_access_variables_outside_scope
     # defined? does not return true or false
-    assert_equal __, defined? inaccesible
+    assert_equal nil, defined? inaccesible
   end
 
   # ------------------------------------------------------
 
   def test_blocks_can_access_variables_outside_scope 
-    test = "Hi"
+    test = 'Hi'
     (1..2).each do
-      test = "Hey"
+      test = 'Hey'
     end
 
-    assert_equal __, test    
+    assert_equal 'Hey', test    
   end
 
   def test_block_variables_cannot_be_accessed_outside_scope
     (1..2).each do
       x = 0 
     end
-    assert_equal __, defined? x
+    assert_equal nil, defined? x
   end
 
   # ------------------------------------------------------
@@ -64,14 +64,14 @@ class AboutVariableScope < Neo::Koan
   end
 
   def test_instance_variable 
-    oscar = Mouse.new("Oscar")
-    assert_equal __, oscar.name 
+    oscar = Mouse.new('Oscar')
+    assert_equal 'Oscar', oscar.name 
   end
 
   def test_class_variable
     (1..9).each { |i| Mouse.new("#{i}") }
     # Things may appear easier than they actually are.  
-    assert_equal __, Mouse.count
+    assert_equal 10, Mouse.count
   end
 
   # Meditate on the following: 
@@ -83,31 +83,29 @@ class AboutVariableScope < Neo::Koan
   # Global variables are prefixed with the '$' character.
 
   def test_global_variables_can_be_accessed_from_any_scope
-    assert_equal __, $anywhere    
+    assert_equal 'Anywhere', $anywhere    
   end
 
   def test_global_variables_can_be_changed_from_any_scope
     # From within a method
-    $anywhere = "Here"
-    assert_equal __, $anywhere
+    $anywhere = 'Here'
+    assert_equal 'Here', $anywhere
   end
 
   def test_global_variables_retain_value_from_last_change
     # What is $anywhere?
-    assert_equal __, $anywhere
+    assert_equal 'Here', $anywhere
   end
 
   def test_global_variables_can_be_changed_from_any_scope_2
     # From within a block
     (1..2).each do
-      $anywhere = "Hey"
+      $anywhere = 'Hey'
     end
 
-    assert_equal __, $anywhere
+    assert_equal 'Hey', $anywhere
   end
-
 end
-
 # THINK ABOUT IT:
 # 
 # What will $anywhere be down here, outside of the scope of the
