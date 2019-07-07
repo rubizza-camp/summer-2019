e58ca8be2c5265ce1791f0e8b549007ab576b3fd
z3w1l0q <z3w1l0q@PC.localdomain>
2019-07-07 01:32:29 -0700

    human fix

--- 3531/0/about_scope.rb
+++ 3531/0/about_scope.rb
@@ -64,16 +64,16 @@ class AboutScope < Neo::Koan
 
   def test_class_names_are_just_constants
     assert_equal true, MyString == ::String
-    assert_equal true, MyString == "HI".class
+    assert_equal true, MyString == 'HI'.class
   end
 
   def test_constants_can_be_looked_up_explicitly
-    assert_equal true, PI == AboutScope.const_get("PI")
-    assert_equal true, MyString == AboutScope.const_get("MyString")
+    assert_equal true, PI == AboutScope.const_get('PI')
+    assert_equal true, MyString == AboutScope.const_get('MyString')
   end
 
   def test_you_can_get_a_list_of_constants_for_any_class_or_module
     assert_equal [:Dog], Jims.constants
-    assert Object.constants.size > 0
+    assert Object.constants.size.positive?
   end
 end
