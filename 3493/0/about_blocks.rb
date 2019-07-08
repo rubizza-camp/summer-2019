c183101c5933ed2a68916fc54b6b97d0b1309186
Maxxx911 <maxrikal@gmail.com>
2019-07-08 22:09:34 +0300

    finished fix reek errors

--- 3493/0/about_blocks.rb
+++ 3493/0/about_blocks.rb
@@ -67,7 +67,7 @@ class AboutBlocks < Neo::Koan
   end
 
   def test_blocks_can_be_assigned_to_variables_and_called_explicitly
-    add_one = ->(n) { n + 1 }
+    add_one = ->(number) { number + 1 }
     assert_equal 11, add_one.call(10)
 
     # Alternative calling syntax
@@ -75,7 +75,7 @@ class AboutBlocks < Neo::Koan
   end
 
   def test_stand_alone_blocks_can_be_passed_to_methods_expecting_blocks
-    make_upper = ->(n) { n.upcase }
+    make_upper = ->(number) { number.upcase }
     result = method_with_block_arguments(&make_upper)
     assert_equal 'JIM', result
   end
@@ -87,9 +87,9 @@ class AboutBlocks < Neo::Koan
   end
 
   def test_methods_can_take_an_explicit_block_argument
-    assert_equal true, method_with_explicit_block { |n| n * 2 } == 20
+    assert_equal true, method_with_explicit_block { |number| number * 2 } == 20
 
-    add_one = ->(n) { n + 1 }
+    add_one = ->(number) { number + 1 }
     assert_equal 11, method_with_explicit_block(&add_one)
   end
 end
