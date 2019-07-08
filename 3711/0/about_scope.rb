0d60e83c9c0c203232cafb3e94c8a5be1fb4fea2
lokkirill <lokkirill@gmail.com>
2019-07-09 00:07:20 +0300

    Used "rubocop -a" and fixed it

--- 3711/0/about_scope.rb
+++ 3711/0/about_scope.rb
@@ -43,11 +43,11 @@ class AboutScope < Neo::Koan
   end
 
   def test_nested_string_is_not_the_same_as_the_system_string
-    assert_equal false, String == "HI".class
+    assert_equal false, String == 'HI'.class
   end
 
   def test_use_the_prefix_scope_operator_to_force_the_global_scope
-    assert_equal true, ::String == "HI".class
+    assert_equal true, ::String == 'HI'.class
   end
 
   # ------------------------------------------------------------------
@@ -64,12 +64,12 @@ class AboutScope < Neo::Koan
 
   def test_class_names_are_just_constants
     assert_equal true, MyString == ::String
-    assert_equal true, MyString == "HI".class
+    assert_equal true, MyString == 'HI'.class
   end
 
   def test_constants_can_be_looked_up_explicitly
-    assert_equal true, PI == AboutScope.const_get("PI")
-    assert_equal true, MyString == AboutScope.const_get("MyString")
+    assert_equal true, PI == AboutScope.const_get('PI')
+    assert_equal true, MyString == AboutScope.const_get('MyString')
   end
 
   def test_you_can_get_a_list_of_constants_for_any_class_or_module
--- 3711/0/about_scoring_project.rb
+++ 3711/0/about_scoring_project.rb
@@ -35,29 +35,29 @@ def score(dice)
 
   score = 0
   results = dice_result(dice)
-  results.each { |num, count|
+  results.each do |num, count|
     if count >= 3
-      if num == 1
-        score += num*1000*(count/3) + (count%3)*100
-      elsif num == 5
-        score += num*100*(count/3) + (count%3)*50
-      else
-        score += num*100*(count/3)
-      end
+      score += if num == 1
+                 num * 1000 * (count / 3) + (count % 3) * 100
+               elsif num == 5
+                 num * 100 * (count / 3) + (count % 3) * 50
+               else
+                 num * 100 * (count / 3)
+               end
     else
       if num == 1
-        score += count*100
+        score += count * 100
       elsif num == 5
-        score += count*50
+        score += count * 50
       end
     end
-  }
+  end
   score
 end
 
 def dice_result(dice)
   dice.each_with_object(Hash.new(0)) do |side, result|
-     result [side] += 1
+    result [side] += 1
   end
 end
 
@@ -75,32 +75,31 @@ class AboutScoringProject < Neo::Koan
   end
 
   def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
-    assert_equal 300, score([1,5,5,1])
+    assert_equal 300, score([1, 5, 5, 1])
   end
 
   def test_score_of_single_2s_3s_4s_and_6s_are_zero
-    assert_equal 0, score([2,3,4,6])
+    assert_equal 0, score([2, 3, 4, 6])
   end
 
   def test_score_of_a_triple_1_is_1000
-    assert_equal 1000, score([1,1,1])
+    assert_equal 1000, score([1, 1, 1])
   end
 
   def test_score_of_other_triples_is_100x
-    assert_equal 200, score([2,2,2])
-    assert_equal 300, score([3,3,3])
-    assert_equal 400, score([4,4,4])
-    assert_equal 500, score([5,5,5])
-    assert_equal 600, score([6,6,6])
+    assert_equal 200, score([2, 2, 2])
+    assert_equal 300, score([3, 3, 3])
+    assert_equal 400, score([4, 4, 4])
+    assert_equal 500, score([5, 5, 5])
+    assert_equal 600, score([6, 6, 6])
   end
 
   def test_score_of_mixed_is_sum
-    assert_equal 250, score([2,5,2,2,3])
-    assert_equal 550, score([5,5,5,5])
-    assert_equal 1100, score([1,1,1,1])
-    assert_equal 1200, score([1,1,1,1,1])
-    assert_equal 1150, score([1,1,1,5,1])
-    assert_equal 1050, score([5,5,5,5,5,5,5])
+    assert_equal 250, score([2, 5, 2, 2, 3])
+    assert_equal 550, score([5, 5, 5, 5])
+    assert_equal 1100, score([1, 1, 1, 1])
+    assert_equal 1200, score([1, 1, 1, 1, 1])
+    assert_equal 1150, score([1, 1, 1, 5, 1])
+    assert_equal 1050, score([5, 5, 5, 5, 5, 5, 5])
   end
-
 end
