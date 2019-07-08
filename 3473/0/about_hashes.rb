05c8f11c1b5323ed0a1ee4650fb4d5e9667cf914
uvlad7 <47718845+uvlad7@users.noreply.github.com>
2019-07-09 00:50:18 +0300

    Update about_hashes.rb

--- 3473/0/about_hashes.rb
+++ 3473/0/about_hashes.rb
@@ -90,7 +90,7 @@ class AboutHashes < Neo::Koan
     assert_equal 'dos', hash2[:two]
   end
 
-  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable all
   def test_default_value_is_the_same_object
     hash = Hash.new([])
 
@@ -103,7 +103,7 @@ class AboutHashes < Neo::Koan
 
     assert_equal true, hash[:one].object_id == hash[:two].object_id
   end
-  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable all
 
   def test_default_value_with_block
     hash = Hash.new { |hash_local, key| hash_local[key] = [] }
