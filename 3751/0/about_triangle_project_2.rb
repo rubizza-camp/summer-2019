7938ec04bc6b7912e24307d67501dd36e752eaf5
Demeugaliyev <m.demeugaliev@gmail.com>
2019-07-08 16:27:23 +0600

    exceptions and 2nd triange

--- 3751/0/about_triangle_project_2.rb
+++ 3751/0/about_triangle_project_2.rb
@@ -7,10 +7,10 @@ class AboutTriangleProject2 < Neo::Koan
   # The first assignment did not talk about how to handle errors.
   # Let's handle that part now.
   def test_illegal_triangles_throw_exceptions
-    assert_raise(TriangleError) do triangle(0, 0, 0) end
-    assert_raise(TriangleError) do triangle(3, 4, -5) end
-    assert_raise(TriangleError) do triangle(1, 1, 3) end
-    assert_raise(TriangleError) do triangle(2, 4, 2) end
+    assert_raise(TriangleError) do triangles(0, 0, 0) end
+    assert_raise(TriangleError) do triangles(3, 4, -5) end
+    assert_raise(TriangleError) do triangles(1, 1, 3) end
+    assert_raise(TriangleError) do triangles(2, 4, 2) end
     # HINT: for tips, see http://stackoverflow.com/questions/3834203/ruby-koan-151-raising-exceptions
   end
 end
--- 3751/0/triangle.rb
+++ 3751/0/triangle.rb
@@ -14,7 +14,28 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == b && a == c
+      return :equilateral
+    elsif a == b || a == c || b == c
+      return :isosceles
+    else
+      return :scalene
+  end
+end
+
+def check_triangle(a, b, c)
+  zero_side = (a * b * c).zero?
+  negative_side = (a.negative? || b.negative? || c.negative?)
+  less_sides = (a + b <= c || a + c <= b ||
+     b + c <= a)
+  zero_side || negative_side || less_sides
+end
+
+def triangles(a, b, c)
+  triangle_is_right = false if check_triangle(a, b, c)
+
+  raise TriangleError unless triangle_is_right
+  identify_kind_of_triangle(a, b, c)
 end
 
 # Error class used in part 2.  No need to change this code.
