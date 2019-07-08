366ee8bd54b23ff9a13bd2e354a43970b1deebb6
Alex Samets <aliaksandr.samets@gmail.com>
2019-07-09 02:13:32 +0300

    Update about_triangle_project_2.rb

--- 3600/0/about_triangle_project_2.rb
+++ 3600/0/about_triangle_project_2.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # You need to write the triangle method in the file 'triangle.rb'
 require './triangle.rb'
-#:nodoc:
+
+# :reek:TooManyStatements and :reek:UncommunicativeModuleName
 class AboutTriangleProject2 < Neo::Koan
   # The first assignment did not talk about how to handle errors.
   # Let's handle that part now.
