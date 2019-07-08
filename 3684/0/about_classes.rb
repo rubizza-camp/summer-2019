b8bdede26e22ebe3aaf7a79fd3ddeca7e29260b9
qb <inudad@yandex.ru>
2019-07-09 02:35:01 +0300

    fixed ab-classes & ab-blocks

--- 3684/0/about_classes.rb
+++ 3684/0/about_classes.rb
@@ -1,6 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
-
+# rubocop:disable Naming/AccessorMethodName, Style/EvalWithLocation
+#  :reek:TooManyMethods
+#  :reek:InstanceVariableAssumption
+# class for complete task about classes
 class AboutClasses < Neo::Koan
+  # class about dog number 1
   class Dog
   end
 
@@ -10,27 +14,29 @@ class AboutClasses < Neo::Koan
   end
 
   # ------------------------------------------------------------------
-
-  class Dog2
-    def name(a_name)
+  # class about dog number 2
+  class DogNumberTwo
+    def set_name(a_name)
       @name = a_name
     end
   end
 
+  # :reek:DuplicateMethodCall
+  # :reek:FeatureEnvy
   def test_instance_variables_can_be_set_by_assigning_to_them
-    fido = Dog2.new
+    fido = DogNumberTwo.new
     assert_equal [], fido.instance_variables
 
-    fido.name('Fido')
+    fido.set_name('Fido')
     assert_equal [:@name], fido.instance_variables
   end
 
-  # rubocop:disable Style/EvalWithLocation
+  # :reek:TooManyStatements
   def test_instance_variables_cannot_be_accessed_outside_the_class
-    fido = Dog2.new
-    fido.name('Fido')
+    fido = DogNumberTwo.new
+    fido.set_name('Fido')
 
-    assert_raise(ArgumentError) do
+    assert_raise(NoMethodError) do
       fido.name
     end
 
@@ -40,72 +46,77 @@ class AboutClasses < Neo::Koan
     end
   end
 
+  # :reek:FeatureEnvy
   def test_you_can_politely_ask_for_instance_variable_values
-    fido = Dog2.new
-    fido.name('Fido')
+    fido = DogNumberTwo.new
+    fido.set_name('Fido')
 
     assert_equal 'Fido', fido.instance_variable_get('@name')
   end
 
   def test_you_can_rip_the_value_out_using_instance_eval
-    fido = Dog2.new
-    fido.name('Fido')
+    fido = DogNumberTwo.new
+    fido.set_name('Fido')
 
     assert_equal 'Fido', fido.instance_eval('@name') # string version
-    assert_equal 'Fido', (fido.instance_eval { @name }) # block version
+    assert_equal('Fido', fido.instance_eval { @name }) # block version
   end
 
-  # rubocop:enable Style/EvalWithLocation
   # ------------------------------------------------------------------
 
-  class Dog3
-    def name(a_name)
+  # class about dog number 3
+  class DogNumberThree
+    def set_name(a_name)
       @name = a_name
     end
 
-    attr_reader :attr_name
+    attr_reader :name
   end
 
+  # :reek:FeatureEnvy
   def test_you_can_create_accessor_methods_to_return_instance_variables
-    fido = Dog3.new
-    fido.name('Fido')
+    fido = DogNumberThree.new
+    fido.set_name('Fido')
 
     assert_equal 'Fido', fido.name
   end
 
   # ------------------------------------------------------------------
-
-  class Dog4
+  # class about dog number 4
+  class DogNumberFour
     attr_reader :name
 
-    def name(a_name)
+    def set_name(a_name)
       @name = a_name
     end
   end
 
+  # :reek:FeatureEnvy
   def test_attr_reader_will_automatically_define_an_accessor
-    fido = Dog4.new
-    fido.name('Fido')
+    fido = DogNumberFour.new
+    fido.set_name('Fido')
 
     assert_equal 'Fido', fido.name
   end
 
   # ------------------------------------------------------------------
-
-  class Dog5
+  # :reek:Attribute
+  # class about dog number 5
+  class DogNumberFive
     attr_accessor :name
   end
 
+  # :reek:FeatureEnvy
   def test_attr_accessor_will_automatically_define_both_read_and_write_accessors
-    fido = Dog5.new
+    fido = DogNumberFive.new
 
     fido.name = 'Fido'
     assert_equal 'Fido', fido.name
   end
 
   # ------------------------------------------------------------------
-
-  class Dog6
+  # class about dog number 6
+  class DogNumberSix
     attr_reader :name
     def initialize(initial_name)
       @name = initial_name
@@ -113,35 +124,35 @@ class AboutClasses < Neo::Koan
   end
 
   def test_initialize_provides_initial_values_for_instance_variables
-    fido = Dog6.new('Fido')
+    fido = DogNumberSix.new('Fido')
     assert_equal 'Fido', fido.name
   end
 
   def test_args_to_new_must_match_initialize
     assert_raise(ArgumentError) do
-      Dog6.new
+      DogNumberSix.new
     end
     # THINK ABOUT IT:
     # Why is this so?
   end
 
   def test_different_objects_have_different_instance_variables
-    fido = Dog6.new('Fido')
-    rover = Dog6.new('Rover')
+    fido = DogNumberSix.new('Fido')
+    rover = DogNumberSix.new('Rover')
 
     assert_equal true, rover.name != fido.name
   end
 
   # ------------------------------------------------------------------
-
-  class Dog7
+  # class about dog number 7
+  class DogNumberSeven
     attr_reader :name
 
     def initialize(initial_name)
       @name = initial_name
     end
 
-    def getself
+    def get_self
       self
     end
 
@@ -155,24 +166,24 @@ class AboutClasses < Neo::Koan
   end
 
   def test_inside_a_method_self_refers_to_the_containing_object
-    fido = Dog7.new('Fido')
+    fido = DogNumberSeven.new('Fido')
 
-    fidos_self = fido.getself
+    fidos_self = fido.get_self
     assert_equal fido, fidos_self
   end
 
   def test_to_s_provides_a_string_version_of_the_object
-    fido = Dog7.new('Fido')
+    fido = DogNumberSeven.new('Fido')
     assert_equal 'Fido', fido.to_s
   end
 
   def test_to_s_is_used_in_string_interpolation
-    fido = Dog7.new('Fido')
+    fido = DogNumberSeven.new('Fido')
     assert_equal 'My dog is Fido', "My dog is #{fido}"
   end
 
   def test_inspect_provides_a_more_complete_string_version
-    fido = Dog7.new('Fido')
+    fido = DogNumberSeven.new('Fido')
     assert_equal "<Dog named 'Fido'>", fido.inspect
   end
 
@@ -186,3 +197,4 @@ class AboutClasses < Neo::Koan
     assert_equal '"STRING"', 'STRING'.inspect
   end
 end
+# rubocop:enable Naming/AccessorMethodName, Style/EvalWithLocation
--- 3684/0/about_hashes.rb
+++ 3684/0/about_hashes.rb
@@ -91,6 +91,7 @@ class AboutHashes < Neo::Koan
   end
 
   # rubocop:disable Metrics/AbcSize
+
   def test_default_value_is_the_same_object
     hash = Hash.new([])
     hash[:one] << 'uno'
@@ -100,6 +101,7 @@ class AboutHashes < Neo::Koan
 
     assert_equal true, hash[:one].object_id == hash[:two].object_id
   end
+  
   # rubocop:enable Metrics/AbcSize
 
   def test_default_value_with_block
