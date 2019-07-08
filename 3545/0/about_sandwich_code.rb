f0d172f485dddd2430b77a8c6d39131d2b60e1d7
nikitamasalkov <nikitamasalkov.tang@gmail.com>
2019-07-09 02:06:21 +0300

    Checking online hound3

--- 3545/0/about_sandwich_code.rb
+++ 3545/0/about_sandwich_code.rb
@@ -14,8 +14,6 @@ class AboutSandwichCode < Neo::Koan
     assert_equal 4, count_lines('example_file.txt')
   end
 
-  # ------------------------------------------------------------------
-
   def find_line(file_name)
     file = open(file_name)
     while line = file.gets
@@ -28,28 +26,6 @@ class AboutSandwichCode < Neo::Koan
   def test_finding_lines
     assert_equal "test\n", find_line('example_file.txt')
   end
-
-  # ------------------------------------------------------------------
-  # THINK ABOUT IT:
-  #
-  # The count_lines and find_line are similar, and yet different.
-  # They both follow the pattern of "sandwich code".
-  #
-  # Sandwich code is code that comes in three parts: (1) the top slice
-  # of bread, (2) the meat, and (3) the bottom slice of bread.  The
-  # bread part of the sandwich almost always goes together, but
-  # the meat part changes all the time.
-  #
-  # Because the changing part of the sandwich code is in the middle,
-  # abstracting the top and bottom bread slices to a library can be
-  # difficult in many languages.
-  #
-  # (Aside for C++ programmers: The idiom of capturing allocated
-  # pointers in a smart pointer constructor is an attempt to deal with
-  # the problem of sandwich code for resource allocation.)
-  #
-  # Consider the following code:
-  #
 
   def file_sandwich(file_name)
     file = open(file_name)
@@ -72,7 +48,6 @@ class AboutSandwichCode < Neo::Koan
     assert_equal 4, count_lines2('example_file.txt')
   end
 
-  # ------------------------------------------------------------------
   def find_line2(file_name)
     file = open(file_name)
     while line = file.gets
@@ -85,8 +60,6 @@ class AboutSandwichCode < Neo::Koan
   def test_finding_lines2
     assert_equal "is\n", find_line2('example_file.txt')
   end
-
-  # ------------------------------------------------------------------
 
   def count_lines3(file_name)
     open(file_name) do |file|
