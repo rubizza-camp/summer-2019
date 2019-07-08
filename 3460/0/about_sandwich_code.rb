c3496f73f52406ed1928f185abfc80a78dc43a94
Anton Stashevski <49589929+AntonStashevski@users.noreply.github.com>
2019-07-08 19:22:56 +0300

    Update about_sandwich_code.rb

--- 3460/0/about_sandwich_code.rb
+++ 3460/0/about_sandwich_code.rb
@@ -1,4 +1,3 @@
-# rubocop:disable Security/Open, Lint/AssignmentInCondition
 # rubocop:disable Performance/RedundantMatch, Lint/UnneededCopDisableDirective
 
 require File.expand_path(File.dirname(__FILE__) + '/neo')
@@ -112,5 +111,4 @@ class AboutSandwichCode < Neo::Koan
     assert_equal 4, count_lines3('example_file.txt')
   end
 end
-# rubocop:enable Security/Open, Lint/AssignmentInCondition
 # rubocop:enable Performance/RedundantMatch, Lint/UnneededCopDisableDirective
