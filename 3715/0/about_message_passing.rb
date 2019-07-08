534502cbd09da8be9f5bad4084433812ac0fe224
Almix047 <Amix047@gmail.com>
2019-07-08 08:15:13 +0000

    Fix issues indicated by the hound in homework 0
      To pass the Hound and match the human style code.

--- 3715/0/about_message_passing.rb
+++ 3715/0/about_message_passing.rb
@@ -115,15 +115,13 @@ class AboutMessagePassing < Neo::Koan
 
   # :reek:ManualDispatch
   # :reek:UtilityFunction
-  # rubocop:disable Style/MethodMissingSuper
-  # rubocop:disable Style/MissingRespondToMissing
+  # rubocop:disable Style/MethodMissing
   class AllMessageCatcher
     def method_missing(method_name, *args)
       "Someone called #{method_name} with <#{args.join(', ')}>"
     end
   end
-  # rubocop:enable Style/MissingRespondToMissing
-  # rubocop:enable Style/MethodMissingSuper
+  # rubocop:enable Style/MethodMissing
 
   def test_all_messages_are_caught
     catcher = AllMessageCatcher.new
@@ -145,8 +143,7 @@ class AboutMessagePassing < Neo::Koan
 
   # ------------------------------------------------------------------
 
-  # rubocop:disable Style/MethodMissingSuper
-  # rubocop:disable Style/MissingRespondToMissing
+  # rubocop:disable Style/MethodMissing
   class WellBehavedFooCatcher
     def method_missing(method_name, *args, &block)
       if method_name.to_s[0, 3] == 'foo'
@@ -156,8 +153,7 @@ class AboutMessagePassing < Neo::Koan
       end
     end
   end
-  # rubocop:enable Style/MissingRespondToMissing
-  # rubocop:enable Style/MethodMissingSuper
+  # rubocop:enable Style/MethodMissing
 
   def test_foo_method_are_caught
     catcher = WellBehavedFooCatcher.new
--- 3715/0/about_proxy_object_project.rb
+++ 3715/0/about_proxy_object_project.rb
@@ -19,12 +19,12 @@ class Proxy
     @messages = Hash.new(0)
   end
 
-  # rubocop:disable Style/MethodMissingSuper
+  # rubocop:disable Style/MethodMissing
   def method_missing(method_name, *args, &block)
     @messages[method_name] += 1
     @object.send(method_name, *args, &block)
   end
-  # rubocop:enable Style/MethodMissingSuper
+  # rubocop:enable Style/MethodMissing
 
   def called?(method_name)
     @messages.key?(method_name)
