23deed51a18e69ac3c9deb232a240700a9e62ade
maksimmanzhai <bender23max@gmail.com>
2019-07-08 22:05:05 +0300

    Edit dice project

--- 3532/0/about_dice_project.rb
+++ 3532/0/about_dice_project.rb
@@ -25,7 +25,7 @@ class AboutDiceProject < Neo::Koan
     dice = DiceSet.new
 
     dice.roll(5)
-    assert dice.values.is_a?(Array), "should be an array"
+    assert dice.values.is_a?(Array), 'should be an array'
     assert_equal 5, dice.values.size
     dice.values.each do |value|
       assert value >= 1 && value <= 6, "value #{value} must be between 1 and 6"
