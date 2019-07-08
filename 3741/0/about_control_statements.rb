0015e5ccdf933f6e79f2a756103e50f4f1009f1b
middaysan <midda7@gmail.com>
2019-07-08 22:47:40 +0300

    Comlete first part koans

---
+++ 3741/0/about_control_statements.rb
@@ -0,0 +1,142 @@
+require File.expand_path(File.dirname(__FILE__) + '/neo')
+
+class AboutControlStatements < Neo::Koan
+
+  def test_if_then_else_statements
+    if true
+      result = :true_value
+    else
+      result = :false_value
+    end
+    assert_equal :true_value, result
+  end
+
+  def test_if_then_statements
+    result = :default_value
+    if true
+      result = :true_value
+    end
+    assert_equal :true_value, result
+  end
+
+  def test_if_statements_return_values
+    value = if true
+              :true_value
+            else
+              :false_value
+            end
+    assert_equal :true_value, value
+
+    value = if false
+              :true_value
+            else
+              :false_value
+            end
+    assert_equal :false_value, value
+
+    # NOTE: Actually, EVERY statement in Ruby will return a value, not
+    # just if statements.
+  end
+
+  def test_if_statements_with_no_else_with_false_condition_return_value
+    value = if false
+              :true_value
+            end
+    assert_equal nil, value
+  end
+
+  def test_condition_operators
+    assert_equal :true_value, (true ? :true_value : :false_value)
+    assert_equal :false_value, (false ? :true_value : :false_value)
+  end
+
+  def test_if_statement_modifiers
+    result = :default_value
+    result = :true_value if true
+
+    assert_equal :true_value, result
+  end
+
+  def test_unless_statement
+    result = :default_value
+    unless false    # same as saying 'if !false', which evaluates as 'if true'
+      result = :false_value
+    end
+    assert_equal :false_value, result
+  end
+
+  def test_unless_statement_evaluate_true
+    result = :default_value
+    unless true    # same as saying 'if !true', which evaluates as 'if false'
+      result = :true_value
+    end
+    assert_equal :default_value, result
+  end
+
+  def test_unless_statement_modifier
+    result = :default_value
+    result = :false_value unless false
+
+    assert_equal :false_value, result
+  end
+
+  def test_while_statement
+    i = 1
+    result = 1
+    while i <= 10
+      result = result * i
+      i += 1
+    end
+    assert_equal 3628800, result
+  end
+
+  def test_break_statement
+    i = 1
+    result = 1
+    while true
+      break unless i <= 10
+      result = result * i
+      i += 1
+    end
+    assert_equal 3628800, result
+  end
+
+  def test_break_statement_returns_values
+    i = 1
+    result = while i <= 10
+      break i if i % 2 == 0
+      i += 1
+    end
+
+    assert_equal 2, result
+  end
+
+  def test_next_statement
+    i = 0
+    result = []
+    while i < 10
+      i += 1
+      next if (i % 2) == 0
+      result << i
+    end
+    assert_equal [1,3,5,7,9], result
+  end
+
+  def test_for_statement
+    array = ["fish", "and", "chips"]
+    result = []
+    for item in array
+      result << item.upcase
+    end
+    assert_equal ["FISH", "AND", "CHIPS"], result
+  end
+
+  def test_times_statement
+    sum = 0
+    10.times do
+      sum += 1
+    end
+    assert_equal 10, sum
+  end
+
+end
---
+++ 3741/0/triangle.rb
@@ -0,0 +1,37 @@
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+#
+def triangle(a, b, c)
+
+	s = (a + b + c) / 2.0
+	validate = (s - a) * (s - b) * (s - c)
+
+	if a <= 0 || b <= 0 || c <= 0 || validate <= 0 then 
+    	raise TriangleError
+	end
+
+	if a == b && b == c then
+		:equilateral
+	elsif a == b || a == c || b == c then
+		:isosceles
+	else
+		:scalene
+	end
+	
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
