f67bbf53267861ffb1d535e5c26a2aba821d1854
MargaretOm <margosha20617@gmail.com>
2019-07-09 14:03:21 +0200

    Rewrote code, the code became clear

--- 3604/0/triangle.rb
+++ 3604/0/triangle.rb
@@ -21,21 +21,43 @@ class Triangle
     @val_c = val_c
   end
 
-  def test_error
-    raise TriangleError if @val_a <= 0 || @val_b <= 0 || @val_c <= 0
-    sum = (@val_a + @val_b + @val_c) / 2.0
-    raise TriangleError if ((sum - @val_a) * (sum - @val_b) * (sum - @val_c)) <= 0
+  private def test_error
+    raise TriangleError if negative_sides? || some_condition_again?
+  end
+
+  private def negative_sides?
+    @val_a <= 0 || @val_b <= 0 || @val_c <= 0
+  end
+
+  private def some_condition_again?
+    ((sum - @val_a) * (sum - @val_b) * (sum - @val_c)) <= 0
+  end
+
+  private def sum
+    @sum ||= (@val_a + @val_b + @val_c) / 2.0
   end
 
   def triangle
     test_error
-    if @val_a == @val_b && @val_a == @val_c
+    if equilateral?
       :equilateral
-    elsif @val_a == @val_b || @val_a == @val_c || @val_b == @val_c
+    elsif isosceles?
       :isosceles
-    else
+    elsif scalene?
       :scalene
     end
+  end
+
+  private def equilateral?
+    @val_a == @val_b && @val_a == @val_c
+  end
+
+  private def isosceles?
+    @val_a == @val_b || @val_a == @val_c || @val_b == @val_c
+  end
+
+  private def scalene?
+    @val_a != @val_b && @val_a != @val_c
   end
 end
 # :reek:UtilityFunction
