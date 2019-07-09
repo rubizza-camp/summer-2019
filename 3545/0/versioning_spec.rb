6d8ea86483aa1df6a2ec3e635e3ee32c5e2343df
nikitamasalkov <nikitamasalkov.tang@gmail.com>
2019-07-09 22:14:59 +0300

    check

--- 3545/0/versioning_spec.rb
+++ 3545/0/versioning_spec.rb
@@ -3,7 +3,7 @@ def version_ints(version)
   version.split('.').map(&:to_i)
 end
 # :reek:NilCheck
-
+# :reek:UncommunicativeMethodName
 def at_least_ruby_version(version)
   vints = version_ints(version)
   ruby_vints = version_ints(RUBY_VERSION)
