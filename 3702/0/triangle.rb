25ad88888874871d863427de5e4788d367dfc2ba
pkhilenko <pkhilenko@yandex.ru>
2019-07-09 15:23:49 +0600

    fix triangle

--- 3702/0/triangle.rb
+++ 3702/0/triangle.rb
@@ -1,28 +1,25 @@
 def triangle(a_side, b_side, c_side)
-  validate_size(a_side, b_side, c_side)
+  side_no_min_zero(a_side, b_side, c_side)
+  two_side_more3(a_side, b_side, c_side)
   angle_model(a_side, b_side, c_side)
 end
 
-def validate_size(a_side, b_side, c_side)
+def side_no_min_zero(a_side, b_side, c_side)
   if a_side.negative? || b_side.negative? || c_side.negative?
-    raise TriangleError, 'сторона не может быть меньше нуля bили равной нулю'
+    raise TriangleError, 'сторона не может быть меньше нуля или равной нулю'
   end
+end
 
+def two_side_more3(a_side, b_side, c_side)
   if a_side + b_side <= c_side || a_side + c_side <= b_side || b_side + c_side <= a_side
     raise TriangleError, '2 стороны не могут быть меньше третей'
   end
 end
 
 def angle_model(a_side, b_side, c_side)
-  if a_side == b_side && b_side == c_side && a_side == c_side
-    :equilateral
-  elsif (a_side == b_side) || (a_side == c_side) || (c_side == b_side)
-    :isosceles
-  else
-    :scalene
-  end
+  (a_side == b_side && b_side == c_side && a_side == c_side) ? :equilateral :
+  (a_side == b_side) || (a_side == c_side) || (c_side == b_side) ? :isosceles : :scalene
 end
-
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
