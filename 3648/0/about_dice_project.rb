d4d280a755eaea1db3ae0ebcfb332b220c1f15bf
xxkvl <xxkvl@yandex.ru>
2019-07-09 12:34:52 +0300

    Fix DiceSet

--- 3648/0/about_dice_project.rb
+++ 3648/0/about_dice_project.rb
@@ -3,19 +3,10 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # Implement a DiceSet Class here:
 #
 class DiceSet
-  def initialize
-    @arr = []
-  end
+  attr_reader :values
 
-  def roll(nrg)
-    @arr = []
-    nrg.times do
-      @arr << Random.new.rand(1..6)
-    end
-  end
-
-  def values
-    @arr
+  def roll(number)
+    @values = Array.new(number) { rand(1..6) }
   end
 end
 
