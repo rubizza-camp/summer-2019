c443edbb791333442f76f8549628589cab2f2335
Maxxx911 <maxrikal@gmail.com>
2019-07-09 11:27:25 +0300

    Come back koens's code and disable rubocop checker

--- 3493/0/about_classes.rb
+++ 3493/0/about_classes.rb
@@ -12,14 +12,14 @@ class AboutClasses < Neo::Koan
   # ------------------------------------------------------------------
   # rubocop: disable Naming/AccessorMethodName,
   # rubocop: disable Style/EvalWithLocation, Lint/AmbiguousBlockAssociation
-  class SecondDog
+  class DogSecond
     def set_name(a_name)
       @name = a_name
     end
   end
 
   def test_instance_variables_can_be_set_by_assigning_to_them
-    fido = SecondDog.new
+    fido = DogSecond.new
     assert_equal [], fido.instance_variables
 
     fido.set_name('Fido')
@@ -27,7 +27,7 @@ class AboutClasses < Neo::Koan
   end
 
   def test_instance_variables_cannot_be_accessed_outside_the_class
-    fido = SecondDog.new
+    fido = DogSecond.new
     fido.set_name('Fido')
 
     assert_raise(NoMethodError) do
@@ -41,14 +41,14 @@ class AboutClasses < Neo::Koan
   end
 
   def test_you_can_politely_ask_for_instance_variable_values
-    fido = SecondDog.new
+    fido = DogSecond.new
     fido.set_name('Fido')
 
     assert_equal 'Fido', fido.instance_variable_get('@name')
   end
 
   def test_you_can_rip_the_value_out_using_instance_eval
-    fido = SecondDog.new
+    fido = DogSecond.new
     fido.set_name('Fido')
 
     assert_equal 'Fido', fido.instance_eval('@name')  # string version
@@ -57,7 +57,7 @@ class AboutClasses < Neo::Koan
 
   # ------------------------------------------------------------------
 
-  class ThirdDog
+  class DogThird
     def set_name(a_name)
       @name = a_name
     end
@@ -66,7 +66,7 @@ class AboutClasses < Neo::Koan
   end
 
   def test_you_can_create_accessor_methods_to_return_instance_variables
-    fido = ThirdDog.new
+    fido = DogThird.new
     fido.set_name('Fido')
 
     assert_equal 'Fido', fido.name
@@ -74,7 +74,7 @@ class AboutClasses < Neo::Koan
 
   # ------------------------------------------------------------------
 
-  class FourDog
+  class DogFour
     attr_reader :name
 
     def set_name(a_name)
@@ -83,7 +83,7 @@ class AboutClasses < Neo::Koan
   end
 
   def test_attr_reader_will_automatically_define_an_accessor
-    fido = FourDog.new
+    fido = DogFour.new
     fido.set_name('Fido')
 
     assert_equal 'Fido', fido.name
@@ -91,12 +91,12 @@ class AboutClasses < Neo::Koan
 
   # ------------------------------------------------------------------
   #:reek:Attribute:
-  class FiveDog
+  class DogFive
     attr_accessor :name
   end
 
   def test_attr_accessor_will_automatically_define_both_read_and_write_accessors
-    fido = FiveDog.new
+    fido = DogFive.new
 
     fido.name = 'Fido'
     assert_equal 'Fido', fido.name
@@ -104,7 +104,7 @@ class AboutClasses < Neo::Koan
 
   # ------------------------------------------------------------------
 
-  class SixDog
+  class DogSix
     attr_reader :name
     def initialize(initial_name)
       @name = initial_name
@@ -112,28 +112,28 @@ class AboutClasses < Neo::Koan
   end
 
   def test_initialize_provides_initial_values_for_instance_variables
-    fido = SixDog.new('Fido')
+    fido = DogSix.new('Fido')
     assert_equal 'Fido', fido.name
   end
 
   def test_args_to_new_must_match_initialize
     assert_raise(ArgumentError) do
-      SixDog.new
+      DogSix.new
     end
     # THINK ABOUT IT:
     # Why is this so?
   end
 
   def test_different_objects_have_different_instance_variables
-    fido = SixDog.new('Fido')
-    rover = SixDog.new('Rover')
+    fido = DogSix.new('Fido')
+    rover = DogSix.new('Rover')
 
     assert_equal true, rover.name != fido.name
   end
 
   # ------------------------------------------------------------------
 
-  class SevenDog
+  class DogSeven
     attr_reader :name
 
     def initialize(initial_name)
@@ -156,25 +156,25 @@ class AboutClasses < Neo::Koan
   end
 
   def test_inside_a_method_self_refers_to_the_containing_object
-    fido = SevenDog.new('Fido')
+    fido = DogSeven.new('Fido')
 
     fidos_self = fido.get_self
     assert_equal fido, fidos_self
   end
 
   def test_to_s_provides_a_string_version_of_the_object
-    fido = SevenDog.new('Fido')
+    fido = DogSeven.new('Fido')
     assert_equal 'Fido', fido.to_s
   end
 
   def test_to_s_is_used_in_string_interpolation
-    fido = SevenDog.new('Fido')
+    fido = DogSeven.new('Fido')
     assert_equal 'My dog is Fido', "My dog is #{fido}"
   end
 
   def test_inspect_provides_a_more_complete_string_version
-    fido = SevenDog.new('Fido')
-    assert_equal fido.inspect, fido.inspect
+    fido = DogSeven.new('Fido')
+    assert_equal "<Dog named 'Fido'>", fido.inspect
   end
 
   def test_all_objects_support_to_s_and_inspect
