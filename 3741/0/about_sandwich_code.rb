3f570caae34a5996067b1f3cf1bcc915e1783bf4
anton rudy <36412453+middaysan@users.noreply.github.com>
2019-07-09 19:22:18 +0300

    Extra empty line fix

--- 3741/0/about_sandwich_code.rb
+++ 3741/0/about_sandwich_code.rb
@@ -5,7 +5,6 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # :reek:UtilityFunction
 # :reek:NilCheck
 class AboutSandwichCode < Neo::Koan
-
   def count_lines(file_name)
     file = File.open(file_name)
     count = 0
