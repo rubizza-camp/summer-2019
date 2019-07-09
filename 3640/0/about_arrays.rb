1691fedfdf7cb94d77c9cd0b46265935f0edc674
SergeiHrenovsky <sergeihrenovsky@yandex.by>
2019-07-09 16:43:53 +0300

    commit to wake hound

--- 3640/0/about_arrays.rb
+++ 3640/0/about_arrays.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # :reek:TooManyStatements
-# Description class
+# Description class about array
 class AboutArrays < Neo::Koan
   def test_creating_arrays
     empty_array = []
