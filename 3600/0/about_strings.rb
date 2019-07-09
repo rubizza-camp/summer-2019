b2843a02bddb3ed3bba9cf630b0fe9e18039d0af
Alex Samets <aliaksandr.samets@gmail.com>
2019-07-09 03:17:30 +0300

    Update about_strings.rb

--- 3600/0/about_strings.rb
+++ 3600/0/about_strings.rb
@@ -1,10 +1,8 @@
-# rubocop:disable Lint/MissingCopEnableDirective, Metrics/ClassLength
-
 require File.expand_path(File.dirname(__FILE__) + '/neo')
-
-# This method smells of :reek:UncommunicativeVariableName
-# This method smells of :reek:TooManyStatements
-# This method smells of :reek:TooManyMethods
+# rubocop:disable Metrics/ClassLength, Layout/IndentHeredoc
+# rubocop:disable Lint/UselessAssignment
+# rubocop:disable Lint/InterpolationCheck
+# :reek:TooManyMethods
 class AboutStrings < Neo::Koan
   def test_double_quoted_strings_are_strings
     string = 'Hello, World'
@@ -18,26 +16,26 @@ class AboutStrings < Neo::Koan
 
   def test_use_single_quotes_to_create_string_with_double_quotes
     string = 'He said, "Go Away."'
-    assert_equal string, string
+    assert_equal 'He said, "Go Away."', string
   end
 
   def test_use_double_quotes_to_create_strings_with_single_quotes
     string = "Don't"
-    assert_equal string, string
+    assert_equal "Don't", string
   end
 
   def test_use_backslash_for_those_hard_cases
-    a = "He said, \"Don't\""
-    b = 'He said, "Don\'t"'
-    assert_equal true, a == b
+    value_a = "He said, \"Don't\""
+    value_b = 'He said, "Don\'t"'
+    assert_equal true, value_a == value_b
   end
 
   def test_use_flexible_quoting_to_handle_really_hard_cases
-    a = %(flexible quotes can handle both ' and " characters)
-    b = %!(flexible quotes can handle both ' and " characters)!
-    c = %(flexible quotes can handle both ' and " characters)
-    assert_equal false, a == b
-    assert_equal true, a == c
+    value_a = %(flexible quotes can handle both ' and " characters)
+    value_b = %(flexible quotes can handle both ' and " characters)
+    value_c = %(flexible quotes can handle both ' and " characters)
+    assert_equal true, value_a == value_b
+    assert_equal true, value_a == value_c
   end
 
   def test_flexible_quotes_can_handle_multiple_lines
@@ -51,10 +49,10 @@ It was the worst of times.
   end
 
   def test_here_documents_can_also_handle_multiple_lines
-    long_string = <<~TEXT
-      It was the best of times,
-      It was the worst of times.
-    TEXT
+    long_string = <<SQL
+It was the best of times,
+It was the worst of times.
+SQL
     assert_equal 53, long_string.length
     assert_equal 2, long_string.lines.count
     assert_equal 'I', long_string[0, 1]
@@ -71,7 +69,6 @@ It was the worst of times.
     string = hi + there
     assert_equal 'Hello, ', hi
     assert_equal 'World', there
-    string
   end
 
   def test_plus_equals_will_concatenate_to_the_end_of_a_string
@@ -87,7 +84,6 @@ It was the worst of times.
     there = 'World'
     hi += there
     assert_equal 'Hello, ', original_string
-    hi
   end
 
   def test_the_shovel_operator_will_also_append_content_to_a_string
@@ -124,7 +120,7 @@ It was the worst of times.
   def test_single_quotes_sometimes_interpret_escape_characters
     string = '\\\''
     assert_equal 2, string.size
-    assert_equal '\\\'', string
+    assert_equal "\\'", string
   end
 
   def test_double_quoted_strings_interpolate_variables
@@ -135,8 +131,8 @@ It was the worst of times.
 
   def test_single_quoted_strings_do_not_interpolate
     value = 123
-    string = "The value is #{value}"
-    assert_equal string, string
+    string = 'The value is #{value}'
+    assert_equal 'The value is #{value}', string
   end
 
   def test_any_ruby_expression_may_be_interpolated
@@ -162,7 +158,7 @@ It was the worst of times.
       assert_equal 'a', 'a'
       assert_equal false, 'a' == 97
 
-      assert_equal false, ('a' + 1) == 'b'
+      assert_equal __, ('a' + 1) == 'b'
     end
   end
 
@@ -195,10 +191,13 @@ It was the worst of times.
   end
 
   def test_strings_are_unique_objects
-    a = 'a string'
-    b = 'a string'
+    value_a = 'a string'
+    value_b = 'a string'
 
-    assert_equal true, a == b
-    assert_equal false, a.object_id == b.object_id
+    assert_equal true, value_a           == value_b
+    assert_equal false, value_a.object_id == value_b.object_id
   end
 end
+# rubocop:enable Metrics/ClassLength, Layout/IndentHeredoc
+# rubocop:enable Lint/UselessAssignment
+# rubocop:enable Lint/InterpolationCheck
