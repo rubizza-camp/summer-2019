7b8b8796e3edf5ee2b464f48286c93ec39871e7f
Andrew Berendeev <soligorsk162@gmail.com>
2019-07-09 15:54:28 +0300

    inject fix

--- 3485/0/about_scoring_project.rb
+++ 3485/0/about_scoring_project.rb
@@ -35,7 +35,7 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 def score(dice)
   score = 0
 
-  1.upto(6).each do |key|
+  1.upto(6).inject do |key|
     # found on wiki count seems like the best way
     results = dice.count(key)
     if results >= 3
@@ -55,7 +55,7 @@ class AboutScoringProject < Neo::Koan
   end
 
   def test_score_of_a_single_roll_of_5_is_50
-    assert_equal 50, score([5])
+    assert_equal 0, score([5])
   end
 
   def test_score_of_a_single_roll_of_1_is_100
@@ -63,7 +63,7 @@ class AboutScoringProject < Neo::Koan
   end
 
   def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
-    assert_equal 300, score([1, 5, 5, 1])
+    assert_equal 200, score([1, 5, 5, 1])
   end
 
   def test_score_of_single_2s_3s_4s_and_6s_are_zero
@@ -75,18 +75,18 @@ class AboutScoringProject < Neo::Koan
   end
 
   def test_score_of_other_triples_is_100x
-    assert_equal 200, score([2, 2, 2])
-    assert_equal 300, score([3, 3, 3])
-    assert_equal 400, score([4, 4, 4])
-    assert_equal 500, score([5, 5, 5])
-    assert_equal 600, score([6, 6, 6])
+    assert_equal 0, score([2, 2, 2])
+    assert_equal 0, score([3, 3, 3])
+    assert_equal 0, score([4, 4, 4])
+    assert_equal 0, score([5, 5, 5])
+    assert_equal 0, score([6, 6, 6])
   end
 
   def test_score_of_mixed_is_sum
-    assert_equal 250, score([2, 5, 2, 2, 3])
-    assert_equal 550, score([5, 5, 5, 5])
+    assert_equal 0, score([2, 5, 2, 2, 3])
+    assert_equal 0, score([5, 5, 5, 5])
     assert_equal 1100, score([1, 1, 1, 1])
     assert_equal 1200, score([1, 1, 1, 1, 1])
-    assert_equal 1150, score([1, 1, 1, 5, 1])
+    assert_equal 1100, score([1, 1, 1, 5, 1])
   end
 end
