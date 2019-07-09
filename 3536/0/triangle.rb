c90e0bceca7f1367a2aafd34b07c2bb4cbb84cc2
Volovenko <vadim.volovenko@gmail.com>
2019-07-09 14:05:08 +0300

    fix style

--- 3536/0/triangle.rb
+++ 3536/0/triangle.rb
@@ -2,21 +2,17 @@
 
 # Triangle analyzes the lengths of the sides of a triangle
 # (represented by a, b and c) and returns the type of triangle.
-#
 # It returns:
 #   :equilateral  if all sides are equal
 #   :isosceles    if exactly 2 sides are equal
 #   :scalene      if no sides are equal
-#
 # The tests for this method can be found in
 #   about_triangle_project.rb
 # and
-#   about_triangle_project_2.rb
-  # :reek:TooManyStatements
+# about_triangle_project_2.rb
+# :reek:TooManyStatements
 def triangle(a_side, b_side, c_side)
-
-  a_side, b_side, c_side = [a_side, b_side, c_side].sort
-  # :reek:FeatureEnvy
+  a_side, b_side, c_side = [a_side, b_side, c_side].sort # :reek:FeatureEnvy
   raise TriangleError unless a_side.positive?
   raise TriangleError unless a_side + b_side > c_side
   return :equilateral if a_side == c_side
