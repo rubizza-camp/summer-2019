4bb27c6ff436c210523a84245b29747899d798f8
Sufl0wer <fr08ty@gmail.com>
2019-07-09 13:41:08 +0300

    about_scope file fix

--- 3750/3750_0/ruby_koans/about_sandwich_code.rb
+++ 3750/3750_0/ruby_koans/about_sandwich_code.rb
@@ -19,9 +19,7 @@ class AboutSandwichCode < Neo::Koan
   def find_line(file_name)
     file = File.open(file_name)
     while (line = file.gets)
-      if line.match(/e/)
-        return line
-      end 
+      return line if line.match(/e/)
     end
   ensure
     file&.close if file
@@ -79,9 +77,7 @@ class AboutSandwichCode < Neo::Koan
   def find_line2(file_name)
     file_sandwich(file_name) do |file|
       while (line = file.gets)
- 	if line.match(/e/)
-          return line
-	end
+ 	return line if line.match(/e/)
       end
     end
     # Rewrite find_line using the file_sandwich library function.
