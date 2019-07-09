200a430e6ad47e381be3ac50d2eb7af7740c4e9a
Anton Stashevski <49589929+AntonStashevski@users.noreply.github.com>
2019-07-10 01:46:45 +0300

    Update about_arrays.rb

--- 3460/0/about_arrays.rb
+++ 3460/0/about_arrays.rb
@@ -1,14 +1,12 @@
-# frozen_string_literal: true
-
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-# class AboutArrays
 class AboutArrays < Neo::Koan
   def test_creating_arrays
     empty_array = []
     assert_equal Array, empty_array.class
     assert_equal 0, empty_array.size
   end
+  # :reek:TooManyStatements
 
   def test_array_literals
     array = []
@@ -23,10 +21,10 @@ class AboutArrays < Neo::Koan
     array << 333
     assert_equal [1, 2, 333], array
   end
+  # :reek:TooManyStatements
 
   def test_accessing_array_elements
     array = %i[peanut butter and jelly]
-
     assert_equal :peanut, array[0]
     assert_equal :peanut, array.first
     assert_equal :jelly, array[3]
@@ -34,6 +32,7 @@ class AboutArrays < Neo::Koan
     assert_equal :jelly, array[-1]
     assert_equal :butter, array[-3]
   end
+  # :reek:TooManyStatements
 
   def test_slicing_arrays
     array = %i[peanut butter and jelly]
@@ -61,6 +60,7 @@ class AboutArrays < Neo::Koan
     assert_equal %i[peanut butter], array[0...2]
     assert_equal %i[and jelly], array[2..-1]
   end
+  # :reek:TooManyStatements
 
   def test_pushing_and_popping_arrays
     array = [1, 2]
@@ -72,6 +72,7 @@ class AboutArrays < Neo::Koan
     assert_equal :last, popped_value
     assert_equal [1, 2], array
   end
+  # :reek:TooManyStatements
 
   def test_shifting_arrays
     array = [1, 2]
