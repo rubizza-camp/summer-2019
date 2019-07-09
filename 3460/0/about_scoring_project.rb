140bc528141e28fe5755f06d54724a16442a5b92
Anton Stashevski <49589929+AntonStashevski@users.noreply.github.com>
2019-07-09 17:29:08 +0300

    Update about_scoring_project.rb

--- 3460/0/about_scoring_project.rb
+++ 3460/0/about_scoring_project.rb
@@ -2,36 +2,6 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-# rubocop:disable Metrics/AbcSize
-# Greed is a dice game where you roll up to five dice to accumulate
-# points.  The following "score" function will be used to calculate the
-# score of a single roll of the dice.
-#
-# A greed roll is scored as follows:
-#
-# * A set of three ones is 1000 points
-#
-# * A set of three numbers (other than ones) is worth 100 times the
-#   number. (e.g. three fives is 500 points).
-#
-# * A one (that is not part of a set of three) is worth 100 points.
-#
-# * A five (that is not part of a set of three) is worth 50 points.
-#
-# * Everything else is worth 0 points.
-#
-#
-# Examples:
-#
-# score([1,1,1,5,1]) => 1150 points
-# score([2,3,4,6,2]) => 0 points
-# score([3,4,5,3,3]) => 350 points
-# score([1,5,1,2,4]) => 250 points
-#
-# More scoring examples are given in the tests below:
-#
-# Your goal is to write the score method.
-
 def score(dice)
   dice = dice.sort
   sum = 0
@@ -86,4 +56,3 @@ class AboutScoringProject < Neo::Koan
     assert_equal 1150, score([1, 1, 1, 5, 1])
   end
 end
-# rubocop:enable Metrics/AbcSize
