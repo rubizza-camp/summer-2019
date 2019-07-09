a8ec257c8bdcd87ce48cee5888be1778a98480c8
vasilro <mail@askolta.ru>
2019-07-09 17:56:35 +0300

    improve code

--- 13/0/about_classes.rb
+++ 13/0/about_classes.rb
@@ -12,7 +12,7 @@ class AboutClasses < Neo::Koan
   # ------------------------------------------------------------------
 
   class Dog2
-    def set_name(a_name)
+    def setname(a_name)
       @name = a_name
     end
   end
@@ -21,13 +21,13 @@ class AboutClasses < Neo::Koan
     fido = Dog2.new
     assert_equal [], fido.instance_variables
 
-    fido.set_name('Fido')
+    fido.setname('Fido')
     assert_equal [:@name], fido.instance_variables
   end
 
   def test_instance_variables_cannot_be_accessed_outside_the_class
     fido = Dog2.new
-    fido.set_name('Fido')
+    fido.setname('Fido')
 
     assert_raise(NoMethodError) do
       fido.name
@@ -41,14 +41,14 @@ class AboutClasses < Neo::Koan
 
   def test_you_can_politely_ask_for_instance_variable_values
     fido = Dog2.new
-    fido.set_name('Fido')
+    fido.setname('Fido')
 
     assert_equal 'Fido', fido.instance_variable_get('@name')
   end
 
   def test_you_can_rip_the_value_out_using_instance_eval
     fido = Dog2.new
-    fido.set_name('Fido')
+    fido.setname('Fido')
 
     assert_equal 'Fido', fido.instance_eval('@name')  # string version
     assert_equal 'Fido', fido.instance_eval { @name } # block version
@@ -57,7 +57,7 @@ class AboutClasses < Neo::Koan
   # ------------------------------------------------------------------
 
   class Dog3
-    def set_name(a_name)
+    def setname(a_name)
       @name = a_name
     end
 
@@ -66,7 +66,7 @@ class AboutClasses < Neo::Koan
 
   def test_you_can_create_accessor_methods_to_return_instance_variables
     fido = Dog3.new
-    fido.set_name('Fido')
+    fido.setname('Fido')
 
     assert_equal 'Fido', fido.name
   end
@@ -76,14 +76,14 @@ class AboutClasses < Neo::Koan
   class Dog4
     attr_reader :name
 
-    def set_name(a_name)
+    def setname(a_name)
       @name = a_name
     end
   end
 
   def test_attr_reader_will_automatically_define_an_accessor
     fido = Dog4.new
-    fido.set_name('Fido')
+    fido.setname('Fido')
 
     assert_equal 'Fido', fido.name
   end
