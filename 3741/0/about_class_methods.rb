fc7fd5d9d084e3bb28eed285747bf8f6fb48f437
anton rudy <36412453+middaysan@users.noreply.github.com>
2019-07-09 19:26:19 +0300

    Trailing whitespace delete

--- 3741/0/about_class_methods.rb
+++ 3741/0/about_class_methods.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # :reek:TooManyMethods
 # :reek:UncommunicativeMethodName
 # :reek:UncommunicativeModuleName
+
 class AboutClassMethods < Neo::Koan
   class Dog
   end
