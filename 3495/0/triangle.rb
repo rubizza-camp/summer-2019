a2ea8717cbbd1b32017d402858ad6f85781dd974
kartowkaateist <kartowkaateist@gmail.com>
2019-07-09 13:00:42 +0300

    Some fix in triangle.rb

--- 3495/0/triangle.rb
+++ 3495/0/triangle.rb
@@ -1,6 +1,6 @@
-# Triangle Project Code.
+# triangle Project Code.
 
-# Triangle analyzes the lengths of the sides of a triangle
+# triangle analyzes the lengths of the sides of a triangle
 # (represented by a, b and c) and returns the type of triangle.
 #
 # It returns:
@@ -14,38 +14,58 @@
 #   about_triangle_project_2.rb
 #
 
-def a_triangle?(side_a, side_b, side_c)
-  true if (side_a + side_b <= side_c) ||
-          (side_a + side_c <= side_b) || (side_c + side_b <= side_a)
-end
+class Triangle
+  attr_reader :side_a, :side_b, :side_c
 
-def a_triangle_2?(side_a, side_b, side_c)
-  true if side_a <= 0 || side_b <= 0 || side_c <= 0
-end
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
 
-def a_equilateral?(side_a, side_b, side_c)
-  true if side_a == side_b && side_b == side_c && side_a == side_c
-end
+  def type
+    if a_triangle?(self) || a_triangle_2?(self) || a_triangle_3?(self)
+      raise TriangleError, 'the triangle does not exist'
+    elsif a_equilateral?(self)
+      :equilateral
+    elsif a_isosceles?(self)
+      :isosceles
+    elsif a_scalene?(self)
+      :scalene
+    end
+  end
 
-def a_isosceles?(side_a, side_b, side_c)
-  true if side_a == side_b || side_b == side_c || side_a == side_c
-end
+  def a_triangle?(triangle)
+    true if (triangle.side_a + triangle.side_b <= triangle.side_c) ||
+            (triangle.side_a + triangle.side_c <= triangle.side_b)
+  end
 
-def a_scalene?(side_a, side_b, side_c)
-  true if side_a != side_b && side_b != side_c && side_a != side_c
+  def a_triangle_2?(triangle)
+    true if triangle.side_a <= 0 || triangle.side_b <= 0 || triangle.side_c <= 0
+  end
+
+  def a_triangle_3?(triangle)
+    true if triangle.side_c + triangle.side_b <= triangle.side_a
+  end
+
+  def a_equilateral?(triangle)
+    true if (triangle.side_a == triangle.side_b) && (triangle.side_b ==
+            triangle.side_c) && (triangle.side_a == triangle.side_c)
+  end
+
+  def a_isosceles?(triangle)
+    true if (triangle.side_a == triangle.side_b) || (triangle.side_b ==
+            triangle.side_c) || (triangle.side_a == triangle.side_c)
+  end
+
+  def a_scalene?(triangle)
+    true if (triangle.side_a != triangle.side_b) && (triangle.side_b !=
+            triangle.side_c) && (triangle.side_a != triangle.side_c)
+  end
 end
 
 def triangle(side_a, side_b, side_c)
-  if a_triangle?(side_a, side_b, side_c) ||
-     a_triangle_2?(side_a, side_b, side_c)
-    raise TriangleError, 'the triangle does not exist'
-  elsif a_equilateral?(side_a, side_b, side_c)
-    :equilateral
-  elsif a_isosceles?(side_a, side_b, side_c)
-    :isosceles
-  elsif a_scalene?(side_a, side_b, side_c)
-    :scalene
-  end
+  Triangle.new(side_a, side_b, side_c).type
 end
 
 # Error class used in part 2.  No need to change this code.
