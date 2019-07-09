a5b7f6a5961d675a50b84828480b87009fa3f19f
Andrew Berendeev <soligorsk162@gmail.com>
2019-07-09 15:46:55 +0300

    add zero error check

--- 3485/0/triangle.rb
+++ 3485/0/triangle.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Triangle Project Code.
-# rubocop:disable Performance/RedundantSortBy
+# rubocop:disable Performance/RedundantSortBy, Metrics/CyclomaticComplexity
 # Triangle analyzes the lengths of the sides of a triangle
 # (represented by a, b and c) and returns the type of triangle.
 #
@@ -20,6 +20,7 @@ def triangle(a_side, b_side, c_side)
   a_side, b_side, c_side = [a_side, b_side, c_side].sort_by { |obj| obj }
   raise TriangleError unless a_side + b_side > c_side
   raise TriangleError if a_side.negative?
+  raise TriangleError if a_side.zero?
   return :equilateral if a_side == c_side
   return :isosceles if a_side == b_side || b_side == c_side
 
@@ -29,4 +30,4 @@ end
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
-# rubocop:enable Performance/RedundantSortBy
+# rubocop:enable Performance/RedundantSortBy, Metrics/CyclomaticComplexity
