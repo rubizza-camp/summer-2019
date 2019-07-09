59c51a7b04b1d27cab8e6a5913bb4340fc479d4f
Anton Stashevski <49589929+AntonStashevski@users.noreply.github.com>
2019-07-10 01:40:16 +0300

    Update about_sandwich_code.rb

--- 3460/0/about_sandwich_code.rb
+++ 3460/0/about_sandwich_code.rb
@@ -1,30 +1,32 @@
-# rubocop:disable all
-# frozen_string_literal: true
-
 require File.expand_path(File.dirname(__FILE__) + '/neo')
-
-# class sandwich
+# :reek:RepeatedConditional
+# :reek:NilCheck
 class AboutSandwichCode < Neo::Koan
+  # :reek:UtilityFunction
+
   def count_lines(file_name)
-    file = open(file_name)
+    file = File.open(file_name)
     count = 0
     count += 1 while file.gets
     count
   ensure
-    file.close
+    file&.close if file
   end
 
   def test_counting_lines
     assert_equal 4, count_lines('example_file.txt')
   end
 
+  # ------------------------------------------------------------------
+  # :reek:UtilityFunction
+
   def find_line(file_name)
-    file = open(file_name)
-    while line = file.gets
+    file = File.open(file_name)
+    while (line = file.gets)
       return line if line =~ /e/
     end
   ensure
-    file.close
+    file&.close if file
   end
 
   def test_finding_lines
@@ -35,7 +37,7 @@ class AboutSandwichCode < Neo::Koan
   # THINK ABOUT IT:
   #
   # The count_lines and find_line are similar, and yet different.
-  # They both follow the pattern of 'sandwich code'.
+  # They both follow the pattern of "sandwich code".
   #
   # Sandwich code is code that comes in three parts: (1) the top slice
   # of bread, (2) the meat, and (3) the bottom slice of bread.  The
@@ -52,17 +54,18 @@ class AboutSandwichCode < Neo::Koan
   #
   # Consider the following code:
   #
+  # :reek:UtilityFunction
 
   def file_sandwich(file_name)
-    file = open(file_name)
+    file = File.open(file_name)
     yield(file)
   ensure
-    file.close
+    file&.close if file
   end
 
   # Now we write:
 
-  def count_lines2(file_name)
+  def count_lines_two(file_name)
     file_sandwich(file_name) do |file|
       count = 0
       count += 1 while file.gets
@@ -70,24 +73,29 @@ class AboutSandwichCode < Neo::Koan
     end
   end
 
-  def test_counting_lines2
-    assert_equal 4, count_lines2('example_file.txt')
+  def test_counting_lines_two
+    assert_equal 4, count_lines_two('example_file.txt')
   end
 
   # ------------------------------------------------------------------
 
-  def find_line2(file_name)
-    # Rewrite find_line using the file_sandwich library function.
+  def find_line_two(file_name)
+    file_sandwich(file_name) do |file|
+      while (line = file.gets)
+        return line if line =~ /e/
+      end
+    end
   end
 
-  def test_finding_lines2
-    assert_equal nil, find_line2('example_file.txt')
+  def test_finding_lines_two
+    assert_equal "test\n", find_line_two('example_file.txt')
   end
 
   # ------------------------------------------------------------------
+  # :reek:UtilityFunction
 
-  def count_lines3(file_name)
-    open(file_name) do |file|
+  def count_lines_three(file_name)
+    File.open(file_name) do |file|
       count = 0
       count += 1 while file.gets
       count
@@ -95,7 +103,6 @@ class AboutSandwichCode < Neo::Koan
   end
 
   def test_open_handles_the_file_sandwich_when_given_a_block
-    assert_equal 4, count_lines3('example_file.txt')
+    assert_equal 4, count_lines_three('example_file.txt')
   end
 end
-# rubocop:enable all
