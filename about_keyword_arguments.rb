efe2bf4345a3fc6104984fedf6f03d52769b7ddd
investor228 <podlessky.max@yandex.by>
2019-07-09 12:54:14 +0300

    Task 0

---
+++ about_keyword_arguments.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/LineLength
+
+require File.expand_path(File.dirname(__FILE__) + '/neo')
+# class
+class AboutKeywordArguments < Neo::Koan
+  def method_with_keyword_arguments(one: 1, two: 'two')
+    [one, two]
+  end
+
+  def test_keyword_arguments
+    assert_equal Array, method_with_keyword_arguments.class
+    assert_equal [1, 'two'], method_with_keyword_arguments
+    assert_equal %w[one two], method_with_keyword_arguments(one: 'one')
+    assert_equal [1, 2], method_with_keyword_arguments(two: 2)
+  end
+
+  def method_with_keyword_arguments_with_mandatory_argument(one, two: 2, three: 3)
+    [one, two, three]
+  end
+
+  def test_keyword_arguments_with_wrong_number_of_arguments
+    assert_raise(ArgumentError) do
+      method_with_keyword_arguments_with_mandatory_argument
+    end
+    # assert_match(/ wrong number of arguments (0 for 1) /, exception.message)
+  end
+  # rubocop:enable Metrics/LineLength
+  # THINK ABOUT IT:
+  #
+  # Keyword arguments always have a default value, making them optional
+end
