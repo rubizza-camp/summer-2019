01dd08fddffb0444ebe5ab1b621a8eec541ca33e
Anton Stashevski <49589929+AntonStashevski@users.noreply.github.com>
2019-07-10 01:45:48 +0300

    Update about_blocks.rb

--- 3460/0/about_blocks.rb
+++ 3460/0/about_blocks.rb
@@ -1,6 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-#:nodoc:
 class AboutBlocks < Neo::Koan
   def method_with_block
     result = yield
@@ -55,10 +54,8 @@ class AboutBlocks < Neo::Koan
   end
 
   def test_methods_can_see_if_they_have_been_called_with_a_block
-    # rubocop: disable Lint/AmbiguousBlockAssociation
-    assert_equal :with_block, yield_tester { :with_block }
+    assert_equal :with_block, (yield_tester { :with_block })
     assert_equal :no_block, yield_tester
-    # rubocop: enable Lint/AmbiguousBlockAssociation
   end
 
   # ------------------------------------------------------------------
@@ -70,7 +67,7 @@ class AboutBlocks < Neo::Koan
   end
 
   def test_blocks_can_be_assigned_to_variables_and_called_explicitly
-    add_one = ->(n) { n + 1 }
+    add_one = ->(number) { number + 1 }
     assert_equal 11, add_one.call(10)
 
     # Alternative calling syntax
@@ -78,25 +75,23 @@ class AboutBlocks < Neo::Koan
   end
 
   def test_stand_alone_blocks_can_be_passed_to_methods_expecting_blocks
-    make_upper = ->(n) { n.upcase }
+    make_upper = ->(string) { string.upcase }
     result = method_with_block_arguments(&make_upper)
     assert_equal 'JIM', result
   end
 
   # ------------------------------------------------------------------
-
+  # rubocop:disable Performance/RedundantBlockCall
+  # :reek:UtilityFunction
   def method_with_explicit_block(&block)
-    # rubocop: disable Performance/RedundantBlockCall
     block.call(10)
-    # rubocop: enable Performance/RedundantBlockCall
   end
+  # rubocop:enable Performance/RedundantBlockCall
 
   def test_methods_can_take_an_explicit_block_argument
-    # rubocop: disable Lint/AmbiguousBlockAssociation
-    assert_equal 20, method_with_explicit_block { |n| n * 2 }
-
-    add_one = ->(n) { n + 1 }
+    assert_equal 20, (method_with_explicit_block { |number| number * 2 })
+    add_one = ->(number) { number + 1 }
     assert_equal 11, method_with_explicit_block(&add_one)
-    # rubocop: enable Lint/AmbiguousBlockAssociation
   end
 end
+
