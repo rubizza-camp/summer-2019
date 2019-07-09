e43b2c7027552cf21d55f1f6c55c2bee7e39123f
MargaretOm <margosha20617@gmail.com>
2019-07-09 12:12:20 +0200

    Updated file with new version code, used OOP

--- 3604/0/triangle.rb
+++ 3604/0/triangle.rb
@@ -13,25 +13,37 @@
 # and
 #   about_triangle_project_2.rb
 #
-# :reek:FeatureEnvy
 
-def test_error(val_a, val_b, val_c)
-  raise TriangleError if val_a <= 0 || val_b <= 0 || val_c <= 0
-  sum = (val_a + val_b + val_c) / 2.0
-  raise TriangleError if ((sum - val_a) * (sum - val_b) * (sum - val_c)) <= 0
-end
-# :reek:FeatureEnvy
+class Triangle
+  def initialize(val_a, val_b, val_c)
+    @val_a = val_a
+    @val_b = val_b
+    @val_c = val_c
+  end
 
-def triangle(val_a, val_b, val_c)
-  test_error(val_a, val_b, val_c)
-  if val_a == val_b && val_a == val_c
-    :equilateral
-  elsif val_a == val_b || val_a == val_c || val_b == val_c
-    :isosceles
-  else
-    :scalene
+  def test_error
+    raise TriangleError if @val_a <= 0 || @val_b <= 0 || @val_c <= 0
+    sum = (@val_a + @val_b + @val_c) / 2.0
+    raise TriangleError if ((sum - @val_a) * (sum - @val_b) * (sum - @val_c)) <= 0
+  end
+
+  def triangle
+    test_error
+    if @val_a == @val_b && @val_a == @val_c
+      :equilateral
+    elsif @val_a == @val_b || @val_a == @val_c || @val_b == @val_c
+      :isosceles
+    else
+      :scalene
+    end
   end
 end
+# :reek:UtilityFunction
+
+def triangle(val_a, val_b, val_c)
+  Triangle.new(val_a, val_b, val_c).triangle
+end
+# :reek:UtilityFunction
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
