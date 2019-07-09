aa9f8ef793489cef67d12eca38edc1dd96d1cf5e
Andrew Berendeev <soligorsk162@gmail.com>
2019-07-09 11:03:54 +0300

    fix triangle

--- 3485/0/triangle.rb
+++ 3485/0/triangle.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Triangle Project Code.
-
+# rubocop:disable Performance/RedundantSortBy
 # Triangle analyzes the lengths of the sides of a triangle
 # (represented by a, b and c) and returns the type of triangle.
 #
@@ -16,10 +16,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a_side, b_side, c_side)
-  a_side, b_side, c_side = [a_side, b_side, c_side].sort
+  # in wiki is written is it better sort_by
+  a_side, b_side, c_side = [a_side, b_side, c_side].sort_by { |obj| obj }
 
-  raise TriangleError unless a_side.positive?
-
+  raise TriangleError if a_side.negative?
   raise TriangleError unless a_side + b_side > c_side
 
   return :equilateral if a_side == c_side
@@ -32,3 +32,4 @@ end
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
+# rubocop:enable Performance/RedundantSortBy
