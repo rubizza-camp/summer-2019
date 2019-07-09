bce0cdd29810df08a6e60eabf614fdef23cf7c85
Pavel Zhachko <rammiel@yandex.ru>
2019-07-09 21:25:06 +0300

    Disable Style/SafeNavigation

--- 3520/0/about_sandwich_code.rb
+++ 3520/0/about_sandwich_code.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Style/SafeNavigation
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 # This class smells of :reek:UncommunicativeMethodName and :reek:RepeatedConditional
 # This class smells of :reek:UtilityFunction
@@ -102,3 +103,4 @@ class AboutSandwichCode < Neo::Koan
     assert_equal 4, count_lines3('example_file.txt')
   end
 end
+# rubocop:enable Style/SafeNavigation
