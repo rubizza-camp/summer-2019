bd00fd90c6dabcc5f754198eb4031883b33584a8
Elena Parchevskaya <elena.lemoon@gmail.com>
2019-07-09 11:45:42 +0300

    Fix for hound

--- 3512/3512/about_scope.rb
+++ 3512/3512/about_scope.rb
@@ -22,6 +22,7 @@ class AboutScope < Neo::Koan
       Dog.new
     end
   end
+  # :reek:TooManyStatements
 
   def test_you_can_reference_nested_classes_using_the_scope_operator
     fido = Jims::Dog.new
@@ -74,6 +75,6 @@ class AboutScope < Neo::Koan
 
   def test_you_can_get_a_list_of_constants_for_any_class_or_module
     assert_equal [:Dog], Jims.constants
-    assert Object.constants.size > 1
+    assert !Object.constants.empty?
   end
 end
