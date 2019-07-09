3943dbc9b4a6744ee016734be26c51e4207639d3
mmmalikov <unitcircle@protonmail.comunitcircle@protonmail.comunitcircle@protonmail.com>
2019-07-09 15:03:37 +0300

    all koans work (i hope)

---
+++ 380/0/ruby_koans/triangle.rb
@@ -0,0 +1,29 @@
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+#
+def triangle(a_side, b_side, c_side)
+  raise TriangleError if a_side.equal?(0) && a_side.equal?(0) && a_side.equal?(0)
+  raise TriangleError if a_side < 0 || b_side < 0 || c_side < 0
+  raise TriangleError if (a_side + b_side <= c_side) || (a_side + c_side <= b_side) || (b_side + c_side <= a_side)
+
+  return :equilateral if a_side.equal?(b_side) && a_side.equal?(c_side)
+  return :isosceles if a_side.equal?(b_side) || a_side.equal?(c_side) || b_side.equal?(c_side)
+
+  :scalene
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
