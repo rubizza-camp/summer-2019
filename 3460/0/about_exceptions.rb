f2a33403e32ae95bef7eb2d993c0de3e6286de73
Anton Stashevski <49589929+AntonStashevski@users.noreply.github.com>
2019-07-10 02:14:17 +0300

    Update about_exceptions.rb

--- 3460/0/about_exceptions.rb
+++ 3460/0/about_exceptions.rb
@@ -1,12 +1,5 @@
-# frozen_string_literal: true
-
-# rubocop: disable Metrics/MethodLength
-# rubocop: disable Lint/UselessAssignment
-# rubocop: disable Lint/HandleExceptions
-
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-# class AboutExceptions < Neo::Koan
 class AboutExceptions < Neo::Koan
   class MySpecialError < RuntimeError
   end
@@ -17,60 +10,68 @@ class AboutExceptions < Neo::Koan
     assert_equal Exception, MySpecialError.ancestors[3]
     assert_equal Object, MySpecialError.ancestors[4]
   end
+  # rubocop:disable Metrics/MethodLength
+  # :reek:TooManyStatements
 
   def test_rescue_clause
     result = nil
     begin
-      raise 'Oops'
-    rescue StandardError => eeee
+      # rubocop:disable Style/SignalException
+      fail 'Oops'
+      # rubocop:enable Style/SignalException
+    rescue StandardError => ex
       result = :exception_handled
     end
 
     assert_equal :exception_handled, result
 
-    assert_equal true, exxx.is_a?(StandardError), 'Should be a Standard Error'
-    assert_equal true, exxx.is_a?(RuntimeError),  'Should be a Runtime Error'
-
-    assert RuntimeError.ancestors.include?(StandardError),
-           'RuntimeError is a subclass of StandardError'
-
-    assert_equal 'Oops', exxx.message
+    assert_equal true, ex.is_a?(StandardError), 'Should be a Standard Error'
+    assert_equal true, ex.is_a?(RuntimeError),  'Should be a Runtime Error'
+    # rubocop:disable Metrics/LineLength
+    assert RuntimeError.ancestors.include?(StandardError), 'RuntimeError is a subclass of StandardError'
+    # rubocop:enable Metrics/LineLength
+    assert_equal 'Oops', ex.message
   end
+  # :reek:TooManyStatements
 
   def test_raising_a_particular_error
     result = nil
     begin
       # 'raise' and 'fail' are synonyms
       raise MySpecialError, 'My Message'
-    rescue MySpecialError => eeee
+    rescue MySpecialError => ex
       result = :exception_handled
     end
 
     assert_equal :exception_handled, result
-    assert_equal 'My Message', exxx.message
+    assert_equal 'My Message', ex.message
   end
+  # rubocop:enable Metrics/MethodLength
 
   def test_ensure_clause
+    # rubocop:disable Lint/UselessAssignment
     result = nil
+    # rubocop:enable Lint/UselessAssignment
     begin
-      raise 'Oops'
+      # rubocop:disable Style/SignalException
+      fail 'Oops'
+    # rubocop:enable Style/SignalException
+    # rubocop:disable Lint/HandleExceptions
     rescue StandardError
       # no code here
     ensure
       result = :always_run
     end
-
+    # rubocop:enable Lint/HandleExceptions
     assert_equal :always_run, result
   end
 
   # Sometimes, we must know about the unknown
   def test_asserting_an_error_is_raised
-    # A do-end is a block, a topic to explore more later
+    # rubocop:disable Style/RaiseArgs
     assert_raise(MySpecialError) do
-      raise MySpecialError, 'New instances can be raised directly.'
+      raise MySpecialError.new('New instances can be raised directly.')
     end
+    # rubocop:enable Style/RaiseArgs
   end
 end
-# rubocop: enable Metrics/MethodLength
-# rubocop: enable Lint/UselessAssignment
-# rubocop: enable Lint/HandleExceptions
