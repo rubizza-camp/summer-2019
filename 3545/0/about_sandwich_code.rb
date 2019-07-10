e1ce95ea7787b37e38eabbd51fb71ee06fda41d1
nikitamasalkov <nikitamasalkov.tang@gmail.com>
2019-07-10 20:58:15 +0300

    check

--- 3545/0/about_sandwich_code.rb
+++ 3545/0/about_sandwich_code.rb
@@ -1,12 +1,13 @@
-require File.expand_path(File.dirname(__FILE__) + '/neo')
-  # :reek:RepeatedConditionalClear
-  # :reek:NilCheck
-  # rubocop:disable Security/Open
+# rubocop:disable Security/Open
 
+require File.expand_path(File.dirname(__FILE__) + '/neo')
+# :reek:UtilityFunction:UncommunicativeMethodName:
+# :reek:NilCheck
+# :reek:UncommunicativeMethodName
+# :reek:RepeatedConditional
 class AboutSandwichCode < Neo::Koan
-  # :reek:UtilityFunction
   def count_lines(file_name)
-    file = File.open(file_name)
+    file = open(file_name)
     count = 0
     count += 1 while file.gets
     count
@@ -17,7 +18,8 @@ class AboutSandwichCode < Neo::Koan
   def test_counting_lines
     assert_equal 4, count_lines('example_file.txt')
   end
-  # :reek:UtilityFunction
+
+  # ------------------------------------------------------------------
 
   def find_line(file_name)
     file = open(file_name)
@@ -32,15 +34,36 @@ class AboutSandwichCode < Neo::Koan
     assert_equal "test\n", find_line('example_file.txt')
   end
 
+  # ------------------------------------------------------------------
+  # THINK ABOUT IT:
+  #
+  # The count_lines and find_line are similar, and yet different.
+  # They both follow the pattern of "sandwich code".
+  #
+  # Sandwich code is code that comes in three parts: (1) the top slice
+  # of bread, (2) the meat, and (3) the bottom slice of bread.  The
+  # bread part of the sandwich almost always goes together, but
+  # the meat part changes all the time.
+  #
+  # Because the changing part of the sandwich code is in the middle,
+  # abstracting the top and bottom bread slices to a library can be
+  # difficult in many languages.
+  #
+  # (Aside for C++ programmers: The idiom of capturing allocated
+  # pointers in a smart pointer constructor is an attempt to deal with
+  # the problem of sandwich code for resource allocation.)
+  #
+  # Consider the following code:
+  #
+
   def file_sandwich(file_name)
-    file = File.open(file_name)
+    file = open(file_name)
     yield(file)
   ensure
     file&.close if file
   end
 
   # Now we write:
-
   def count_lines2(file_name)
     file_sandwich(file_name) do |file|
       count = 0
@@ -52,6 +75,8 @@ class AboutSandwichCode < Neo::Koan
   def test_counting_lines2
     assert_equal 4, count_lines2('example_file.txt')
   end
+
+  # ------------------------------------------------------------------
 
   def find_line2(file_name)
     # Rewrite find_line using the file_sandwich library function.
@@ -65,7 +90,8 @@ class AboutSandwichCode < Neo::Koan
   def test_finding_lines2
     assert_equal "test\n", find_line2('example_file.txt')
   end
-  # :reek:UtilityFunction
+
+  # ------------------------------------------------------------------
 
   def count_lines3(file_name)
     open(file_name) do |file|
@@ -79,4 +105,4 @@ class AboutSandwichCode < Neo::Koan
     assert_equal 4, count_lines3('example_file.txt')
   end
 end
-# rubocop:disable Security/Open
+# rubocop:enable Security/Open
