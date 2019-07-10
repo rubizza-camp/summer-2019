ae6bd9f520968300565655ed734d4efd60a709f6
Sufl0wer <fr08ty@gmail.com>
2019-07-10 14:10:49 +0300

    in sandwich_code =~ fix

--- 3750/0/about_sandwich_code.rb
+++ 3750/0/about_sandwich_code.rb
@@ -26,7 +26,7 @@ class AboutSandwichCode < Neo::Koan
   def find_line(file_name)
     file = File.open(file_name)
     while (line = file.gets)
-      return line if line.match(/e/)
+      return line if line =~ /e/
     end
   ensure
     file&.close if file
@@ -83,7 +83,7 @@ class AboutSandwichCode < Neo::Koan
   def find_line2(file_name)
     file_sandwich(file_name) do |file|
       while (line = file.gets)
-        return line if line.match(/e/)
+        return line if line =~ /e/
       end
     end
     # Rewrite find_line using the file_sandwich library function.
