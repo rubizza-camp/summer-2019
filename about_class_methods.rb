4e59cdece66016c7d60dcd59fc7ed981b410820a
investor228 <podlessky.max@yandex.by>
2019-07-10 13:39:01 +0300

    def

--- about_class_methods.rb
+++ about_class_methods.rb
@@ -1,4 +1,4 @@
-# rubocop:disable Layout/EndAlignment, Lint/MissingCopEnableDirective, Metrics/LineLength
+# rubocop:disable Layout/EndAlignment, Lint/MissingCopEnableDirective, 
 
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 # Class
