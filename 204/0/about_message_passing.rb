778d88261822ffcd7ef6b87f8cbe1044fd80d37a
Artur <rutraniloms@icloud.com>
2019-07-10 13:19:44 +0300

    fix

--- 204/0/about_message_passing.rb
+++ 204/0/about_message_passing.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 # rubocop:disable Style/MethodMissing
 >>>>>>> squash ruby koans + reek
+=======
+>>>>>>> fix
 # rubocop:disable Lint/UnneededCopDisableDirective, Style/MissingRespondToMissing
 
 require File.expand_path(File.dirname(__FILE__) + '/neo')
@@ -164,9 +167,13 @@ class AboutMessagePassing < Neo::Koan
   end
   # rubocop: enable Style/MethodMissing
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> squash ruby koans + reek
+=======
+
+>>>>>>> fix
   def test_foo_method_are_caught
     catcher = WellBehavedFooCatcher.new
 
@@ -202,7 +209,10 @@ class AboutMessagePassing < Neo::Koan
 end
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 # rubocop:enable Style/MethodMissing
 >>>>>>> squash ruby koans + reek
+=======
+>>>>>>> fix
 # rubocop:enable Lint/UnneededCopDisableDirective, Style/MissingRespondToMissing
