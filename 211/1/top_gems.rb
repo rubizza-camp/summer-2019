4d57074b32444fab37011b282824475911f4f476
iris-the-fox <iris-miz@yandex.ru>
2019-07-10 20:41:31 +0300

    Add YAML

--- 211/1/top_gems.rb
+++ 211/1/top_gems.rb
@@ -1,13 +1,14 @@
+require 'yaml'
 require 'httparty'
 require 'nokogiri'
 
 class GemPopularity
   include HTTParty
   base_uri 'https://github.com/'
-  attr_accessor :used, :name, :watch , :star, :fork
+  attr_accessor :used, :name, :watch, :star, :fork
 
   def initialize(gem_name)
-  	@name = gem_name
+    @name = gem_name
     page = HTTParty.get("https://github.com/#{gem_name}/#{gem_name}")
     doc = Nokogiri::HTML(page.body)
     @watch = doc.css('a.social-count')[0].text.strip
@@ -16,7 +17,12 @@ class GemPopularity
   end
 end
 
-sinatra = GemPopularity.new('sinatra')
+gems_array = YAML.safe_load(File.read('gem_list.yml'))
+gems_array['gems'].each do |gem_n|
+  # puts gem_n.class
+  gem_name = GemPopularity.new(gem_n)
+  puts "#{gem_name.name} | watched by #{gem_name.watch} | #{gem_name.star} stars | #{gem_name.fork} forks "
+end
+# sinatra = GemPopularity.new('sinatra')
 
-
-puts "#{sinatra.name} | watched by #{sinatra.watch} | #{sinatra.star} stars | #{sinatra.fork} forks "
+# puts "#{sinatra.name} | watched by #{sinatra.watch} | #{sinatra.star} stars | #{sinatra.fork} forks "
