7bb841225ba7e38716ef4f43f8e117c5a8549a07
Artur <rutraniloms@icloud.com>
2019-07-10 13:59:44 +0300

    Revert "fix about_class_methods"
    This reverts commit 6f348491145b9f1b05b129ff1c2ec443209cb198.

--- 204/0/about_class_methods.rb
+++ 204/0/about_class_methods.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 require File.expand_path(File.dirname(__FILE__) + '/neo')
+
+# :reek:Attribute
+# :reek:UncommunicativeMethodName
 # :reek:TooManyMethods
 class AboutClassMethods < Neo::Koan
   class Dog; end
@@ -16,6 +21,7 @@ class AboutClassMethods < Neo::Koan
     assert_equal true, Dog.is_a?(Object)
   end
 
+  # :reek:FeatureEnvy
   def test_objects_have_methods
     fido = Dog.new
     assert fido.methods.any? == true
@@ -36,6 +42,7 @@ class AboutClassMethods < Neo::Koan
   def test_other_objects_are_not_affected_by_these_singleton_methods
     fido = Dog.new
     rover = Dog.new
+
     def fido.wag
       :fidos_wag
     end
@@ -47,26 +54,36 @@ class AboutClassMethods < Neo::Koan
 
   # ------------------------------------------------------------------
 <<<<<<< HEAD
+<<<<<<< HEAD
   # :reek:UncommunicativeModuleName
   class Dachshund
 =======
 
   class DogSecond
 >>>>>>> fix about_class_methods
+=======
+  # :reek:UncommunicativeModuleName
+  class Dog2
+>>>>>>> Revert "fix about_class_methods"
     def wag
       :instance_level_wag
     end
   end
 
 <<<<<<< HEAD
+<<<<<<< HEAD
   def Dachshund.wag
 =======
   def DogSecond.wag
 >>>>>>> fix about_class_methods
+=======
+  def Dog2.wag
+>>>>>>> Revert "fix about_class_methods"
     :class_level_wag
   end
 
   def test_since_classes_are_objects_you_can_define_singleton_methods_on_them_too
+<<<<<<< HEAD
 <<<<<<< HEAD
     assert_equal :class_level_wag, Dachshund.wag
   end
@@ -77,26 +94,26 @@ class AboutClassMethods < Neo::Koan
     assert_equal :class_level_wag, Dachshund.wag
 =======
     assert_equal :class_level_wag, DogSecond.wag
+=======
+    assert_equal :class_level_wag, Dog2.wag
+>>>>>>> Revert "fix about_class_methods"
   end
 
   def test_class_methods_are_independent_of_instance_methods
-    fido = DogSecond.new
+    fido = Dog2.new
     assert_equal :instance_level_wag, fido.wag
+<<<<<<< HEAD
     assert_equal :class_level_wag, DogSecond.wag
 >>>>>>> fix about_class_methods
+=======
+    assert_equal :class_level_wag, Dog2.wag
+>>>>>>> Revert "fix about_class_methods"
   end
 
   # ------------------------------------------------------------------
-
   class Dog
-  # :reek:Attribute
-    attr_accessor :name
+    attr_reader :name
   end
-  # rubocop:disable Style/TrivialAccessors
-  def Dog.name
-    name
-  end
-  # rubocop:enable Style/TrivialAccessors
 
   def test_classes_and_instances_do_not_share_instance_variables
     fido = Dog.new
@@ -118,10 +135,10 @@ class AboutClassMethods < Neo::Koan
   end
 
   # ------------------------------------------------------------------
-  # rubocop:disable Naming/ConstantName
+
   LAST_EXPRESSION = class Dog
-                                     21
-                                   end
+                      21
+                    end
 
   def test_class_statements_return_the_value_of_their_last_expression
     assert_equal 21, LAST_EXPRESSION
@@ -130,9 +147,9 @@ class AboutClassMethods < Neo::Koan
   # ------------------------------------------------------------------
 
   SELF_INSIDE_OF_CLASS_STATEMENT = class Dog
-                                 self
-                               end
-  # rubocop:enable Naming/ConstantName
+                                     self
+                                   end
+
   def test_self_while_inside_class_is_class_object_not_instance
     assert_equal true, Dog == SELF_INSIDE_OF_CLASS_STATEMENT
   end
