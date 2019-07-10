3a3fea6afe8220076a7f1d02bc6f996d9d5433a7
Nikivi <nikivieve@gmail.com>
2019-07-10 21:21:50 +0700

    some changes for reek

--- 3729/0/about_methods.rb
+++ 3729/0/about_methods.rb
@@ -1,9 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
+# reek: UtilityFunction
 def my_global_method(variable_a, variable_b)
   variable_a + variable_b
 end
 
+# reek: TooManyMethods
 class AboutMethods < Neo::Koan
   def test_calling_global_methods
     assert_equal 5, my_global_method(2, 3)
@@ -31,6 +33,8 @@ class AboutMethods < Neo::Koan
 
   # NOTE: wrong number of arguments is not a SYNTAX error, but a
   # runtime error.
+
+  # reek: TooManyStatements
   def test_calling_global_methods_with_wrong_number_of_arguments
     exception = assert_raise(ArgumentError) do
       my_global_method
@@ -91,7 +95,7 @@ class AboutMethods < Neo::Koan
   end
 
   # ------------------------------------------------------------------
-
+  # reek: UtilityFunction
   def my_method_in_the_same_class(variable_a, variable_b)
     variable_a * variable_b
   end
