f1ef75738fdbaa93eed543b7d9b6b053380d283c
Sufl0wer <fr08ty@gmail.com>
2019-07-10 10:58:25 +0300

    fix

--- 3750/0/about_control_statements.rb
+++ 3750/0/about_control_statements.rb
@@ -1,86 +1,67 @@
+# rubocop:disable Lint/MissingCopEnableDirective, Lint/LiteralAsCondition, Style/For
+
 require File.expand_path(File.dirname(__FILE__) + '/neo')
-# rubocop:disable all
+# :reek:FeatureEnvy
+# :reek:TooManyStatements
+# :reek:UncommunicativeVariableName
+# :reek:RepeatedConditional
+
 class AboutControlStatements < Neo::Koan
-# rubocop:disable Lint/LiteralAsCondition
   def test_if_then_else_statements
-    if true
-      # rubocop:enable all
-      result = :true_value
-    else
-      result = :false_value
-    end
+    result = true ? :true_value : :false_value
     assert_equal :true_value, result
   end
 
   def test_if_then_statements
     result = :default_value
-    # rubocop:disable Lint/LiteralAsCondition
-    if true
-      # rubocop:enable Lint/LiteralAsCondition
-      result = :true_value
-    end
+    result = :true_value if true
     assert_equal :true_value, result
   end
-  # rubocop:disable all
+
   def test_if_statements_return_values
-    value = if true
-              # rubocop:enable all
-              :true_value
-            else
-              :false_value
-            end
+    value = true ? :true_value : :false_value
     assert_equal :true_value, value
-    value = if false
-              :true_value
-            else
-              :false_value
-            end
+
+    value = false ? :true_value : :false_value
     assert_equal :false_value, value
+
     # NOTE: Actually, EVERY statement in Ruby will return a value, not
     # just if statements.
   end
 
   def test_if_statements_with_no_else_with_false_condition_return_value
-    value = if false
-              :true_value
-            end
+    value = :true_value if false
     assert_equal nil, value
   end
 
   def test_condition_operators
-    # rubocop:disable Lint/LiteralAsCondition
     assert_equal :true_value, (true ? :true_value : :false_value)
     assert_equal :false_value, (false ? :true_value : :false_value)
-    # rubocop:enable Lint/LiteralAsCondition
   end
 
   def test_if_statement_modifiers
     result = :default_value
     result = :true_value if true
+
     assert_equal :true_value, result
   end
 
   def test_unless_statement
     result = :default_value
-    # rubocop:disable Lint/LiteralAsCondition
-    result = :false_value unless false 
-    # rubocop:enable Lint/LiteralAsCondition
+    result = :false_value unless false # same as saying 'if !false', which evaluates as 'if true'
     assert_equal :false_value, result
   end
 
   def test_unless_statement_evaluate_true
     result = :default_value
-    # rubocop:disable Lint/LiteralAsCondition
-    result = :true_value unless true 
-    # rubocop:enable Lint/LiteralAsCondition
+    result = :true_value unless true # same as saying 'if !true', which evaluates as 'if false'
     assert_equal :default_value, result
   end
 
   def test_unless_statement_modifier
     result = :default_value
-    # rubocop:disable Lint/LiteralAsCondition
     result = :false_value unless false
-    # rubocop:enable Lint/LiteralAsCondition
+
     assert_equal :false_value, result
   end
 
@@ -88,12 +69,10 @@ class AboutControlStatements < Neo::Koan
     i = 1
     result = 1
     while i <= 10
-      result *= result
+      result *= i
       i += 1
     end
-    # rubocop:disable all
-    assert_equal 1, result
-    # rubocop:enable all
+    assert_equal 3_628_800, result
   end
 
   def test_break_statement
@@ -101,28 +80,32 @@ class AboutControlStatements < Neo::Koan
     result = 1
     loop do
       break unless i <= 10
-      result *= result
+
+      result *= i
       i += 1
     end
-    assert_equal 1, result
+    assert_equal 3_628_800, result
   end
 
   def test_break_statement_returns_values
     i = 1
     result = while i <= 10
-               break i if i.even?
+               break i if (i % 2).zero?
+
                i += 1
              end
-
     assert_equal 2, result
   end
+
+  # Interesting
 
   def test_next_statement
     i = 0
     result = []
     while i < 10
       i += 1
-      next if i.even?
+      next if (i % 2).zero?
+
       result << i
     end
     assert_equal [1, 3, 5, 7, 9], result
@@ -131,7 +114,7 @@ class AboutControlStatements < Neo::Koan
   def test_for_statement
     array = %w[fish and chips]
     result = []
-    array.each do |item|
+    for item in array
       result << item.upcase
     end
     assert_equal %w[FISH AND CHIPS], result
@@ -144,5 +127,4 @@ class AboutControlStatements < Neo::Koan
     end
     assert_equal 10, sum
   end
-  # rubocop:enable Lint/LiteralAsCondition
 end
