692b8c28639c44e9c1e20b9e4eb188949237be26
Maxxx911 <maxrikal@gmail.com>
2019-07-11 22:57:44 +0300

    added function checking arguments command line

--- 3493/1/top_gems.rb
+++ 3493/1/top_gems.rb
@@ -13,7 +13,16 @@ def parse_yml_file
 end
 
 def find_repo_html_url(name)
-  response_body_hash(URI.parse("https://api.github.com/search/repositories?q=#{name}&sort=stars&order=desc"))[:items][0][:html_url]
+begin
+  uri = URI.parse("https://api.github.com/search/repositories?q=#{name}&sort=stars&order=desc")
+  a = response_body_hash(uri)
+  b = a[:items]
+  c = b[0]
+  d= c[:html_url]
+  # sleep(1)
+  rescue NoMethodError
+  require'pry';binding.pry;
+end
 end
 
 def find_fields(url)
@@ -36,7 +45,7 @@ def response_body_hash url
   Net::HTTP.start(url.host, url.port,
   :use_ssl => url.scheme == 'https') do |http|
     request = Net::HTTP::Get.new url
-    response = http.request request
+    response = http.request(request)
     return JSON.parse(response.body, symbolize_names: true)
   end
 end
@@ -51,22 +60,58 @@ def show_table(args)
   puts table
 end
 
-def main(*args)
-  array= []
-  parsed_file = parse_yml_file
-  complited = 0
-  print "Complited status #{complited}/#{parsed_file.count}"
-  parsed_file.each do |gem| 
-    complited += 1
-    print "\rComplited status----------------------------------------------------------------#{complited}/#{parsed_file.count}"
-    print "\n" if complited == parsed_file.count
-    gem_object = Gems.new(gem)
-    gem_object.url = find_repo_html_url(gem)
-    gem_object.set_fields(find_fields(gem_object.url))
-    gem_object.count_used_by = find_used_by(gem_object.url)
-    array.push(gem_object)
+def pars_gems()
+  begin
+    array= []
+    parsed_file = parse_yml_file
+    complited = 0
+    print "Complited status #{complited}/#{parsed_file.count}"
+    parsed_file.each do |gem| 
+      complited += 1
+      print "\rComplited status----------------------------------------------------------------#{complited}/#{parsed_file.count}"
+      print "\n" if complited == parsed_file.count
+      # require 'pry'; binding.pry;
+      gem_object = Gems.new(gem)
+      gem_object.url = find_repo_html_url(gem)
+      gem_object.set_fields(find_fields(gem_object.url))
+      gem_object.count_used_by = find_used_by(gem_object.url)
+      array.push(gem_object)
+    end
+    array
+    # show_table(array.each_slice(1).to_a)
+    rescue :NoMethodErrors 
+    require 'pry';binding.pry
   end
-  show_table(array.each_slice(1).to_a)
 end
 
-main(1)
+def choose_top_gem(gems)
+  p gems
+end
+
+def main()
+  # require 'pry'; binding.pry;
+  ARGV.each do |argumet|
+    case argumet
+      when /--top/
+      argumet.split('=')[1]
+      choose_top_gem(pars_gems) 
+    end
+  end
+  # array= []
+  # parsed_file = parse_yml_file
+  # complited = 0
+  # print "Complited status #{complited}/#{parsed_file.count}"
+  # parsed_file.each do |gem| 
+  #   complited += 1
+  #   print "\rComplited status----------------------------------------------------------------#{complited}/#{parsed_file.count}"
+  #   print "\n" if complited == parsed_file.count
+  #   gem_object = Gems.new(gem)
+  #   gem_object.url = find_repo_html_url(gem)
+  #   gem_object.set_fields(find_fields(gem_object.url))
+  #   gem_object.count_used_by = find_used_by(gem_object.url)
+  #   array.push(gem_object)
+  # end
+  # show_table(array.each_slice(1).to_a)
+end
+
+main
