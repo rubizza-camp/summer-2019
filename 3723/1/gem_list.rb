3facc42f7cfc599ef53678b881fe45b2e2352afb
Hawat <gmhawat@gmail.com>
2019-07-11 14:27:34 +0300

    Adding gem presence

--- 3723/1/gem_list.rb
+++ 3723/1/gem_list.rb
@@ -1,24 +1,37 @@
+require 'rubygems'
+require 'terminal-table'
 require 'json'
 require 'yaml'
 require 'faraday'
+require 'nokogiri'
+require 'open-uri'
 
 list = YAML.load_file('ruby_gems.yml')
 
+def gem_presence(html)
+  #page = Nokogiri::HTML.parse(open(html))  
+  #page.css('body').text.include?('gem install')
+end
+
 def take_repo(list)
   list.dig('gems').map do |info|
-    response = Faraday.get "https://rubygems.org/api/v1/gems/#{info}.json"
+    response = Faraday.get "https://api.github.com/search/repositories?q=#{info}"
     data = JSON.parse(response.body)
-    data['source_code_uri'].sub!('http:', 'https:')
-    data['source_code_uri'].sub!('/github.com/', '/api.github.com/repos/')
+    repo = data['items'][0]['full_name']
+    for_presence = "https://raw.githubusercontent.com/#{repo}/master/README.md"
+    "https://api.github.com/repos/#{repo}"
   end
 end
 
 def gem_stats(list)
+  rows = []
   take_repo(list).map do |repo|
     response  = Faraday.get "#{repo}"
     data = JSON.parse(response.body)
+    rows << [data['name'], "watched by #{data['watchers_count']}", "#{data['stargazers_count']} stars", "forks #{data['forks_count']}", "issues #{data['open_issues_count']}"]
   end
+  table = Terminal::Table.new :rows => rows
+  puts table
 end
 
-puts take_repo(list)
-puts gem_stats(list)
+gem_stats(list)
