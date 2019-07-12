80d4b84bcc8d6cdf4d1fda2a78ef63e78c4cce23
Sufl0wer <fr08ty@gmail.com>
2019-07-12 16:17:55 +0300

    add hash for stats storing add calculating of average stats for future usage in sorting

--- 3750/1/gemy.rb
+++ 3750/1/gemy.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'mechanize'
 require 'nokogiri'
 require 'open-uri'
+<<<<<<< HEAD
 require_relative 'statistics'
 
 # :reek:Attribute
@@ -18,3 +19,74 @@ class Gemy
   attr_reader :gem_name
   attr_accessor :stats
 end
+=======
+require 'pry'
+
+class Gemy
+  def initialize(gem_name)
+    @gem_name = gem_name
+  end
+
+  attr_reader :gem_name
+  attr_reader :stats
+
+  def self.get_stats(gem_name)
+    gemy = new(gem_name)
+    gemy.get_stats
+    gemy.stats
+  end
+
+  def get_stats
+    get_link_to_repo
+    load_stats
+  end
+
+  private
+
+  attr_reader :link_to_repo
+
+  def get_link_to_repo
+    agent = Mechanize.new
+    page = agent.get("https://rubygems.org/gems/#{@gem_name}")
+    @link_to_repo = (page.links.find { |l| l.text == 'Source Code' }).href
+  end
+
+  def load_stats
+    @stats ||= {
+      watched:        main_doc.css('.social-count')[0].text.strip,
+      stars:          main_doc.css('.social-count')[1].text.strip,
+      forks:          main_doc.css('.social-count')[2].text.strip,
+      contributors:   main_doc.css('.text-emphasized')[3].text.strip,
+      issues:         main_doc.css('.Counter')[0].text.strip,
+      used:           used
+    }
+  end
+
+  def main_doc
+    @main_doc ||= Nokogiri::HTML(open(@link_to_repo.to_s))
+  end
+
+  def doc_for_used_by
+    @doc_for_used_by = Nokogiri::HTML(open("#{@link_to_repo}/network/dependents"))
+  end
+
+  def used
+    return @used if @used
+
+    doc_for_used_by.css('.btn-link')[1].text.strip.each_line do |line|
+      @used = line.chomp
+      break
+    end
+
+    @used
+  end
+
+end
+
+
+
+# [11] pry(main)> @doc.css('div.text-center .link-gray').first.text
+# binding.pry
+
+
+>>>>>>> add hash for stats storing
--- 3750/1/main.rb
+++ 3750/1/main.rb
@@ -2,22 +2,58 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
-require_relative 'gem'
+require_relative 'gemy'
 
 def read_file(array)
-  File.open('/home/alexander/RubymineProjects/top_gems/gems.yaml', 'r') do |f|
+  File.open('/home/alexander/summer-2019/3750/1/gems.yaml', 'r') do |f|
     f.each_line do |line|
       next if line.chomp == 'gems:'
       array << Gemy.new(line.strip[2..-1])
-      p array
     end
+  end
+end
+
+def average(arr, key)
+  sum = 0
+  arr.each do |gem|
+    sum += (gem.stats[key].delete!',').to_f
+  end
+  sum / arr.size
+end
+
+def calculate_average(arr_of_gems)
+  average_stats = {
+    average_used:          average(arr_of_gems, :used),
+    average_forks:         average(arr_of_gems, :forks),
+    average_stars:         average(arr_of_gems, :stars),
+    average_contributors:  average(arr_of_gems, :contributors),
+    average_watched:       average(arr_of_gems, :watched),
+    average_issues:        average(arr_of_gems, :issues)
+  }
+  p average_stats
+end
+
+def calculate_overall_score
+
+end
+
+def show_gems(gems_array)
+  gems_array.each do |gem|
+    gem.get_stats
+    puts '--------------------------------------------------------------------------'
+    puts "#{gem.gem_name} |"
+    puts '----------------'
+    puts "used by #{gem.stats[:used]} repositories"
+    puts 'stars - ' + gem.stats[:stars]
+    puts 'times watched - ' + gem.stats[:watched]
+    puts 'number of forks - ' + gem.stats[:forks]
+    puts 'contributors - ' + gem.stats[:contributors]
+    puts 'number of issues - ' + gem.stats[:issues]
   end
 end
 
 gems = []
 read_file(gems)
-
-gems.each &:show_stats
-
-
+show_gems(gems)
+calculate_average(gems)
 
