90f8aa631ed3e9d2ee9d397dcd420779a24dcd79
Maxxx911 <maxrikal@gmail.com>
2019-07-12 14:19:31 +0300

    Create class-manager, update command line parser, rework function find_fields

--- 3493/1/top_gems.rb
+++ 3493/1/top_gems.rb
@@ -1,117 +1,138 @@
 require 'yaml'
 require 'json'
 require 'terminal-table'
-require './Models/Gems.rb'
+require './Models/gem_model.rb'
 require 'net/http'
 require 'uri'
 require 'nokogiri'
 require 'open-uri'
 
-def parse_yml_file
-  file = YAML.load_file("gems.yml")
-  file['gems']
-end
+class GemManager
+  attr_reader :gem_hash 
 
-def find_repo_html_url(name)
-begin
-  uri = URI.parse("https://api.github.com/search/repositories?q=#{name}&sort=stars&order=desc")
-  a = response_body_hash(uri)
-  b = a[:items]
-  c = b[0]
-  d= c[:html_url]
-  # sleep(1)
-  rescue NoMethodError
-  require'pry';binding.pry;
-end
-end
-
-def find_fields(url)
-  result = []
-  doc = Nokogiri::HTML(open(url))
-  doc.css('.social-count').each do |a|
-    result.push (a.content.gsub(/[^0-9]/, '').to_i)
+  def initialize
+    @gem_hash = {}
   end
-  result.push(doc.css('.Counter')[0].content.gsub(/[^0-9]/, '').to_i)
-  result.push(doc.css('.num').css('.text-emphasized')[3].text.gsub(/[^0-9]/, ''))
-end
 
-def find_used_by(url)
-  url+= "/network/dependents"
-  doc = Nokogiri::HTML(open(url))
-  doc.css('.btn-link').css('.selected').text.gsub(/[^0-9]/, '')
-end
-
-def response_body_hash url
-  Net::HTTP.start(url.host, url.port,
-  :use_ssl => url.scheme == 'https') do |http|
-    request = Net::HTTP::Get.new url
-    response = http.request(request)
-    return JSON.parse(response.body, symbolize_names: true)
+  def parse_yml_file(file_name)
+    file = YAML.load_file(file_name)
+    file['gems']
   end
-end
 
-def show_table(args)
-  arr = []
-  args.each { |i| arr.push(i[0].get_fields) }
-  table = Terminal::Table.new do |t|
-    t.rows = arr
-    t.style = { :border_top => false, :border_bottom => false }
-  end
-  puts table
-end
-
-def pars_gems()
-  begin
-    array= []
-    parsed_file = parse_yml_file
-    complited = 0
-    print "Complited status #{complited}/#{parsed_file.count}"
-    parsed_file.each do |gem| 
-      complited += 1
-      print "\rComplited status----------------------------------------------------------------#{complited}/#{parsed_file.count}"
-      print "\n" if complited == parsed_file.count
-      # require 'pry'; binding.pry;
-      gem_object = Gems.new(gem)
-      gem_object.url = find_repo_html_url(gem)
-      gem_object.set_fields(find_fields(gem_object.url))
-      gem_object.count_used_by = find_used_by(gem_object.url)
-      array.push(gem_object)
+  def find_repo_html_url(name)
+    begin
+      uri = URI.parse("https://api.github.com/search/repositories?q=#{name}&sort=stars&order=desc")
+      response_body_hash(uri)[:items][0][:html_url] 
     end
-    array
-    # show_table(array.each_slice(1).to_a)
-    rescue :NoMethodErrors 
-    require 'pry';binding.pry
   end
+
+  def response_body_hash(url)
+    Net::HTTP.start(url.host, url.port,
+    :use_ssl => url.scheme == 'https') do |http|
+      request = Net::HTTP::Get.new(url)
+      response = http.request(request)
+      return JSON.parse(response.body, symbolize_names: true)
+    end
+  end  
+
+  def find_fields(url)
+    result = []
+    doc = Nokogiri::HTML(open(url))
+    doc.css('.social-count').each do |a|
+      result.push (a.content.gsub(/[^0-9]/, ''))
+    end
+    result.push(doc.css('.Counter')[0].content.gsub(/[^0-9]/, ''))
+    result.push(doc.css('.num').css('.text-emphasized')[3].text.gsub(/[^0-9]/, ''))
+  end  
+
+  def find_used_by(url)
+    url+= "/network/dependents"
+    doc = Nokogiri::HTML(open(url))
+    doc.css('.btn-link').css('.selected').text.gsub(/[^0-9]/, '')
+  end
+  
+  def show_table
+    rows = []
+    @gem_hash.each { |key, value| rows << value.strings }
+    table = Terminal::Table.new do |t|
+      t.rows = rows
+      t.style = { :border_top => false, :border_bottom => false }
+    end
+    puts table
+  end
+  
+  def parse_gem(file_name:"gems.yml", name_gems:nil)
+    parsed_yml_file = parse_yml_file(file_name) 
+    threads = []
+    parsed_yml_file.each do |gem| 
+        if gem.match(/#{name_gems}/) 
+          threads << Thread.new do
+            gem_object = GemModel.new(gem)
+            gem_object.url = find_repo_html_url(gem)
+            gem_object.install_fields(find_fields(gem_object.url))
+            gem_object.count_used_by = find_used_by(gem_object.url)
+            @gem_hash[gem] = gem_object
+          end
+        end
+    end
+    threads.each(&:join)
+  end
+
+  def choose_top_gem(top_count)
+    score = {}
+    hash = {}
+    gem_hash.each do |key, value|
+      array = value.fields
+      score[array[0]] = 3 * array[1] + 1 * array[2] + 2 * array[3] +
+                        2 * array[4] + 1 * array[5] + 2 * array[6]
+    end
+    score = score.sort_by{|key, value| value}.last(top_count.to_i)
+    score.each{ |elem| hash[elem[0]] = @gem_hash[elem[0]] }
+    @gem_hash = hash
+  end 
+
 end
 
-def choose_top_gem(gems)
-  p gems
-end
+
+
+
+
 
 def main()
-  # require 'pry'; binding.pry;
+  gem_manager = GemManager.new
+  setting = {}
   ARGV.each do |argumet|
     case argumet
       when /--top/
-      argumet.split('=')[1]
-      choose_top_gem(pars_gems) 
+        string_fields = argumet.split('=')
+        setting[string_fields[0]] = string_fields[1] 
+      when /--name/
+        string_fields = argumet.split('=')
+        setting[string_fields[0]] = string_fields[1]
+      when /--file/
+        string_fields = argumet.split('=')
+        setting[string_fields[0]] = string_fields[1]
+      else 
+        puts 'Argument Error'  
     end
   end
-  # array= []
-  # parsed_file = parse_yml_file
-  # complited = 0
-  # print "Complited status #{complited}/#{parsed_file.count}"
-  # parsed_file.each do |gem| 
-  #   complited += 1
-  #   print "\rComplited status----------------------------------------------------------------#{complited}/#{parsed_file.count}"
-  #   print "\n" if complited == parsed_file.count
-  #   gem_object = Gems.new(gem)
-  #   gem_object.url = find_repo_html_url(gem)
-  #   gem_object.set_fields(find_fields(gem_object.url))
-  #   gem_object.count_used_by = find_used_by(gem_object.url)
-  #   array.push(gem_object)
-  # end
-  # show_table(array.each_slice(1).to_a)
+  setting.each do |key, value| 
+    case key
+      when '--top'
+        gem_manager.parse_gem
+        gem_manager.choose_top_gem(value) 
+        gem_manager.show_table
+      when '--name'
+        # binding.pry
+        gem_manager.parse_gem(name_gems: value)
+        gem_manager.show_table
+      when '--file'
+        gem_manager.parse_gem(file_name:value)
+        gem_manager.show_table
+    end
+  end
+  # p setting
+  # parse()
 end
 
 main
