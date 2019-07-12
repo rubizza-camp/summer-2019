9e9b67bb65a1cac8ed17c52b08d6f6baa6bdc99e
Iris The Fox <iris-miz@yandex.ru>
2019-07-13 00:22:24 +0300

    Change criteria

--- 211/1/weights.rb
+++ 211/1/weights.rb
@@ -4,91 +4,116 @@
 # rspec   | 316338  | 96         | 2536  | 202   | 21           | 1            | 12           
 # sinatra | 144266  | 406        | 10617 | 1905  | 361          | 69           | 639      
 
-rails_g = {:name => :rails, :used_by => 1173065, :watch => 2608, :star => 43599, :fork => 17526, :contr => 3837, :op_iss => 372, :cl_iss => 12381}
-rspec_g = {:name => :rspec, :used_by => 316338 , :watch => 96  , :star => 2536 , :fork => 202  , :contr => 21  , :op_iss => 1  , :cl_iss => 12   }
-sinat_g = {:name => :sinat, :used_by => 144266 , :watch => 406 , :star => 10617, :fork => 1905 , :contr => 361 , :op_iss => 69 , :cl_iss => 639  }
+p hash_g= {:rails_g => { :used_by => 1173065, :watch => 2608, :star => 43599, :fork => 17526, :contr => 3837, :op_iss => 372, :cl_iss => 12381},
+:rspec_g => { :used_by => 316338 , :watch => 96  , :star => 2536 , :fork => 202  , :contr => 21  , :op_iss => 1  , :cl_iss => 12   },
+:sinat_g => { :used_by => 144266 , :watch => 406 , :star => 10617, :fork => 1905 , :contr => 361 , :op_iss => 69 , :cl_iss => 639  }}
 
-arr = [rails_g,rspec_g, sinat_g]
+# arr = [rails_g,rspec_g, sinat_g]
 
 # arr.each {|gem_n| puts  gem_n[:used_by]}
 
 # weights = {:used_by => 9 , :watch => 5 , :star => 8, :fork => 10 , :contr => 1 , :op_iss => 1 , :cl_iss => 7  }
 
 
-#нормализация критериев
-# def normalise(x, xmin, xmax, d0=0, d1=1)
-#   xrange = xmax - xmin
-#   drange = d1 - d0
-#   normalized = (d0 + (x - xmin) * (drange.to_f / xrange) ).round(2)
+# нормализация критериев
+def normalise(x, xmin, xmax, d0=0, d1=1)
+  xrange = xmax - xmin
+  drange = d1 - d0
+  normalized = (d0 + (x - xmin) * (drange.to_f / xrange) ).round(2)
+end
+
+ashn = {}
+hash_g.each do |k, vh|
+	vh.each do |k1,v1|
+		ashn[k1] ||= []
+		ashn[k1] << v1
+	end
+end
+p ashn 
+# ashn.each do |k,v|
+# 	ashn[k] = [v.max, v.min]
+# end
+
+hash_g.each do |k,vh|
+		vh.each do |k1,v1|
+		vh[k1] = normalise(v1, ashn[k1].min,  ashn[k1].max )
+	end
+end
+
+p hash_g
+# class Hash
+#   def normalise!(d0,d1)
+#   	ar = self.values
+#     xMin,xMax = ar.minmax
+#     dx = d1-d0
+#     xrange = (xMax-xMin).to_f
+#     self.each {|k,x| self[k] =  (d0 + dx * (x-xMin) / xrange).round(2) }
+
+#   end
+# end
+
+# # a = [3.0, 6.0, 3.1416]
+# # a.normalize!
+# # => [0.0, 1.0, 0.047199999999999985]
+
+# criteria = {:used_by => {} , :watch => {} , :star => {}, :fork => {} , :contr => {} , :op_iss => {} , :cl_iss => {}  }
+
+# criteria.each do |cr,hsh|
+#   arr.each do |gemn|
+#   hsh[gemn[:name]] = gemn[cr]
+#   end
 # end
 
 
-class Hash
-  def normalise!(d0,d1)
-  	ar = self.values
-    xMin,xMax = ar.minmax
-    dx = d1-d0
-    xrange = (xMax-xMin).to_f
-    self.each {|k,x| self[k] =  (d0 + dx * (x-xMin) / xrange).round(2) }
-
-  end
-end
-
-# a = [3.0, 6.0, 3.1416]
-# a.normalize!
-# => [0.0, 1.0, 0.047199999999999985]
-
-criteria = {:used_by => {} , :watch => {} , :star => {}, :fork => {} , :contr => {} , :op_iss => {} , :cl_iss => {}  }
-
-criteria.each do |cr,hsh|
-  arr.each do |gemn|
-  hsh[gemn[:name]] = gemn[cr]
-  end
-end
-
-
-p criteria
-
-# criteria.each do |cr, ar|
-# 	xmin = ar.min
-# 	xmax = ar.max
-# 	normalized_arr = []
-# 	ar.each do |crit|
-# 	  normalized_arr << normalise(crit, xmin, xmax)
-# 	end
-# 	p normalized_arr
-# 	p criteria[cr] = normalized_arr
-
-# end
-
-# puts
 # p criteria
 
-criteria.each do |cr, hsh|
-	criteria[cr] = hsh.normalise!(0,1)
-end
+# # criteria.each do |cr, ar|
+# # 	xmin = ar.min
+# # 	xmax = ar.max
+# # 	normalized_arr = []
+# # 	ar.each do |crit|
+# # 	  normalized_arr << normalise(crit, xmin, xmax)
+# # 	end
+# # 	p normalized_arr
+# # 	p criteria[cr] = normalized_arr
 
-p criteria
+# # end
+
+# # puts
+# # p criteria
+
+# criteria.each do |cr, hsh|
+# 	criteria[cr] = hsh.normalise!(0,1 )
+# end
+
+# p criteria
 
 weights = {:used_by => 10 , :watch => 4 , :star => 8, :fork => 6 , :contr => 0 , :op_iss => 0 , :cl_iss => 2  }
-
-criteria.each do |cr, hsh|
-	criteria[cr] = hsh.each {|ge, num| hsh[ge] = (num * weights[cr]).round(3) }
-end
-
-def transp(hsh)
-  hsh.inject({}) do |a,(k,v)|
-    v.inject(a) do |a1,(k1,v1)|
-      a1[k1] ||= {}
-      a1[k1][k] = v1
-      a1
-    end
-  end
-end
 puts
-puts
+hash_g.each do |g, cr|
+	cr.each do |k,v|
+		cr[k] = v*weights[k]
+	end
+end
+puts hash_g
 
-p criteria_new = transp(criteria)
+# criteria.each do |cr, hsh|
+# 	criteria[cr] = hsh.each {|ge, num| hsh[ge] = (num * weights[cr]).round(3) }
+# end
+
+# def transp(hsh)
+#   hsh.inject({}) do |a,(k,v)|
+#     v.inject(a) do |a1,(k1,v1)|
+#       a1[k1] ||= {}
+#       a1[k1][k] = v1
+#       a1
+#     end
+#   end
+# end
+# puts
+# puts
+
+# p criteria_new = transp(criteria)
 
 def summarize(hsh)
   hsh.each do |k,v|
@@ -97,4 +122,4 @@ def summarize(hsh)
 end
 puts
 puts
-p summarize(criteria_new)
+p summarize(hash_g)
