d1352e8c68373e584924f6570edd1153a36859af
Artur <rutraniloms@icloud.com>
2019-07-12 15:36:38 +0300

    verison 1.0.0

--- 204/1/top_gems_api.rb
+++ 204/1/top_gems_api.rb
@@ -8,40 +8,33 @@ require 'terminal-table'
 require 'optparse'
 
 def opt_file
-  options = {}
+  @options = {}
   optparse = OptionParser.new do |opts|
-    options[:config_file] = nil
-    options[:name_sort] = nil
-    options[:top] = 2
-    opts.on('-f', '--config-file [Array]', Array , 'Enter the config file to open.') do |file|
-      options[:config_file] = file
+    @options[:config_file] = nil
+    @options[:name_sort] = nil
+    @options[:top] = nil
+    opts.on('-f', '--config-file [Array]', Array, 'Enter the config file to open.') do |file|
+      @options[:config_file] = file
     end
     opts.on('-n', '--name [STRING]', String, 'Enter the config file to open.') do |name|
-      options[:name_sort] = name
-      byebug
+      @options[:name_sort] = name
     end
     opts.on('-t', '--top [INTEGER]', Integer, 'Enter the config file to open.') do |t|
-      options[:top] = t
+      @options[:top] = t
     end
   end
   optparse.parse!
-  if options[:config_file].methods
-    file = 'gems.yaml'
-  else
-    file = options[:config_file].to_s.gsub!(/[^0-9A-Za-z.]/, '')
-  end
-  file
+  file = if @options[:config_file].nil?
+           'gems.yaml'
+         else
+           @options[:config_file].to_s.gsub!(/[^0-9A-Za-z.]/, '')
+         end
 end
 
 def authentication
-  puts 'Введите Github account'
-  @username = gets.chomp
-  puts 'Введите пароль'
-  @password = gets.chomp
-  @client = Octokit::Client.new\
-  :client_id     => @username,
-  :client_secret => @password
-  user = @client.user 'drujok'
+  @client = Octokit::Client.new(access_token: 'ec1b04b3765d7a5fe6030337ee82faaec5b090f1')
+  user = @client.user
+  user.login
 end
 
 def yaml_parser
@@ -50,12 +43,25 @@ def yaml_parser
   @gems_hash = {}
   thing.values[0].each do |name|
     gem = Gems.info(name)
-    uri = gem['source_code_uri'].sub! 'https://github.com/', ''
-    user = @client.repo uri
-    doc = Nokogiri::HTML(open('https://github.com/' + uri))
-    doc_dependents = Nokogiri::HTML(open('https://github.com/' + uri + '/network/dependents'))
+    @uri = if gem['source_code_uri'].eql?(nil)
+             if gem['homepage_uri'].include?('https:')
+               gem['homepage_uri'].sub! 'https://github.com/', ''
+             else
+               gem['homepage_uri'].sub! 'http://github.com/', ''
+             end
+           else
+             if gem['source_code_uri'].include?('https:')
+               gem['source_code_uri'].sub! 'https://github.com/', ''
+             else
+               gem['source_code_uri'].sub! 'http://github.com/', ''
+             end
+           end
+    user = @client.repo @uri
+    doc = Nokogiri::HTML(open('https://github.com/' + @uri))
+    doc_dependents = Nokogiri::HTML(open('https://github.com/' + @uri + '/network/dependents'))
     @gems_hash.merge!("#{name}": gem_stats(user))
     @gems_hash.merge!("#{name}": contributors(doc, doc_dependents))
+    @gems_hash.merge!("#{name}": popularizer)
   end
 end
 
@@ -65,34 +71,57 @@ def gem_stats(user)
   @gem_stats.merge!(stargazers: user[:stargazers_count])
   @gem_stats.merge!(forks_count: user[:forks_count])
   @gem_stats.merge!(issues: user[:open_issues_count])
-  @gem_stats.merge!(subscribers: user[:watchers_count])
-  @gem_stats.merge!(used_by: '')
+  @gem_stats.merge!(subscribers: user[:subscribers_count])
 end
 
 def contributors(doc, doc_dependents)
   contributors = doc.css('span.num.text-emphasized').children[2].to_s.delete('^0-9').to_i
   used_by = doc_dependents.css('.btn-link')[1].text.delete('^0-9').to_i
   @gem_stats.merge!(contributors: contributors)
-  @gem_stats.merge!(used_by: contributors)
+  @gem_stats.merge!(used_by: used_by)
+end
+
+def popularizer
+  popularity = @gem_stats.values.select { |i| Numeric === i }.reduce(:+)
+  @gem_stats.merge!(popularity: popularity)
 end
 
 @keys = yaml_parser
 
-def table
+def table(sorted_hash)
   table = Terminal::Table.new do |t|
-    t.headings = ['gem', 'used by', 'watched by', 'stars', 'forks', 'contributors', 'issues']
-    hash =
-      @gems_hash.keys.each do |key|
-        key_hash = @gems_hash[key]
-        t << [key, key_hash[:used_by], key_hash[:subscribers], key_hash[:stargazers], key_hash[:forks_count], key_hash[:contributors], key_hash[:issues]]
-        t << :separator
-        t.style = { border_top: false, border_bottom: false }
-      end
+    t.headings = ['gem', 'used by', 'watched by', 'stars', 'forks', 'contributors', 'issues', 'popularity']
+    table_hash = sorted_hash.to_h
+    table_hash.to_h.keys.each do |key|
+      key_hash = table_hash[key]
+      t << [key, key_hash[:used_by], key_hash[:subscribers], key_hash[:stargazers], key_hash[:forks_count], key_hash[:contributors], key_hash[:issues], key_hash[:popularity]]
+      t << :separator
+      t.style = { border_top: false, border_bottom: false }
+    end
   end
 end
 
-def final
-  puts table
+def name_sort
+  if @options[:name_sort].eql?(nil)
+    @gems_hash
+  else
+    @gems_hash = @gems_hash.select { |k, _v| k.match? @options[:name_sort] }
+  end
 end
 
-final
+def sorted_hash
+  if @options[:top].eql?(nil)
+    @gems_hash.sort_by { |_k, v| v[:popularity] }.reverse.to_h
+  else
+    @gems_hash.sort_by { |_k, v| v[:popularity] }.reverse.to_h
+    @gems_hash.first(@options[:top])
+  end
+end
+
+def result
+  name_sort
+  sorted_hash
+  puts table(sorted_hash)
+end
+
+result
