5659e3d511668107dc83d61206d27d56f8f5bb34
Maxxx911 <maxrikal@gmail.com>
2019-07-12 14:17:20 +0300

    Rename Gems to gem_model

---
+++ 3493/1/Models/gem_model.rb
@@ -0,0 +1,26 @@
+class GemModel
+  attr_reader :url, :name
+  attr_writer :url, :name, :count_contributors, :count_used_by
+
+  def initialize(name)
+    @name = name
+  end
+
+  def install_fields(args)
+    @count_watched = args[0]
+    @count_stars = args[1]
+    @count_forks = args[2]
+    @count_issues = args[3]
+    @count_contributors = args[4]
+  end
+
+  def fields
+    [@name, @count_used_by.to_i, @count_watched.to_i, @count_stars.to_i,
+     @count_forks.to_i, @count_contributors.to_i, @count_issues.to_i]
+  end
+
+  def strings
+    [@name, "used by #{@count_used_by}", "watched by #{@count_watched}", "#{@count_stars} stars",
+     "#{@count_forks} forks", "#{@count_contributors} contibutors", "#{@count_issues} issues"]
+  end
+end
