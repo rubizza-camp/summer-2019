11961c93c2c9f4261c1e7bb61b89b9b997c7d021
Your Name <you@example.com>
2019-07-12 12:47:24 +0300

    Improve scraping(search in HomePage if there is no Code section) to fix bug with Spring gem

--- 3696/1/name_to_link_transformer.rb
+++ 3696/1/name_to_link_transformer.rb
@@ -9,8 +9,19 @@ class NameToLinkTransformer
 
   def github_link
     doc = Nokogiri::HTML(URI.open("https://rubygems.org/gems/#{@name}"))
-    all_links = doc.css('a#code').map { |link| link.attribute('href').to_s }
+    github_in_code_link(doc)
+    github_in_home_link(doc) unless @valid
+  end
+
+  def github_in_code_link(page)
+    all_links = page.css('a#code').map { |link| link.attribute('href').to_s }
     @valid = !all_links.empty?
-    @link = all_links.first if @valid
+    @link = all_links.first
+  end
+
+  def github_in_home_link(page)
+    alternative = page.css('a#home').map { |link| link.attribute('href').to_s }.first
+    @valid = alternative.include? 'github'.freeze
+    @link = alternative
   end
 end
