a3eebb5562ec860f51a2b80d77380d0bb3b561af
IvanBoris <Jdjjad89coq5ven5uqoweOOvqw3n@gmail.com>
2019-07-13 20:42:45 +0300

    Correcting TopGems class

--- 3501/1/top_gems.rb
+++ 3501/1/top_gems.rb
@@ -1,17 +1,36 @@
+require_relative 'git_get.rb'
 require 'yaml'
 require 'terminal-table'
-require './git_get.rb'
+require 'optparse'
 
+#:reek:FeatureEnvy
+#:reek:TooManyInstanceVariables
 class TopGems
-  def initialize(params)
-    @params = params.map { |param| param.split('=') }.to_h
+  attr_reader :threads
+  attr_reader :list
+
+  def initialize
+    @threads = []
+    @list = []
+    @params = load_options
     @yaml_file = yaml_from_file
-    @gem_list = @params['--name'] ? select_name : @yaml_file['gems']
-    @repos = @params['--top'] ? top(gems) : gems
+    @gem_list = @params[:name] ? select_name : @yaml_file['gems']
+    @repos = @params[:top] ? sort_top(load_gems) : load_gems
     show
   end
 
-  #:reek:FeatureEnvy
+  #:reek:TooManyStatements
+  #:reek:UtilityFunction
+  def load_options
+    options = {}
+    OptionParser.new do |opts|
+      opts.on('--name[=OPTIONAL]', String, 'pick by name')
+      opts.on('--file[=OPTIONAL]', String, 'gems file name')
+      opts.on('--top[=OPTIONAL]', Integer, 'showing first in raiting')
+    end.parse!(into: options)
+    options
+  end
+
   def show
     table_str = @repos.map do |repo|
       [
@@ -21,15 +40,13 @@ class TopGems
         "#{repo.issues} issues"
       ]
     end
-    print_terminal table_str
+    print_terminal(table_str)
   end
 
-  #:reek:FeatureEnvy
   def print_terminal(str_table)
-    style = { border_top: false, border_bottom: false }
     terminal = Terminal::Table.new do |tab|
       tab.rows = str_table
-      tab.style = style
+      tab.style = { border_top: false, border_bottom: false }
     end
     puts terminal
   end
@@ -37,30 +54,28 @@ class TopGems
   private
 
   def select_name
-    @yaml_file['gems'].select { |gem| gem.include? @params['--name'] }
+    @yaml_file['gems'].select { |gem| gem.include? @params[:name] }
   end
 
-  #:reek:FeatureEnvy
   #:reek:TooManyStatements
-  def gems
-    threads = []
-    list = []
+  def load_gems
     @gem_list.map do |gem|
-      threads << Thread.new do
-        list << GitGet.new(gem)
+      @threads << Thread.new do
+        @list << GetGemDataFromGit.new(gem)
       end
     end
-    threads.each(&:join)
-    list
+    @threads.each(&:join)
+    @list
   end
 
-  def top(gems)
-    gems.sort_by(&:used_by)[-@params['--top'].to_i..-1].reverse
+  def sort_top(gems)
+    limit_range = [@params[:top], gems.size].min
+    gems.sort_by(&:used_by)[-limit_range..-1].reverse
   end
 
   def yaml_from_file
-    YAML.safe_load(File.open(@params['--file']))
+    YAML.load_file(File.open(@params[:file]))
   end
 end
 
-TopGems.new ARGV
+TopGems.new
