e65c71a8af7154ab88009f02dac0d0da5f6410e8
AS-AlStar <starovojtov.alexander@gmail.com>
2019-07-13 21:56:40 +0300

    Fix class for rubocop and reek

--- 3573/1/gem_statictic.rb
+++ 3573/1/gem_statictic.rb
@@ -1,24 +1,19 @@
+# :reek:TooManyInstanceVariables
 class RubyGemStatistic
   attr_reader :gem_name, :used_by, :watches, :stars, :forks, :contributors, :issues
 
-  def initialize(gem_name:, used_by:, watches:, stars:, forks:, contributors:, issues:)
-    @gem_name = gem_name
-    @used_by = used_by
-    @watches = watches
-    @stars = stars
-    @forks = forks
-    @contributors = contributors
-    @issues = issues
+  def initialize(opts = {})
+    @gem_name = opts.fetch(:gem_name)
+    @used_by = opts.fetch(:used_by)
+    @watches = opts.fetch(:watches)
+    @stars = opts.fetch(:stars)
+    @forks = opts.fetch(:forks)
+    @contributors = opts.fetch(:contributors)
+    @issues = opts.fetch(:issues)
   end
-
-  # def to_s
-  #   "#{gem_name} | used by #{used_by} | watched by #{watches} |  #{stars} stars | #{forks} forks | #{contributors} contributors | #{issues} issues |"
-  # end
 
   def strings
     [gem_name, "used by #{used_by}", "watched by #{watches}", "#{stars} stars",
      "#{forks} forks", "#{contributors} contributors", "#{issues} issues"]
   end
 end
-
-
