acaac26b8de3184e6a965e90145b1927a9f2cf9b
kartowkaateist <kartowkaateist@gmail.com>
2019-07-13 15:21:38 +0300

    Got watch, star and fork

--- 3495/1/top_gems.rb
+++ 3495/1/top_gems.rb
@@ -1,44 +1,26 @@
 require 'gems'
 require 'yaml'
-require 'graphql'
-gem 'github_api-v4-client'
+require 'github_api'
 
-def get_url
-  # yaml_data = YAML.load_file('gem_list.yml')
-  # gems_arr = yaml_data.values[0]
-  # gems_arr.each do |gem_for_search|
-  #   gem_url = Gems.info(gem_for_search).values_at('source_code_uri').to_s.delete('", \, [, ]').chomp('/issues')
-  #   if (gem_url == 'nil') or !(gem_url.include? 'https://github.com/') and !(gem_url.include? 'http://github.com/')
-  #     gem_url = Gems.info(gem_for_search).values_at('homepage_uri').to_s.delete('", \, [, ]').chomp('/issues')
-  #   end
-  #   if (gem_url == 'nil') or !(gem_url.include? 'https://github.com/') and !(gem_url.include? 'http://github.com/')
-  #     gem_url = Gems.info(gem_for_search).values_at('bug_tracker_uri').to_s.delete('", \, [, ]').chomp('/issues')
-  #   end
-  #   puts gem_url
-  # end
-  user = Github::User.find('rails')
+def ggWp
+  yaml_data = YAML.load_file('gem_list.yml')
+  gems_arr = yaml_data.values[0]
+  gems_arr.each do |gem_for_search|
+    gem_url = Gems.info(gem_for_search).values_at('source_code_uri').to_s.delete('", \, [, ]').chomp('/issues')
+    if (gem_url == 'nil') or !(gem_url.include? 'https://github.com/') and !(gem_url.include? 'http://github.com/')
+      gem_url = Gems.info(gem_for_search).values_at('homepage_uri').to_s.delete('", \, [, ]').chomp('/issues')
+    end
+    if (gem_url == 'nil') or !(gem_url.include? 'https://github.com/') and !(gem_url.include? 'http://github.com/')
+      gem_url = Gems.info(gem_for_search).values_at('bug_tracker_uri').to_s.delete('", \, [, ]').chomp('/issues')
+    end
+    puts gem_url
+    user = gem_url.split('/', 4).last.split('/').first
+    puts user
+    repo = gem_url.split('/').last
+    puts repo
+    github = Github::new.repos.get user, repo
+    puts github['subscribers_count']
+    puts github['stargazers_count']
+    puts github['forks']
+  end
 end
-# network/dependents
-subscribers_count
-stargazers_count
-forks
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
