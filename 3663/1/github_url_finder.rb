b4f347d1815344dd946147079e89e502420b60dd
dzmitrytrystsen <dzmitrytrystsen@gmail.com>
2019-07-13 12:57:44 +0300

    github_url_finder.rb now returns hash

--- 3663/1/github_url_finder.rb
+++ 3663/1/github_url_finder.rb
@@ -4,19 +4,24 @@ require 'mechanize'
 require_relative 'yml_parser.rb'
 
 class GitHubUrlFinder
-  attr_reader :gem_rubygems_page, :gem_github_url
+  attr_reader :gems_github_urls_hash
 
-  def initialize(gem_name)
-    look_for_gem_github_url(gem_name)
+  def initialize(gem_names_array)
+    @gems_github_urls_hash = {}
+    look_for_gem_github_url(gem_names_array)
   end
 
   protected
 
-  def look_for_gem_github_url(gem_name)
+  def look_for_gem_github_url(gem_names_array)
     mechanize = Mechanize.new
-    @gem_rubygems_page = mechanize.get("https://rubygems.org/gems/#{gem_name}")
-    @gem_github_url = gem_rubygems_page.link_with(text: 'Source Code').uri.to_s
+
+    gem_names_array.each do |gem_name|
+      gem_rubygems_page = mechanize.get("https://rubygems.org/gems/#{gem_name}")
+      gem_github_url = gem_rubygems_page.link_with(text: 'Source Code').uri.to_s
+      @gems_github_urls_hash[gem_name.to_sym] = gem_github_url
+    end
   end
 end
 
-# gem_github_url = GitHubUrlFinder.new(YmlParser.new('gems.yml').gems_hash.values.flatten[0]).gem_github_url
+# p gems_github_urls_hash = GitHubUrlFinder.new(["rubocop", "nokogiri", "mechanize"]).gems_github_urls_hash
