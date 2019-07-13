29df5f71e9710a4cd5cf101a94a72d4fa88a4b5b
z3w1l0q <z3w1l0q@protonmail.com>
2019-07-13 08:45:01 -0700

    rewrite output logic

--- 3531/1/lib/output.rb
+++ 3531/1/lib/output.rb
@@ -12,17 +12,24 @@ class Output
     ready_gems = gems[0..@top] if @top
     ready_gems = ready_gems.select { |gem| gem.dig(1).include?(@name) } if @name
 
-    output_gems(ready_gems)
+    outputs_gems(ready_gems)
   end
 
-  def output_gems(ready_gems)
+  private
+
+  def outputs_gems(ready_gems)
+    system('clear')
+    puts 'no gems' if ready_gems.empty?
+    create_table(ready_gems) unless ready_gems.empty?
+  end
+
+  def create_table(ready_gems)
     table = Terminal::Table.new do |t|
       ready_gems.each do |gem|
         t.add_row [gem[2].name.to_s, "used by #{gem[2].stats[:used_by]}", "watched by #{gem[2].stats[:watched_by]}", "#{gem[2].stats[:stars]} stars", "#{gem[2].stats[:forks]} forks", "#{gem[2].stats[:contributors]} contributors", "#{gem[2].stats[:issues]} issues"]
       end
     end
 
-    system('clear')
     puts table
   end
 end
