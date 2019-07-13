610265cc2af844b4bce9308da1ee86a237baf75c
kartowkaateist <kartowkaateist@gmail.com>
2019-07-13 18:18:09 +0300

    I HATE MY LIFE

--- 3495/1/top_gems.rb
+++ 3495/1/top_gems.rb
@@ -1,6 +1,6 @@
 require 'gems'
 require 'yaml'
-require 'github_api'
+require 'nokogiri'
 
 def ggWp
   yaml_data = YAML.load_file('gem_list.yml')
@@ -13,14 +13,7 @@ def ggWp
     if (gem_url == 'nil') or !(gem_url.include? 'https://github.com/') and !(gem_url.include? 'http://github.com/')
       gem_url = Gems.info(gem_for_search).values_at('bug_tracker_uri').to_s.delete('", \, [, ]').chomp('/issues')
     end
-    puts gem_url
     user = gem_url.split('/', 4).last.split('/').first
-    puts user
     repo = gem_url.split('/').last
-    puts repo
-    github = Github::new.repos.get user, repo
-    puts github['subscribers_count']
-    puts github['stargazers_count']
-    puts github['forks']
   end
 end
