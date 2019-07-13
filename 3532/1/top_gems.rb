3768bcdc7113bbe6790b0ce49d6a11252eb30517
maksimmanzhai <bender23max@gmail.com>
2019-07-14 00:43:22 +0300

    Include list of gems to command-line tool

--- 3532/1/top_gems.rb
+++ 3532/1/top_gems.rb
@@ -1,2 +1,38 @@
+# require 'rubygems'
+# require 'yaml'
+# require 'nokogiri'
+# require 'mechanize'
+# require 'open-uri'
+
+# config = YAML.load(File.open("gems.yml"))
+
+# x = config["gems"]
+
+# agent = Mechanize.new
+# page = agent.get('https://github.com/')
+
+# x.each do |item|
+#   puts item.to_s
+# end
+
 require 'rubygems'
-require 'nokogiri'
+require 'mechanize'
+
+# mechanize = Mechanize.new
+# page = mechanize.get('https://www.gov.uk/')
+# form = page.forms.first
+# form['q'] = 'passport'
+# page = form.submit
+# page.search('#results h3').each do |h3|
+#   puts h3.text.strip
+# end
+
+mechanize = Mechanize.new
+page = mechanize.get('https://github.com/')
+form = page.forms.first
+form['q'] = 'dickhead'
+page = form.submit
+
+# page.search('#results h3').each do |h3|
+#   puts h3.text.strip
+# end
