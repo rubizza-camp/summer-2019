062c758ef3ac7b1ddd220a2508579402647558af
yurik763 <yurik763@tut.by>
2019-07-14 12:15:50 +0300

    return Hash.new

--- 3457/0/about_hashes.rb
+++ 3457/0/about_hashes.rb
@@ -4,10 +4,12 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # :reek:UncommunicativeVariableName
 class AboutHashes < Neo::Koan
   def test_creating_hashes
-    empty_hash = {}
+    # rubocop:disable Style/EmptyLiteral
+    empty_hash = Hash.new
     assert_equal Hash, empty_hash.class
     assert_equal({}, empty_hash)
     assert_equal 0, empty_hash.size
+    # rubocop:enable Style/EmptyLiteral
   end
 
   def test_hash_literals
