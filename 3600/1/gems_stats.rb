4adae4b128b72515a4c6d336d9e3b92160b2cff1
Alex Samets <aliaksandr.samets@gmail.com>
2019-07-14 16:36:34 +0300

    Create gems_stats.rb
    Creating gems_stats file

---
+++ 3600/1/gems_stats.rb
@@ -0,0 +1,19 @@
+# :reek:TooManyInstanceVariables
+class Stats
+  attr_reader :gem_name, :used_by, :watches, :stars, :forks, :contributors, :issues
+
+  def initialize(opts = {})
+    @gem_name = opts[:gem_name]
+    @used_by = opts[:used_by]
+    @watches = opts[:watches]
+    @stars = opts[:stars]
+    @forks = opts[:forks]
+    @contributors = opts[:contributors]
+    @issues = opts[:issues]
+  end
+
+  def strings
+    [gem_name, "used by #{used_by}", "watched by #{watches}", "#{stars} stars",
+     "#{forks} forks", "#{contributors} contributors", "#{issues} issues"]
+  end
+end 
