e01c7d2432a89669e85f76030992ae3674e8d447
maksimmanzhai <bender23max@gmail.com>
2019-07-14 12:24:03 +0300

    Small edit

--- 3532/1/top_gems.rb
+++ 3532/1/top_gems.rb
@@ -1,38 +1,18 @@
-# require 'rubygems'
-# require 'yaml'
-# require 'nokogiri'
-# require 'mechanize'
-# require 'open-uri'
-
-# config = YAML.load(File.open("gems.yml"))
-
-# x = config["gems"]
-
-# agent = Mechanize.new
-# page = agent.get('https://github.com/')
-
-# x.each do |item|
-#   puts item.to_s
-# end
-
 require 'rubygems'
+require 'yaml'
+# require 'nokogiri'
 require 'mechanize'
+require 'open-uri'
 
-# mechanize = Mechanize.new
-# page = mechanize.get('https://www.gov.uk/')
-# form = page.forms.first
-# form['q'] = 'passport'
-# page = form.submit
-# page.search('#results h3').each do |h3|
-#   puts h3.text.strip
-# end
+config = YAML.load(File.open("gems.yml"))
 
-mechanize = Mechanize.new
-page = mechanize.get('https://github.com/')
-form = page.forms.first
-form['q'] = 'dickhead'
-page = form.submit
+x = config["gems"]
 
-# page.search('#results h3').each do |h3|
-#   puts h3.text.strip
-# end
+agent = Mechanize.new
+page = agent.get('https://github.com/')
+
+x.each do |item|
+  form = page.forms.first
+  form['q'] = "#{item.to_s}"
+  page = form.submit
+end
