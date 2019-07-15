41306fef9b049c86f5439ddabb4bef4152f02f52
Maxxx911 <maxrikal@gmail.com>
2019-07-15 20:33:35 +0300

    Use static call

--- 3493/1/top_gems.rb
+++ 3493/1/top_gems.rb
@@ -12,16 +12,12 @@ require 'optparse'
 
 OptionParser.new do |parser|
   parser.on('-f', '--file=FILE') do |file|
-    gem_manager = GemMethod::GemManager.new(file, nil, -1)
-    gem_manager.call
+    GemMethod::GemManager.call(file, nil, -1)
   end
   parser.on('-n', '--name=NAME') do |name|
     GemMethod::GemManager.call('gems.yaml', name, -1)
-    # gem_manager = GemMethod::GemManager.new('gems.yaml', name, -1)
-    # gem_manager.call
   end
   parser.on('-t', '--top=TOP') do |top|
-    gem_manager = GemMethod::GemManager.new('gems.yaml', nil, top.to_i)
-    gem_manager.call
+    GemMethod::GemManager.call('gems.yaml', nil, top.to_i)
   end
 end.parse!
