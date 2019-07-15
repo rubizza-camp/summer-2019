77d7f08bef2a77383eabb2298b9783806b7476f9
qb <inudad@yandex.ru>
2019-07-15 14:08:34 +0300

    Delete reek and rubocop disablers in options.rb

--- 3684/1/options.rb
+++ 3684/1/options.rb
@@ -1,8 +1,5 @@
 require 'optparse'
 
-# :reek:TooManyStatements
-# rubocop:disable Metrics/MethodLength
-# :reek:NestedIterators
 class Parser
   attr_reader :flags
 
@@ -11,29 +8,31 @@ class Parser
     @flags[:file_name] = file_string.to_s
   end
 
+  def add_word_search_and_top_options(options)
+    options.on('-t', '--top NUMBER', 'Displays NUMBER of top gems') do |num|
+      flags[:number] = num
+    end
+
+    options.on('-n', '--name ANYWORD', 'Shows only gems with ANYWORD in name') do |word|
+      flags[:word] = word
+    end
+  end
+
+  def add_file_and_help_options(options)
+    options.on('-f', '--file FILE', 'Enable desired FILE') { |file| flags[:file_name] = file }
+
+    options.on('-h', '--help', 'Shows this information') do
+      puts options
+      abort
+    end
+  end
+
   def parse
     opt_parser = OptionParser.new do |options|
-      options.banner = 'Usage: top_gems.rb [options]'
-
-      options.on('-t', '--top NUMBER', 'Displays NUMBER of top gems') do |amount|
-        flags[:number] = amount
-      end
-
-      options.on('-n', '--name ANYWORD', 'Shows only gems with ANYWORD in name') do |gem_name|
-        flags[:word] = gem_name
-      end
-
-      options.on('-f', '--file FILE', 'Enable desired FILE') do |file|
-        flags[:file_name] = file
-      end
-
-      options.on('-h', '--help', 'Shows this information') do
-        puts options
-        abort
-      end
+      add_word_search_and_top_options(options)
+      add_file_and_help_options(options)
     end
     opt_parser.parse!
     flags
   end
 end
-# rubocop:enable Metrics/MethodLength
