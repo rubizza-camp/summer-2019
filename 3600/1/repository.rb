6d447d18cf0885a0e2c516c0eaaa5315a570a4d9
Alex Samets <aliaksandr.samets@gmail.com>
2019-07-15 23:18:03 +0300

    TooManyInstanceVariables fixed

--- 3600/1/repository.rb
+++ 3600/1/repository.rb
@@ -2,13 +2,23 @@
 class Repository
   attr_reader :gem_name, :used_by, :watches, :stars, :forks, :contributors, :issues
 
-  def initialize(opts = {})
+  def initialize(opts)
     @gem_name = opts[:gem_name]
     @used_by = opts[:used_by]
     @watches = opts[:watches]
-    @stars = opts[:stars]
-    @forks = opts[:forks]
-    @contributors = opts[:contributors]
-    @issues = opts[:issues]
+    init_stats_and_forks(opts)
   end
-end
+
+  def init_stats_and_forks(opts)
+      @stars = opts[:stars]
+      @forks = opts[:forks]
+      init_contributors_and_issues(opts)
+
+  end
+
+  def init_contributors_and_issues(opts)
+      @contributors = opts[:contributors]
+      @issues = opts[:issues]
+  end
+  	
+  end
