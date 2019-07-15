0698872acd1fe166ddf46207ca48c568c1cb9215
Ganna Goodkevich <gannagoodkevich@gmail.com>
2019-07-15 12:47:45 -0700

    fix hound woofs

--- 3529/1/apihendler.rb
+++ 3529/1/apihendler.rb
@@ -15,14 +15,14 @@ class GemsApiHendler
   end
 
   def url_check
-    unless @url['source_code_uri'] && @url['homepage_uri']
-      puts "ERROR: There is no github links on gem, named #{@gem_name}. Sorry, bro"
-      return
-    end
-    @gem_github = if @url['source_code_uri']
+    if @url['source_code_uri'] && @url['homepage_uri']
+      @gem_github = if @url['source_code_uri']
                     @url['source_code_uri']
                   else
                     @url['homepage_uri']
                   end
+    else
+      puts "ERROR: There is no github links on gem, named #{@gem_name}. Sorry, bro"
+    end
   end
 end
