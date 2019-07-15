9f62693edb00fde92c219be436251fe5914446f3
Kirill Verenich <kirillverenih@ya.ru>
2019-07-15 16:28:17 +0300

    Fix argument reading, top output. Add necessary constants

--- 3478/1/top_gems/terminal_output.rb
+++ 3478/1/top_gems/terminal_output.rb
@@ -4,17 +4,20 @@ class TerminalOutput
   HEADER = %w[Name Used_by Watchers Stars Forks Contributors Issues].freeze
 
   def initialize(gem_top, top_size, contains_in_name)
-    @gem_top = gem_top.sort_by { |gem| gem[:total_score] }
-    @contains_in_name = contains_in_name
-    top_size ||= @gem_top.size
+    top_size ||= gem_top.size
     @top_size = top_size.to_i
+    @contains_in_name = contains_in_name
+    @gem_top = prepare_top(gem_top)
     @rows = []
   end
 
+  def prepare_top(gem_top)
+    gem_top.sort_by! { |gem| - gem[:total_score] }.take(@top_size)
+  end
+
   def print_top
-    @gem_top.reverse.each do |gem|
+    @gem_top.each do |gem|
       next unless appropriate_name?(gem)
-      break if @rows.size >= @top_size
       create_new_position_in_top(gem)
     end
     puts Terminal::Table.new headings: HEADER, rows: @rows
@@ -23,10 +26,8 @@ class TerminalOutput
   private
 
   def create_new_position_in_top(gem)
-    all_fields = %i[name used_by watchers stars forks contributors issues]
-    item = []
-    all_fields.each { |field| item << gem[field] }
-    @rows << item
+    all_fields = %i[name used_by watchers stars forks contributors issues].freeze
+    @rows << gem.values_at(*all_fields)
   end
 
   def appropriate_name?(gem)
--- 3478/1/top_gems/top.rb
+++ 3478/1/top_gems/top.rb
@@ -6,26 +6,25 @@ require_relative 'terminal_output.rb'
 class Top
   def initialize(token)
     @github_parser = GitHubParser.new(token)
-    @gem_top = []
-    @parameters = { top: nil, name: nil, file: nil }
+    @parameters = load_parameters
   end
 
   def create_top
-    load_parameters
-    load_to_search_list.each { |name| @gem_top << @github_parser.parse(name) }
-    TerminalOutput.new(@gem_top, @parameters[:top], @parameters[:name]).print_top
+    gem_top = []
+    load_to_search_list.each { |name| gem_top << @github_parser.parse(name) }
+    TerminalOutput.new(gem_top, @parameters[:top], @parameters[:name]).print_top
   end
 
   private
 
   def load_to_search_list
-    YAML.safe_load(File.read(@parameters[:file]))['gems']
+    YAML.safe_load(File.read(@parameters[:file] ||= 'gems.yml'))['gems']
   end
 
   def load_parameters
-    @parameters[:top]  = ARGV.grep(/--top=(\d+)/).to_s[8..-3]
-    @parameters[:name] = ARGV.grep(/--name=(.+)/).to_s[9..-3]
-    @parameters[:file] = ARGV.grep(/--file=(.+)/).to_s[9..-3]
-    @parameters[:file] ||= 'gems.yml'
+    ARGV.each_with_object({}) do |parametr, hash|
+      splited = parametr.delete('-').split('=')
+      hash[splited.first.to_sym] = splited.last
+    end
   end
 end
