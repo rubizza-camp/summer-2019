25c015404404efc79ba970e92b24e3c434c14777
AS-AlStar <starovojtov.alexander@gmail.com>
2019-07-15 16:18:16 +0300

    Add class for create table

---
+++ 3573/1/create_table.rb
@@ -0,0 +1,10 @@
+# create table
+class CreateTable
+  def create(obj)
+    table = Terminal::Table.new do |tab|
+      tab.rows = obj
+      tab.style = { border_top: false, border_bottom: false }
+    end
+    puts table
+  end
+end
