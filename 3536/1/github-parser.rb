c0bbfef9f83c633cc2783942c8a5f4cc1b4497f4
Volovenko <vadim.volovenko@gmail.com>
2019-07-15 17:03:50 +0300

    update parser

---
+++ 3536/1/github-parser.rb
@@ -0,0 +1,46 @@
+@token = 'c501c09d054a3b93ca223e8c28ef2e458598a742'
+@api_url = 'https://api.github.com/graphql'
+@yml_file = 'top-gems.yml'
+
+def get_owner(repo)
+
+  uri = URI.parse(@api_url)
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true
+  request = Net::HTTP::Post.new(uri.path)
+  request['User-Agent'] = '3538-1'
+  request['Authorization'] = "token #{@token}"
+  request.body = "{\"query\":\"{search(query: \\\"#{repo}\\\", type: REPOSITORY, first:1){edges { node { ... on Repository { owner { login }}}}}}\"}"
+
+  response = https.request(request)
+  owner = JSON.parse(response.read_body)['data']['search']['edges'][0]['node']['owner']['login']
+end
+
+def get_data(owner,gem)
+
+  params = 'watchers{totalCount}forks{totalCount}mentionableUsers{totalCount}stargazers{totalCount}issues(states: OPEN){totalCount}'
+  parameters =  "{\"query\":\"{repository(owner: \\\"#{owner}\\\", name: \\\"#{gem}\\\"){#{params}}}\"}"
+
+  uri = URI.parse(@api_url)
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true
+  request = Net::HTTP::Post.new(uri.path)
+  request['User-Agent'] = '3538-1'
+  request['Authorization'] = "token #{@token}"
+  request.body = parameters
+  res = https.request(request)
+  result = JSON.parse(res.body)['data']['repository']
+
+  case res
+  when Net::HTTPSuccess, Net::HTTPRedirection
+    table = {}
+    table['watchers'] = result['watchers']['totalCount']
+    table['forks'] = result['forks']['totalCount']
+    table['contributors'] = result['mentionableUsers']['totalCount']
+    table['stars'] = result['stargazers']['totalCount']
+    table['issues'] = result['issues']['totalCount']
+    return table
+  else
+
+  end
+end
--- 3536/1/top-gems.rb
+++ 3536/1/top-gems.rb
@@ -2,27 +2,13 @@ require 'yaml'
 require 'net/http'
 require 'uri'
 require 'json'
+require_relative 'github-parser'
+require_relative 'params'
 
-
-def gems_stat
-  names = YAML::load(open('top-gems.yml'))['gems']
-  result = []
-  names.each do |gem|
-    url = 'https://api.github.com/search/repositories?q=' + gem.to_s
-    uri = URI.parse(url)
-
-    gem_content = Net::HTTP.get(uri)
-    key_data = JSON.parse(gem_content)
-
-    repo_url = key_data['items'][0]['url']
-    star = key_data['items'][0]['stargazers_count']
-    fork = key_data['items'][0]['forks_count']
-    issues = key_data['items'][0]['open_issues_count']
-
-    result.push([star, fork, issues])
-
-  end
-    p result
+def print_data(gem,data)
+  result = "#{gem} \s  \t| watched by #{data['watchers']} \t| #{data['stars']} stars \t| #{data['forks']} forks \t| #{data['contributors']} contributors \t| #{data['issues']} issues \t|"
+  puts result
 end
 
-gems_stat
+params_file
+main
