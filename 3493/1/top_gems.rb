628f37214cb0b3e76c8fa5df9f4ddb7b01f0e76b
Maxxx911 <maxrikal@gmail.com>
2019-07-15 13:06:29 +0300

    Delete my argument parser, used default parser

--- 3493/1/top_gems.rb
+++ 3493/1/top_gems.rb
@@ -1,59 +1,45 @@
 require 'yaml'
 require 'json'
 require 'terminal-table'
-require './Models/gem_model.rb'
-require './Functions/gem_manager.rb'
+require_relative './models/gem_model.rb'
+require_relative './functional/gem_manager.rb'
 require 'net/http'
 require 'uri'
 require 'nokogiri'
 require 'open-uri'
 require 'gems'
+require 'optparse'
 
 # :reek:UtilityFunction:
 def name_instruction(name_gems)
   gem_manager = Functions::GemManager.new
-  gem_manager.parse_gem(name_gems: name_gems)
-  gem_manager.show_table
+  gem_manager.parse_gem_info(name_gem: name_gems)
+  gem_manager.print_table
 end
 
 # :reek:UtilityFunction:
 def file_instruction(file_name)
   gem_manager = Functions::GemManager.new
-  gem_manager.parse_gem(file_name: file_name)
-  gem_manager.show_table
+  gem_manager.parse_gem_info(file_name: file_name)
+  gem_manager.print_table
 end
 
 # :reek:UtilityFunction:
 def top_instruction(top_count)
   gem_manager = Functions::GemManager.new
-  gem_manager.parse_gem
+  gem_manager.parse_gem_info
   gem_manager.choose_top_gem(top_count)
-  gem_manager.show_table
+  gem_manager.print_table
 end
 
-def choose_option(setting)
-  setting.each do |key, value|
-    case key
-    when '--top'
-      top_instruction(value)
-    when '--name'
-      name_instruction(value)
-    when '--file'
-      file_instruction(value)
-    end
+OptionParser.new do |parser|
+  parser.on('-f', '--file=FILE') do |file|
+    file_instruction(file)
   end
-end
-
-# rubocop:disable Style/RedundantBegin
-def main
-  begin
-    setting = ARGV.map { |argumet| argumet.split('=') }
-    setting = { '--file' => 'gems.yaml' } if ARGV.empty?
-    choose_option(setting.to_h)
-  rescue StandardError
-    puts 'Please, check list of gems!!'
+  parser.on('-n', '--name=NAME') do |name|
+    name_instruction(name)
   end
-end
-
-# rubocop:enable Style/RedundantBegin
-main
+  parser.on('-t', '--top=TOP') do |top|
+    top_instruction(top.to_i)
+  end
+end.parse!
