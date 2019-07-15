147114567afef8252268d5347357138f5f2cda97
MargaretOm <margosha20617@gmail.com>
2019-07-15 15:17:16 +0200

    Update class gem_info. Create new file name. Change a lot of names of methods and values

--- 3604/1/gem_info_fetcher.rb
+++ 3604/1/gem_info_fetcher.rb
@@ -1,65 +1,60 @@
 require 'mechanize'
 
 # class for get github url something gem and get information about this gem
-class GetInformationAboutGem
+class GemInfoFetcher
   attr_reader :temporary_array
+
+  URL = 'https://rubygems.org/'.freeze
 
   def initialize(name_gem)
     @name_gem = name_gem
-    @url = 'https://rubygems.org/'
-    @agent = Mechanize.new
     @temporary_array = []
   end
 
   def information_about_gem
-    if such_gem?
-      entry_all_values
-      temporary_array
-    else
-      false
-    end
+    all_values if such_gem?
+    temporary_array
   end
 
   private
 
   def such_gem?
-    page_all_gems
     begin
-      github_url
-      true
-    rescue NoMethodError
+      page_gems
+    rescue Mechanize::ResponseCodeError
       puts "Тo such gem #{@name_gem}"
-      false
+      return false
     end
+    github_url
   end
 
-  def entry_all_values
-    entry_value_name
-    entry_value_used_by
-    entry_value_watch_star_fork
-    entry_value_contributors
-    entry_value_issues
+  def all_values
+    value_name_gem
+    value_used_by
+    value_watch_star_fork
+    value_contributors
+    value_issues
   end
 
-  def entry_value_name
+  def value_name_gem
     temporary_array << @name_gem
   end
 
-  def entry_value_used_by
-    github_used_by = @agent.get(github_url + '/network/dependents')
+  def value_used_by
+    github_used_by = agent.get(github_url + '/network/dependents')
     temporary_array << github_used_by.css("a[class='btn-link selected']").text.gsub(/[^\d,]/, '')
   end
 
-  def entry_value_watch_star_fork
+  def value_watch_star_fork
     array_include_watched_stars_forks.each { |iter| temporary_array << iter.text.strip }
   end
 
-  def entry_value_contributors
+  def value_contributors
     arr_conributors = github.css("span[class='num text-emphasized']")
     temporary_array << arr_conributors[3].text.strip
   end
 
-  def entry_value_issues
+  def value_issues
     temporary_array << github.at('.Counter').text
   end
 
@@ -67,23 +62,20 @@ class GetInformationAboutGem
     @array_include_watched_stars_forks ||= github.css('.social-count')
   end
 
-  def page_all_gems
-    form.fields.each { |form| form.value = @name_gem if form.name == 'query' }
-    @page_all_gems ||= form.submit
+  def page_gems
+    @page_gems ||= agent.get(URL + 'gems/' + @name_gem)
   end
 
   def github_url
-    page_gems = @agent.get(@url + page_all_gems.at('.gems__gem')['href'])
     return @github_url ||= page_gems.at('#code')['href'] if page_gems.at('#code')
     return @github_url ||= page_gems.at('#home')['href'] unless page_gems.at('#code')
   end
 
   def github
-    @github ||= @agent.get github_url
+    @github ||= agent.get(github_url)
   end
 
-  def form
-    page = @agent.get @url
-    @form ||= page.forms.first # open form in https://rubygems.org/
+  def agent
+    @agent ||= Mechanize.new
   end
 end
