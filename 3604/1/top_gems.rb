116506cd5a172941f2b9430bf06be6a162afa7cc
MargaretOm <margosha20617@gmail.com>
2019-07-15 15:19:15 +0200

    Update class top_gems. Rename value and create const

--- 3604/1/top_gems.rb
+++ 3604/1/top_gems.rb
@@ -1,40 +1,34 @@
 require 'terminal-table'
 require './data_initializer.rb'
-require './get_information_about_gem.rb'
+require './gem_info_fetcher.rb'
 require './create_rating.rb'
 
 # class for main task: start project, get array of names and get arguments
 # return table with all information
 class TopGems
+  ARR_FOR_TABLE = ['used by', ' watched by', ' stars', ' forks', ' contributors', ' issues'].freeze
+
   def initialize
     @array_gems = []
-    @temp_arr = ['', ' used by', ' watched by', ' stars', ' forks', ' contributors', ' issues']
   end
 
   def start_top_gems
-    getting_parameters_and_array_names_gems
+    initialize_object.hash_arguments
     create_array_with_all_information_about_gems
     add_rating_in_array_with_all_information
     table_output(@array_gems)
   end
 
-  def getting_parameters_and_array_names_gems
-    initialize_object.getting_parameters
-    initialize_object.open_file_with_names_of_gems
-    flags_parameters
-    array_name_gems
-  end
-
   def create_array_with_all_information_about_gems
     array_name_gems.size.times do |iter|
       name_gem = array_name_gems[iter]
-      information = GetInformationAboutGem.new(name_gem).information_about_gem
-      @array_gems << information if information
+      information = GemInfoFetcher.new(name_gem).information_about_gem
+      @array_gems << information if information != []
     end
   end
 
   def add_rating_in_array_with_all_information
-    @array_gems = CreateRating.new(@array_gems).create_rating
+    @array_gems = CreateRating.new(@array_gems).rating_of_gems
   end
 
   def table_output(array_gems)
@@ -50,13 +44,13 @@ class TopGems
 
   def transformation_into_a_nice_view(array)
     1.upto(6) do |iter|
-      array[iter] += @temp_arr[iter]
+      array[iter] += ARR_FOR_TABLE[iter - 1]
     end
     array
   end
 
   def how_many_names_need_to_display?
-    number_top_gems = flags_parameters[:top]
+    number_top_gems = hash_arguments[:top]
     count_gems = @array_gems.size
     if number_top_gems && number_top_gems.to_i < count_gems
       number_top_gems
@@ -65,12 +59,12 @@ class TopGems
     end
   end
 
-  def flags_parameters
-    @flags_parameters ||= initialize_object.hash_argv
+  def hash_arguments
+    @hash_arguments ||= initialize_object.hash_argv
   end
 
   def array_name_gems
-    @array_name_gems ||= initialize_object.array_name_gems
+    @array_name_gems ||= initialize_object.array_names_of_gems
   end
 
   def initialize_object
