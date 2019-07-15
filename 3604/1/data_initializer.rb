35fe9560afbdf46e6b079e360e37f9d55a1595c4
MargaretOm <margosha20617@gmail.com>
2019-07-15 15:21:27 +0200

    Update class data_initializer. Use YAML module, rename methods

--- 3604/1/data_initializer.rb
+++ 3604/1/data_initializer.rb
@@ -3,46 +3,41 @@ class DataInitializer
   # create object for InvalidArgumentError error
   InvalidArgumentError = Class.new(StandardError)
 
-  attr_reader :argv
-  attr_reader :hash_argv, :array_name_gems
+  # attr_reader :array_names_gems
+  attr_reader :hash_argv
 
   def initialize
-    @argv = ARGV
-
     @hash_argv = {
       top: nil,
       name: nil,
       file: 'gems.yml'
     }
 
-    @array_name_gems = []
+    @array_names_gems = []
   end
 
-  def getting_parameters
-    argv.each do |argv|
-      parametr = argv.split('=')
-      parametr_name = parametr[0].gsub(/[^A-za-z]/, '').to_sym
-      unless hash_argv.include? parametr_name
+  def hash_arguments
+    ARGV.each do |argvs|
+      name, value = argvs.split('=')
+      name = name.gsub(/[^A-za-z]/, '').to_sym
+      unless hash_argv.include? name
         raise InvalidArgumentError, 'Please, enter the correct parameters.'
       end
-      hash_argv[parametr_name] = parametr[1]
+      hash_argv[name] = value
     end
   end
 
-  def open_file_with_names_of_gems
-    text = File.open(hash_argv[:file]).read
-    text.each_line do |line|
-      name_gem = line.gsub(/[^A-za-z]/, '')
-      array_name_gems << name_gem if name_include_parameter_name?(name_gem)
-    end
-    array_name_gems.shift if array_name_gems[0] == 'gems'
+  def array_names_of_gems
+    hash = YAML.load_file(hash_argv[:file])
+    array_names_gems = hash['gems']
+    array_names_gems.delete_if { |gem_name| !name_of_gem_include_argv_name?(gem_name) }
+    array_names_gems
   end
 
   private
 
-  def name_include_parameter_name?(name)
-    parm_name = hash_argv[:name]
-    return true unless parm_name
-    name.include?(parm_name)
+  def name_of_gem_include_argv_name?(name)
+    return true unless hash_argv[:name]
+    name.include?(hash_argv[:name])
   end
 end
