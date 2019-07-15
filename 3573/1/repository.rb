0e708dedb9f50691c2b6516f192d157a601d0e4a
AS-AlStar <starovojtov.alexander@gmail.com>
2019-07-15 20:50:07 +0300

    Create class for save statictic repository

---
+++ 3573/1/repository.rb
@@ -0,0 +1,15 @@
+# :reek:TooManyInstanceVariables
+# Task class: save ruby gem repository statistic
+class Repository
+  attr_reader :gem_name, :used_by, :watches, :stars, :forks, :contributors, :issues
+
+  def initialize(opts = {})
+    @gem_name = opts[:gem_name]
+    @used_by = opts[:used_by]
+    @watches = opts[:watches]
+    @stars = opts[:stars]
+    @forks = opts[:forks]
+    @contributors = opts[:contributors]
+    @issues = opts[:issues]
+  end
+end
