a8ad4ff1d9e85f3cf0de6840382c0b1a5be53b3f
All <volodya22all@gmail.com>
2019-07-15 13:22:38 +0300

    Fix about_regular expressions koan

--- 3467/0/about_regular_expressions.rb
+++ 3467/0/about_regular_expressions.rb
@@ -128,8 +128,8 @@ class AboutRegularExpressions < Neo::Koan
 
   def test_variables_can_also_be_used_to_access_captures
     assert_equal 'Gray, James', 'Name:  Gray, James'[/(\w+), (\w+)/]
-    assert_equal 'Gray', Regexp.last_match(1)
-    assert_equal 'James', Regexp.last_match(2)
+    assert_equal 'Gray', $1
+    assert_equal 'James', $2
   end
 
   # ------------------------------------------------------------------
@@ -152,10 +152,10 @@ class AboutRegularExpressions < Neo::Koan
   end
 
   def test_sub_is_like_find_and_replace
-    assert_equal 'one t-three', 'one two-three'.sub(/(t\w*)/) { Regexp.last_match(1)[0, 1] }
+    assert_equal 'one t-three', 'one two-three'.sub(/(t\w*)/) { $1[0, 1] }
   end
 
   def test_gsub_is_like_find_and_replace_all
-    assert_equal 'one t-t', 'one two-three'.gsub(/(t\w*)/) { Regexp.last_match(1)[0, 1] }
+    assert_equal 'one t-t', 'one two-three'.gsub(/(t\w*)/) { $1[0, 1] }
   end
 end
