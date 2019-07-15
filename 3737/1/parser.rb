e269dc6514fe0f658699bc052c6e5e431b991847
M1ha11 <tedpoolioo@gmail.com>
2019-07-15 21:19:09 +0300

    Fix instance varaibles & some rubocop error

--- 3737/1/parser.rb
+++ 3737/1/parser.rb
@@ -2,99 +2,95 @@ require 'nokogiri'
 require 'open-uri'
 require 'xpath'
 
-# This module is convert string to intger
-module ModifyData
-  def modify(str)
-    str = str.strip.delete(',').to_i
-    str
-  end
-end
-
 # class for parsing info about gem from github page
 class Parser
   def initialize
     @arr = {}
   end
 
-  include ModifyData
-
   def search_repo(gname)
     page = Nokogiri::HTML(URI.open("http://rubygems.org/gems/#{gname}"))
     homepage = page.xpath('//a[@id="home"]').attr('href')
     sourcepage = page.xpath('//a[@id="code"]').attr('href')
-    check_page(sourcepage, homepage, gname)
+    check_page(sourcepage, homepage)
   end
 
-  def check_page(sourcepage, homepage, gname)
-    if !sourcepage.nil?
+  def check_page(sourcepage, homepage)
+    if sourcepage
       sourcepage.value if sourcepage.value.include?('github')
     elsif homepage.value.include?('github')
       homepage.value
-    else
-      puts "There is no github repo for #{gname} gem"
     end
   end
 
   def git_repo(link)
-    @github_page = Nokogiri::HTML(URI.open(link))
-    @github_page
+    github_page = Nokogiri::HTML(URI.open(link))
+    github_page
   end
 
   def git_repo_for_usedby(link)
     link_usedby = link + '/network/dependents'
-    @github_page_usedby = Nokogiri::HTML(URI.open(link_usedby))
-    @github_page_usedby
+    github_page_usedby = Nokogiri::HTML(URI.open(link_usedby))
+    github_page_usedby
   end
 
-  def parse_usedby
-    used_by = @github_page_usedby.xpath('//div[@class="Box-header clearfix"]//a')[0].content
+  def modify(str)
+    str = str.strip.delete(',').to_i
+    str
+  end
+
+  def parse_usedby(link)
+    github_page_usedby = git_repo_for_usedby(link)
+    used_by = github_page_usedby.xpath('//div[@class="Box-header clearfix"]//a')[0].content
     @arr[:used_by] = modify(used_by)
   end
 
-  def parse_watch_fork
-    watch = @github_page.xpath('//div//ul/li/a[@class="social-count"]')[-2].content
+  def parse_watch_fork(link)
+    github_page = git_repo(link)
+    watch = github_page.xpath('//div//ul/li/a[@class="social-count"]')[-2].content
     @arr[:watch] = modify(watch)
-    forks = @github_page.xpath('//div//ul/li/a[@class="social-count"]')[-1].content
+    forks = github_page.xpath('//div//ul/li/a[@class="social-count"]')[-1].content
     @arr[:forks] = modify(forks)
   end
 
-  def parse_star
-    star = @github_page.xpath('//div//ul/li/a[@class="social-count js-social-count"]')[0].content
+  def parse_star(link)
+    github_page = git_repo(link)
+    star = github_page.xpath('//div//ul/li/a[@class="social-count js-social-count"]')[0].content
     @arr[:star] = modify(star)
   end
 
-  def parse_issue
-    issues = @github_page.xpath('//nav//span[@class="Counter"]')[0].content
+  def parse_issue(link)
+    github_page = git_repo(link)
+    issues = github_page.xpath('//nav//span[@class="Counter"]')[0].content
     @arr[:issues] = modify(issues)
   end
 
-  def parse_contributor
-    contributors = @github_page.xpath('//div[@class="stats-switcher-wrapper"]/ul/li//span')[3].content
+  def parse_contributor(link)
+    git_page = git_repo(link)
+    contributors = git_page.xpath('//div[@class="stats-switcher-wrapper"]/ul/li//span')[3].content
     @arr[:contributors] = modify(contributors)
   end
 
   def total
-    total = 0
-    @arr.each { |_, value| total += value }
-    total -= @arr.fetch(:issues)
-    @arr[:total] = total
+    sum = 0
+    @arr.each { |_, value| sum += value }
+    sum -= @arr.fetch(:issues)
+    @arr[:sum] = sum
   end
 
-  def gethash
-    parse_usedby
-    parse_watch_fork
-    parse_star
-    parse_issue
-    parse_contributor
+  def gethash(link)
+    parse_usedby(link)
+    parse_watch_fork(link)
+    parse_star(link)
+    parse_issue(link)
+    parse_contributor(link)
     total
     @arr
   end
 
   def repository(gname)
     link = search_repo(gname)
-    return if link.nil?
-    git_repo(link)
-    git_repo_for_usedby(link)
-    gethash
+    return "There is no github repo for #{gname}" unless link
+    gethash(link)
   end
 end
--- 3737/1/top_gems.rb
+++ 3737/1/top_gems.rb
@@ -6,23 +6,23 @@ require 'yaml'
 # class that show gems with their's stats
 class TopGems
   def initialize
-    @top_gems = {}
+    @gems = {}
   end
 
   def gems_file
     options_for_gems
-    file = YAML.load_file(@options[:file])
+    file = YAML.load_file(@optn[:file])
     file['gems'].each do |lib|
       values = Parser.new.repository(lib)
       next if values.nil?
-      @top_gems[lib] = values
+      @gems[lib] = values
     end
     options_select
   end
 
   def tableshow
     Terminal::Table.new do |tab|
-      @top_gems.each do |key, val|
+      @gems.each do |key, val|
         tab << [
           key, "used by #{val[:used_by]}", "watch #{val[:watch]}",
           "forks #{val[:forks]}", "stars #{val[:star]}",
@@ -33,12 +33,12 @@ class TopGems
   end
 
   def options_for_gems
-    @options = {}
-    @options[:file] = 'gems.yml'
+    @optn = {}
+    @optn[:file] = 'gems.yml'
     OptionParser.new do |opt|
-      opt.on('-t', '--top = top', Integer) { |top| @options[:top] = top }
-      opt.on('-n', '--name = nam', String) { |nam| @options[:name] = nam }
-      opt.on('-f', '--file = file', String) { |file| @options[:file] = file }
+      opt.on('-t', '--top = top', Integer) { |top| @optn[:top] = top }
+      opt.on('-n', '--name = nam', String) { |nam| @optn[:name] = nam }
+      opt.on('-f', '--file = file', String) { |file| @optn[:file] = file }
     end.parse!
   end
 
@@ -49,13 +49,14 @@ class TopGems
   end
 
   def same_gem_name
-    return unless @options[:name]
-    @top_gems.select! { |key, _| key.include?(@options[:name]) }
+    return unless @optn[:name]
+    @gems.select! { |key, _| key.include?(@optn[:name]) }
   end
 
   def top
-    return @top_gems = @top_gems.sort_by { |_, arr| -arr[:total] }[0..@options[:top] - 1].to_h if @options[:top]
-    @top_gems = @top_gems.sort_by { |_, arr| -arr[:total] }.to_h
+    puts @gems
+    return @gems = @gems.sort_by { |_, arr| -arr[:sum] }[0..@optn[:top] - 1].to_h if @optn[:top]
+    @gems = @gems.sort_by { |_, arr| puts arr; -arr[:sum] }.to_h
   end
 end
 
