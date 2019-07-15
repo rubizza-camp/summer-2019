cb4377108b4d6286f8b81583a2e754dde1d1de33
Maxxx911 <maxrikal@gmail.com>
2019-07-15 16:44:37 +0300

    delete trash methods

--- 3493/1/top_gems.rb
+++ 3493/1/top_gems.rb
@@ -10,36 +10,17 @@ require 'open-uri'
 require 'gems'
 require 'optparse'
 
-# :reek:UtilityFunction:
-def name_instruction(name_gems)
-  gem_manager = Functions::GemManager.new
-  gem_manager.parse_gem_info(name_gem: name_gems)
-  gem_manager.print_table
-end
-
-# :reek:UtilityFunction:
-def file_instruction(file_name)
-  gem_manager = Functions::GemManager.new
-  gem_manager.parse_gem_info(file_name: file_name)
-  gem_manager.print_table
-end
-
-# :reek:UtilityFunction:
-def top_instruction(top_count)
-  gem_manager = Functions::GemManager.new
-  gem_manager.parse_gem_info
-  gem_manager.choose_top_gem(top_count)
-  gem_manager.print_table
-end
-
 OptionParser.new do |parser|
   parser.on('-f', '--file=FILE') do |file|
-    file_instruction(file)
+    gem_manager = Functions::GemManager.new(file, nil, -1)
+    gem_manager.call
   end
   parser.on('-n', '--name=NAME') do |name|
-    name_instruction(name)
+    gem_manager = Functions::GemManager.new('gems.yaml', name, -1)
+    gem_manager.call
   end
   parser.on('-t', '--top=TOP') do |top|
-    top_instruction(top.to_i)
+    gem_manager = Functions::GemManager.new('gems.yaml', nil, top.to_i)
+    gem_manager.call
   end
 end.parse!
