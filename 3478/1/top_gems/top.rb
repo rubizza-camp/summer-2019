3a3727edf1de160d7fc35a825449ea583d0806e1
Kirill Verenich <kirillverenih@ya.ru>
2019-07-15 17:41:11 +0300

    Fix spelling

--- 3478/1/top_gems/top.rb
+++ 3478/1/top_gems/top.rb
@@ -5,13 +5,13 @@ require_relative 'terminal_output.rb'
 
 class Top
   def initialize(token)
-    @github_parser = GitHubParser.new(token)
+    @github_parser = Octokit::Client.new(access_token: token)
     @parameters = load_parameters
   end
 
   def create_top
     gem_top = []
-    load_to_search_list.each { |name| gem_top << @github_parser.parse(name) }
+    load_to_search_list.each { |name| gem_top << GitHubParser.new(name, @github_parser).parse }
     TerminalOutput.new(gem_top, @parameters[:top], @parameters[:name]).print_top
   end
 
@@ -23,8 +23,8 @@ class Top
 
   def load_parameters
     ARGV.each_with_object({}) do |parametr, hash|
-      splited = parametr.delete('-').split('=')
-      hash[splited.first.to_sym] = splited.last
+      split = parametr.delete('-').split('=')
+      hash[splited.first.to_sym] = split.last
     end
   end
 end
