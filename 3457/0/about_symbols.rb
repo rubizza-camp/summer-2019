acd74ae763c9fc7f692da0317b4811bcdeae7b4f
yurik763 <yurik763@tut.by>
2019-07-15 22:46:43 +0300

    RubyConstant

--- 3457/0/about_symbols.rb
+++ 3457/0/about_symbols.rb
@@ -41,7 +41,7 @@ class AboutSymbols < Neo::Koan
     def test_constants_become_symbols
       all_symbols_as_strings = Symbol.all_symbols.map(&:to_s)
 
-      assert_equal true, all_symbols_as_strings.include?('test_method_names_become_symbols')
+      assert_equal true, all_symbols_as_strings.include?('RubyConstant')
     end
   end
 
