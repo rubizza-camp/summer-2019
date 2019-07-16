dcfe67f605669d8f8086387f0c450b5ed2f806de
MargaretOm <margosha20617@gmail.com>
2019-07-16 15:31:00 +0200

    Change method logic
    Create const hash for output
    Create array with data for output, because for gem terminal-table need array

--- 3604/1/table_output.rb
+++ 3604/1/table_output.rb
@@ -1,11 +1,21 @@
 require 'terminal-table'
 
-# blabla seokvfkse;dofkosekfo
+# output table with all information about gems
 class TableOutput
+  HASH_FOR_TABLE = {
+    name: '',
+    used_by: 'used by ',
+    watched_by: 'watched by ',
+    stars: 'stars ',
+    forks: 'forks ',
+    contributors: 'contributors ',
+    issues: 'issues '
+  }.freeze
+
   def table_output(array_gems, count_gems_in_argv)
     table = Terminal::Table.new
     table.style = { border_top: false, border_bottom: false }
-    how_many_names_need_to_display?(count_gems_in_argv, array_gems.size).to_i.times do |iter|
+    how_many_names_need_to_display(count_gems_in_argv, array_gems.size).times do |iter|
       table.add_row transformation_into_a_nice_view(array_gems[iter])
     end
     puts table
@@ -14,15 +24,14 @@ class TableOutput
   private
 
   def transformation_into_a_nice_view(hash)
-    arr_for_table = ['', 'used by ', 'watched by ', 'stars ', 'forks ', 'contributors ', 'issues ']
-    iter = -1
-    hash.each do |_key, value|
-      arr_for_table[iter += 1] += value
+    arr_for_table = []
+    hash.each_key do |key|
+      arr_for_table << HASH_FOR_TABLE[key] + hash[key]
     end
     arr_for_table
   end
 
-  def how_many_names_need_to_display?(count_gems_in_argv, count_gems)
+  def how_many_names_need_to_display(count_gems_in_argv, count_gems)
     if count_gems_in_argv.to_i.positive? && count_gems_in_argv.to_i < count_gems
       count_gems_in_argv.to_i
     else
