6d79b3e9e82f289cca53a2e289b8e62fd45cea38
Alex Samets <aliaksandr.samets@gmail.com>
2019-07-16 23:10:13 +0300

    Got rid of extra instance variables using metaprogramming

--- 3600/1/repository.rb
+++ 3600/1/repository.rb
@@ -2,21 +2,9 @@
 class Repository
   attr_reader :gem_name, :used_by, :watches, :stars, :forks, :contributors, :issues
 
-  def initialize(opts)
-    @gem_name = opts[:gem_name]
-    @used_by = opts[:used_by]
-    @watches = opts[:watches]
-    init_stats_and_forks(opts)
-  end
-
-  def init_stats_and_forks(opts)
-    @stars = opts[:stars]
-    @forks = opts[:forks]
-    init_contributors_and_issues(opts)
-  end
-
-  def init_contributors_and_issues(opts)
-    @contributors = opts[:contributors]
-    @issues = opts[:issues]
+  def initialize(opts = {})
+    opts.each_pair do |attr, value|
+      instance_variable_set("@#{attr}", value)
+    end
   end
 end
