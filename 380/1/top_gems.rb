74f35a959b88cfa46caf0c449f9cb8932e6ed0be
mmmalikov <unitcircle@protonmail.comunitcircle@protonmail.comunitcircle@protonmail.com>
2019-07-16 15:28:14 +0300

    first workin version before refactoring

--- 380/1/top_gems.rb
+++ 380/1/top_gems.rb
@@ -7,47 +7,30 @@ class TopGems
   def run
     list = gem_list('gems.yml')
 
-    client = client('8ab9b46acee5e6c2eae941db1dd39b5d4767ef24')
+    client = Octokit::Client.new(access_token: 'dafc3260d5cb09fb99dd60fe872f7134cf194daa')
     client.user.login
+
+    result = {}
     list.each do |gem|
-      puts link_to_gem(gem)
-      gem_link = link_to_gem(gem)
-      result[gem.to_sym] = info(gem_link)
+      gem_link = Gems.info(gem)['source_code_uri'].gsub('https://github.com/', '')
+      puts gem_link
+      repo = client.repo gem_link
+      puts repo
+      info = {
+        name: repo[:name],
+        stargazers: repo[:stargazers_count],
+        forks_count: repo[:forks_count],
+        issues: repo[:open_issues_count],
+        subscribers: repo[:subscribers_count]
+      }
+      puts info
+      result[gem] = info
     end
+    puts result
   end
 
   def gem_list(file)
     YAML.load_file(file)['gems']
-  end
-
-  def client(token)
-    Octokit::Client.new(access_token: token)
-  end
-
-  def link_to_gem(gem)
-    Gems.info(gem)['source_code_uri'].gsub('https://github.com/', '')
-  end
-
-  def used_by
-    used_by_count = dependents(uri).css('.btn-link')[1].text.delete('^0-9').to_i
-  end
-
-  def contributors
-    contributors_count = contributors(uri).css('span.num.text-emphasized').children[2].text.to_i
-  end
-
-  def info(gem_link)
-    repositoy = client.repo gem_link
-
-    {
-      name: repositoy[:name],
-      used: used_by,
-      watchers: repositoy[:subscribers_count],
-      stargazers: repositoy[:stargazers_count],
-      forks_count: repositoy[:forks_count],
-      contributors: contributors,
-      issues: repositoy[:open_issues_count]
-    }
   end
 end
 
