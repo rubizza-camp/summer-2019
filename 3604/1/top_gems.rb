4ef09451e664f1ad9e559e830189af662216de35
MargaretOm <margosha20617@gmail.com>
2019-07-16 15:23:08 +0200

    Correct the method. Replacement each for each_with_object

--- 3604/1/top_gems.rb
+++ 3604/1/top_gems.rb
@@ -7,7 +7,7 @@ require './oprions_creator.rb'
 # class for main task: start project
 class TopGems
   def start_top_gems
-    hash_options = OptionsCreator.new.hash_options
+    p hash_options = OptionsCreator.new.hash_options
     array_name_gems = ArrayNamesGemsCreator.new.array_names_of_gems(hash_options)
     array_of_all_gems = create_array_with_all_information_about_gems(array_name_gems)
     array_of_all_gems = RatingCreator.new.array_of_rating_gems(array_of_all_gems)
@@ -18,11 +18,10 @@ class TopGems
 
   def create_array_with_all_information_about_gems(array_name_gems)
     array_of_all_gems = []
-    array_name_gems.each do |name_gem|
+    array_name_gems.each_with_object(array_of_all_gems) do |name_gem|
       hash_with_info_about_gem = GemInfoFetcher.new(name_gem).information_about_gem
       array_of_all_gems << hash_with_info_about_gem if hash_with_info_about_gem
     end
-    array_of_all_gems
   end
 end
 
