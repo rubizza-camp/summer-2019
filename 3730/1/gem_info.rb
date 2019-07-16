1c51eeafec7feef6637d0f651651f2062c081806
nikitathebest <nikita.m.7007@gmail.com>
2019-07-16 16:26:47 +0300

    Fix reek:TooManyInstanceVariables

--- 3730/1/gem_info.rb
+++ 3730/1/gem_info.rb
@@ -2,29 +2,24 @@ require 'rest-client'
 require 'nokogiri'
 require 'open-uri'
 require 'terminal-table'
+require 'byebug'
 
-# :reek:TooManyInstanceVariables
 class GemInfo
-  attr_reader :score
+  attr_reader :score, :gem_info
 
   def initialize(name, github_url)
-    @name = name
     @github_url = github_url
-    @used_by = used_by
-    @watched_by = watched_by
-    @stars = stars
-    @forks = forks
-    @contributors = contributors
-    @issues = issues
+    @gem_info = { name: name, used_by: used_by, watched_by: watched_by,
+                  stars: stars, forks: forks, contributors: contributors, issues: issues }
   end
 
   def calculate_score
-    @score = @used_by.to_i / 5 + @watched_by.to_i / 2 + @stars.to_i +
-             @forks.to_i + 10 * @contributors.to_i + @issues.to_i
+    @score = gem_info[:used_by].to_i / 5 + gem_info[:watched_by].to_i / 2 + gem_info[:stars].to_i +
+             gem_info[:forks].to_i + 10 * gem_info[:contributors].to_i + gem_info[:issues].to_i
   end
 
   def output
-    [@name, @used_by, @watched_by, @stars, @forks, @contributors, @issues]
+    gem_info.values
   end
 
   private
