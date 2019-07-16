3d9a846385523108b960be91c2cb632c166e5fcb
vasilro <mail@askolta.ru>
2019-07-16 19:28:34 +0300

    Add sorting, fix parser url with exception

--- 13/1/top_gems.rb
+++ 13/1/top_gems.rb
@@ -21,19 +21,19 @@ class TopRubyGems
 
 	def get_params_from_term
 		unless ARGV == nil
-			options = {}
+			@options = {}
 			optparse = OptionParser.new do |opts|
-  	  	opts.on("-v", "--name [STRING]", String, "Run verbosely") { |args| options[:raz] = args.strip.chomp }
-				opts.on('-t', '--top= [STRING]', String, "Run verbose") { |args| options[:dva] = args.strip.chomp }
-				opts.on('-f', '--file= [INTEGER[', Integer, "Run verbose") { |args| options[:tri] = args.strip.chomp }
+  	  	opts.on("-n", "--name [STRING]", String, "Run verbosely") { |args| @options[:input_name] = args.strip.chomp }
+				opts.on('-t', '--top= [STRING]', Integer, "Run verbose") { |args| @options[:input_qty] = args.strip.chomp }
+				opts.on('-f', '--file= [INTEGER]', Integer, "Run verbose") { |args| @options[:input_path_to_file] = args.strip.chomp }
 			end
   		optparse.parse!
-  		options
+  		@options
   	end
 	end
 
 	def get_data_from_yaml_to_hash
-		@list_of_gems_in_yml = YAML.load(File.read('data/gems.yml'))
+		@list_of_gems_in_yml = YAML.safe_load(File.read('data/gems.yml'))
 		@list_of_gems_in_yml['gems'].each do |_gem_|
 		_gem_.downcase!
 		@hash_of_gems[_gem_] = Gems.info _gem_
@@ -42,11 +42,18 @@ class TopRubyGems
 
 	def create_hash_with_urls_for_parcing
 		@hash_of_gems.each do |key, value|
-			@urls[key] = @hash_of_gems[key]['source_code_uri']
-			if @hash_of_gems[key]['source_code_uri'][4] == ':' 
-				@urls[key] = @hash_of_gems[key]['source_code_uri'].insert(4, 's')
-		end
-	end
+			if @hash_of_gems[key]['source_code_uri']
+				@urls[key] = @hash_of_gems[key]['source_code_uri']
+					if @hash_of_gems[key]['source_code_uri'][4] == ':' 
+					@urls[key] = @hash_of_gems[key]['source_code_uri'].insert(4, 's')
+					end
+			else
+				@urls[key] = @hash_of_gems[key]['homepage_uri']
+					if @hash_of_gems[key]['homepage_uri'][4] == ':' 
+					@urls[key] = @hash_of_gems[key]['homepage_uri'].insert(4, 's')
+					end
+				end
+			end
 	end
 
 	def preparing_url_and_parsing(url_for_parsing, key_css_tag)
@@ -85,6 +92,7 @@ class TopRubyGems
 			@rank_of_gem = @watches + @used_by + @stars + @forks + @contributors - @issues  
 
 			@total_data_about_gem[key] = {
+									name: key,
 									watches: @watches,
 									used_by: @used_by,
 									stars: @stars,
@@ -97,25 +105,33 @@ class TopRubyGems
 	end
 
 	def show_top_ruby_gems
+		@total_data_about_gem.sort_by { |_k, value| value[:stars]}.reverse.to_h
+	end
+
+	def show_table_with_all_gems
 		show_greeting
 		@rows = []
-		@total_data_about_gem.each do |key, value|
-		@rows << ["#{key}" ,
-			"used by #{@total_data_about_gem[key][:used_by]}",
-			 "watched by #{@total_data_about_gem[key][:watches]}",
-			 "#{@total_data_about_gem[key][:stars]}  stars",
-			 "#{@total_data_about_gem[key][:forks]}  forks",
-			 "#{@total_data_about_gem[key][:contributors]}  contributors",
-			 "#{@total_data_about_gem[key][:issues]}  issues"]
-		end
+		show_top_ruby_gems.each do |key, value|
+			@rows << ["#{@total_data_about_gem[key][:name]}" ,
+				"used by #{@total_data_about_gem[key][:used_by]}",
+			 	"watched by #{@total_data_about_gem[key][:watches]}",
+			 	"#{@total_data_about_gem[key][:stars]}  stars",
+			 	"#{@total_data_about_gem[key][:forks]}  forks",
+			 	"#{@total_data_about_gem[key][:contributors]}  contributors",
+			 	"#{@total_data_about_gem[key][:issues]}  issues"]
+			end
 		table = Terminal::Table.new :rows => @rows
 		puts table
 		3.times { puts '' }
 	end
 
 	def show_gems_in_table
+		if @options[:input_qty].nil?
+			show_table_with_all_gems
+		else
 
-		
+
+		end
 	end
 
 	def show_greeting
@@ -128,4 +144,6 @@ end
 start_programm = TopRubyGems.new
 start_programm.get_data_from_yaml_to_hash
 start_programm.get_and_parsing_data
-start_programm.show_top_ruby_gems
+start_programm.show_table_with_all_gems
+
+
