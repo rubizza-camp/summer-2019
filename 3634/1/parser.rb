602ecb82f6aa8c6c3c050bba810165a45fa97a46
Aleks <mikitsik@gmail.com>
2019-07-16 20:51:25 +0300

    Completed second part of the task (star case)

--- 3634/1/parser.rb
+++ 3634/1/parser.rb
@@ -9,24 +9,39 @@ class Parser
     @rows = []
   end
 
-  def scrap(links, names)
-    store_data(links, names)
+  def scrap(links, names, terminal_opts)
+    store_data(links, names, terminal_opts)
   end
 
   private
 
-  def store_data(links, names)
+  def store_data(links, names, terminal_opts)
     one_part = links.map { |link| run(link) }
     second_part = links.map { |link| run("#{link.gsub(%r{/$}, '')}/network/dependents") }
-    helper(one_part, second_part, names)
+    helper(one_part, second_part, names, terminal_opts)
   end
 
-  def helper(one_part, second_part, names)
+  def helper(one_part, second_part, names, terminal_opts)
     data = one_part.map.with_index { |hash, item| hash.merge(second_part[item]) }
     data.each_with_index do |value, item|
       @rows << value.values.unshift(item + 1, names[item])
       header_preparation(value) if @header.empty?
     end
+    if terminal_opts[:name]
+      @rows.select! { |item| item[1][/#{terminal_opts[:name]}/] }
+      @rows.map(&:shift)
+      @rows.map { |item| item.unshift(@rows.index(item) + 1) }
+    end
+    top_score(terminal_opts[:top]) if terminal_opts[:top]
+  end
+
+  def top_score(num)
+    prepared_rows = []
+    score = @rows.map { |number| number[2, 8].inject(0) { |sum, item| sum + item.to_i } }
+    num.to_i.times { |times| prepared_rows << @rows[score.index(score.sort[-1 - times])] }
+    prepared_rows.map(&:shift)
+    prepared_rows.map { |item| item.unshift(prepared_rows.index(item) + 1) }
+    @rows = prepared_rows
   end
 
   def header_preparation(value)
--- 3634/1/top_gems.rb
+++ 3634/1/top_gems.rb
@@ -3,14 +3,29 @@ require 'json'
 require 'open-uri'
 require 'net/http'
 require 'nokogiri'
+require 'optparse'
 require 'pry'
 require 'terminal-table'
 require_relative 'gems'
 require_relative 'parser'
 require_relative 'printer'
 
-gems = Gems.new('gems.yml')
+terminal_opts = {}
+
+OptionParser.new do |parser|
+  parser.on('-t', '--top=NUMBER') do |top|
+    terminal_opts[:top] = top
+  end
+  parser.on('-n', '--name=NAME') do |name|
+    terminal_opts[:name] = name
+  end
+  parser.on('-f', '--file=FILE') do |file|
+    terminal_opts[:file_name] = file
+  end
+end.parse!
+
+gems = Gems.new(terminal_opts[:file_name] ||= 'gems.yml')
 parser = Parser.new
-parser.scrap(gems.links, gems.names)
+parser.scrap(gems.links, gems.names, terminal_opts)
 printer = Printer.new
 puts printer.output(parser.header, parser.rows)
