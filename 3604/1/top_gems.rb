11a22aef878343b8e20ba5ccfbc2677ac6cd078d
MargaretOm <margosha20617@gmail.com>
2019-07-16 23:20:08 +0200

    Rewrite method, use .each_with_object([])

--- 3604/1/top_gems.rb
+++ 3604/1/top_gems.rb
@@ -7,7 +7,7 @@ require './options_creator.rb'
 # class for main task: start project
 class TopGems
   def start_top_gems
-    p hash_options = OptionsCreator.new.hash_options
+    hash_options = OptionsCreator.new.hash_options
     array_name_gems = ArrayNamesGemsCreator.new.array_names_of_gems(hash_options)
     array_of_all_gems = create_array_with_all_information_about_gems(array_name_gems)
     array_of_all_gems = RatingCreator.new.array_of_rating_gems(array_of_all_gems)
@@ -17,8 +17,7 @@ class TopGems
   private
 
   def create_array_with_all_information_about_gems(array_name_gems)
-    array_of_all_gems = []
-    array_name_gems.each_with_object(array_of_all_gems) do |name_gem|
+    array_name_gems.each_with_object([]) do |name_gem, array_of_all_gems|
       hash_with_info_about_gem = GemInfoFetcher.new(name_gem).information_about_gem
       array_of_all_gems << hash_with_info_about_gem if hash_with_info_about_gem
     end
