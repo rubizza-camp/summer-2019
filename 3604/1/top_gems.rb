7d86eb9a0a8f94554581aff1c43afff873c35d4d
MargaretOm <margosha20617@gmail.com>
2019-07-16 12:45:07 +0200

    Divide class into two classes
    Delete methods output table, because it's another function

--- 3604/1/top_gems.rb
+++ 3604/1/top_gems.rb
@@ -1,74 +1,28 @@
-require 'terminal-table'
-require './data_initializer.rb'
+require './array_names_gems_creator.rb'
 require './gem_info_fetcher.rb'
 require './create_rating.rb'
+require './table_output.rb'
+require './oprions_creator.rb'
 
-# class for main task: start project, get array of names and get arguments
-# return table with all information
+# class for main task: start project
 class TopGems
-  ARR_FOR_TABLE = ['used by', ' watched by', ' stars', ' forks', ' contributors', ' issues'].freeze
-
-  def initialize
-    @array_gems = []
-  end
-
   def start_top_gems
-    initialize_object.hash_arguments
-    create_array_with_all_information_about_gems
-    add_rating_in_array_with_all_information
-    table_output(@array_gems)
-  end
-
-  def create_array_with_all_information_about_gems
-    array_name_gems.size.times do |iter|
-      name_gem = array_name_gems[iter]
-      information = GemInfoFetcher.new(name_gem).information_about_gem
-      @array_gems << information if information != []
-    end
-  end
-
-  def add_rating_in_array_with_all_information
-    @array_gems = CreateRating.new(@array_gems).rating_of_gems
-  end
-
-  def table_output(array_gems)
-    table = Terminal::Table.new
-    table.style = { border_top: false, border_bottom: false }
-    how_many_names_need_to_display?.to_i.times do |iter|
-      table.add_row transformation_into_a_nice_view(array_gems[iter])
-    end
-    puts table
+    hash_options = OptionsCreator.new.hash_options
+    array_name_gems = ArrayNamesGemsCreator.new.array_names_of_gems(hash_options)
+    array_of_all_gems = create_array_with_all_information_about_gems(array_name_gems)
+    array_of_all_gems = CreateRating.new.array_of_rating_gems(array_of_all_gems)
+    TableOutput.new.table_output(array_of_all_gems, hash_options[:top])
   end
 
   private
 
-  def transformation_into_a_nice_view(array)
-    1.upto(6) do |iter|
-      array[iter] += ARR_FOR_TABLE[iter - 1]
+  def create_array_with_all_information_about_gems(array_name_gems)
+    array_of_all_gems = []
+    array_name_gems.each do |name_gem|
+      hash_with_info_about_gem = GemInfoFetcher.new(name_gem).information_about_gem
+      array_of_all_gems << hash_with_info_about_gem if hash_with_info_about_gem
     end
-    array
-  end
-
-  def how_many_names_need_to_display?
-    number_top_gems = hash_arguments[:top]
-    count_gems = @array_gems.size
-    if number_top_gems && number_top_gems.to_i < count_gems
-      number_top_gems
-    else
-      count_gems
-    end
-  end
-
-  def hash_arguments
-    @hash_arguments ||= initialize_object.hash_argv
-  end
-
-  def array_name_gems
-    @array_name_gems ||= initialize_object.array_names_of_gems
-  end
-
-  def initialize_object
-    @initialize_object ||= DataInitializer.new
+    array_of_all_gems
   end
 end
 
