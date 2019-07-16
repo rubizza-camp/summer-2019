c65b78c688c522cba11b26544efb7f9a7a532ab5
Alex Samets <aliaksandr.samets@gmail.com>
2019-07-16 18:02:31 +0300

    Fix FeatureEnvy bug into table_creator file

--- 3600/1/table_creator.rb
+++ 3600/1/table_creator.rb
@@ -3,7 +3,6 @@ class TableCreator
   def create(obj)
     table = Terminal::Table.new do |tab|
       tab.rows = obj
-      tab.style = { border_top: false, border_bottom: false }
     end
     puts table
   end
