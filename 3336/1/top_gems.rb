afad5e72c2d4ccf127d9c4e30c57d100ba90cd06
Likemurvin <romashi12@gmail.com>
2019-07-17 04:59:51 +0300

    Fix all logic, Fix style, Fi methods

--- 3336/1/top_gems.rb
+++ 3336/1/top_gems.rb
@@ -1,3 +1,5 @@
+#ruboop:disable Metrics/AbcSize, Metrics/LineLength, Security/Open, Lint/UselessAssiment
+
 require 'open-uri'
 require 'nokogiri'
 require 'json'
@@ -7,137 +9,136 @@ require 'gems'
 require 'terminal-table'
 require 'optparse'
 
-@options = {:top_gem => nil, :name_gem => nil, :file => nil}
+@options = { top_gem: nil, name_gem: nil, file: nil }
 
 OptionParser.new do |parser|
-  parser.on('--top=','--t=', Integer) do |top|
+  parser.on('--top=', '--t=', Integer) do |top|
     puts "I give you #{top} gems"
     @options[:top_gem] = top
-  end 
-  parser.on('--name=','--n=', String) do |name|
+  end
+  parser.on('--name=', '--n=', String) do |name|
     puts "Searching for #{name}"
     @options[:name_gem] = name
   end
-  parser.on('--file=','--f=') do |file|
+  parser.on('--file=', '--f=') do |file|
     puts "Take info from #{file}"
     @options[:file] = file
   end
 end.parse!
 
-def initialize
-  @gem_hash = []
-  @doc = []
-  @second_doc = []
-  @stats = []
-  @contributers = []
-  @issues = []
-  @used_by = []
-  @arr_of_stats = %i[watch star fork contributers issues used_by]
-  @hash_stat = {}
+def runner
+  g = gems
+  p "1.Execute gems: #{g}"
+  check_for_optparse(g)
+  urls = get_url(g)
+  p "2.Turn them into url's: #{urls}"
+  html_result_first = nokogiri_parse_first(urls)
+  html_result_second = nokogiri_parse_second(urls)
+  gems_stats = main_stats(html_result_first, html_result_second)
+  gems_stats = polish_main_stats(g, gems_stats)
+  p "3.Takin all stats: #{gems_stats}"
+  stat_rows = rating_rows(g, gems_stats)
+  p '4.Calculate rating'
+  p '5.Draw table:'
+  table(stat_rows)
 end
 
-def yaml_parse
-  initialize
-  data = YAML.safe_load(File.read('gems.yaml'))
-  unless @options[:file] == nil
-    data = YAML.safe_load(File.read(@options[:file]))
-  end
-  @arr_of_gems = Array.new(data['gems'].delete('-').split(' '))
-  p @arr_of_gems
-  unless @options[:name_gem] == nil
-    @arr_of_gems.each do |i|
-      @arr_of_gems.delete_if { |i| !(i.include?(@options[:name_gem])) }
+def gems
+  data = if @options[:file].nil?
+           YAML.safe_load(File.read('gems.yaml'))
+         else
+           YAML.safe_load(File.read(@options[:file]))
+         end
+
+  gems = Array.new(data['gems'].delete('-').split(' '))
+  gems
+end
+
+def check_for_optparse(gem_list)
+  unless @options[:name_gem].nil?
+    gem_list.each do
+      gem_list.delete_if { |x| !x.include?(@options[:name_gem]) }
     end
-    p "find #{@arr_of_gems.length} gems with name #{@options[:name_gem]}"
+    p "find #{gem_list.length} gems with name #{@options[:name_gem]}"
   end
+  p ' 1.1.Sorting by optional parameters'
+  @options[:top_gem] = gem_list.length if @options[:top_gem].nil?
 end
 
-def check_for_optparse
-  if @options[:top_gem] == nil
-    @options[:top_gem] = @arr_of_gems.length
-  end 
-end
-
-def takin_sources
-  yaml_parse
-  @arr_of_gems.each do |url|
+def get_url(gem_list)
+  arr_url = []
+  gem_list.each do |url|
     # p Gems.info(url)
-    unless (Gems.info(url)['source_code_uri']) == nil
-      @gem_hash << Gems.info(url)['source_code_uri']
-    else
-      @gem_hash << Gems.info(url)['homepage_uri']
-    end
+    (arr_url << Gems.info(url)['homepage_uri']) if (Gems.info(url)['source_code_uri']).nil?
   end
+  arr_url
+end
+
+def nokogiri_parse_first(url_list)
   # p @gem_hash
-  @gem_hash.each do |el|
-    @doc << Nokogiri::HTML(open(el))
-    @second_doc << Nokogiri::HTML(open(el + '/network/dependents'))
+  doc = []
+  url_list.each do |el|
+    doc << Nokogiri::HTML(open(el))
+    # @second_doc << Nokogiri::HTML(open(el + '/network/dependents'))
   end
-  # p @gem_hash
+  doc
 end
 
-def parsing_url
-  takin_sources
-  @doc.each do |el|
-    @stats << el.css('a.social-count').text.delete(' ').delete(',').split("\n").reject(&:empty?).map(&:to_i)
-    @contributers << el.css('span.num.text-emphasized')[3].text.delete(' ').delete(',').split("\n").reject(&:empty?).map(&:to_i)
-    @issues << [el.css('span.Counter')[0].text.to_i]
+def nokogiri_parse_second(url_list)
+  second_doc = []
+  url_list.each do |el|
+    second_doc << Nokogiri::HTML(open(el + '/network/dependents'))
   end
-
-  @second_doc.each do |el|
-      @used_by << [el.css('.btn-link')[1].text.delete(' ').delete("\n").delete(',').to_i]
-  end
-
-  @contributers.each_index do |i|
-    if @contributers[i].empty?
-      @contributers[i] = [0]
-    end
-  end
+  second_doc
 end
 
-# Set up an array with url's
-def arr_to_hash
-  parsing_url
-  @arr_n = []
-  @arr_n = @stats.concat(@contributers).concat(@issues).concat(@used_by)
-  l = @arr_of_gems.length
-  @arr_of_gems.each_index do |i|
-    @arr_n[i] = @arr_n[i].concat(@arr_n[i + l]).concat(@arr_n[i + 2 * l]).concat(@arr_n[i + 3 * l])
+def main_stats(main_html, sub_html)
+  watch_star_fork = []
+  contributers = []
+  issues = []
+  used_by = []
+  main_html.each do |el|
+    watch_star_fork << el.css('a.social-count').text.delete(' ').delete(',').split("\n").reject(&:empty?).map(&:to_i)
+    contributers << el.css('span.num.text-emphasized')[3].text.delete(' ').delete(',').split("\n").reject(&:empty?).map(&:to_i)
+    issues << [el.css('span.Counter')[0].text.to_i]
   end
-  @arr_n.delete_if { |index| index.length < 5 }
-  # p arr_n
-  @arr_of_gems.each_with_index do |val, i|
-    @hash_stat[val.to_sym] = Hash[@arr_of_stats.zip(@arr_n[i])]
+  contributers.each_index { |i| (contributers[i] = [0] if contributers[i].empty?) }
+  sub_html.each do |el|
+    used_by << [el.css('.btn-link')[1].text.delete(' ').delete("\n").delete(',').to_i]
   end
-  # p @arr_n
+  stats_arr = Array.new(watch_star_fork.concat(contributers).concat(issues).concat(used_by))
 end
 
-def rating
-    arr_to_hash
-    check_for_optparse
-    arr_rate = []
-    rows =[]
-
-    @arr_of_gems.each_index do |i|
-      arr_rate[i] = @arr_n[i].sum
-    end
-
-    @arr_of_gems.each_index do |i|
-      @arr_n[i].unshift(@arr_of_gems[i]).push(arr_rate[i])
-      rows << @arr_n[i]
-    end
-
-    # p rows
-    rows.each do |i|
-      rows.sort_by! do |item|
-        item.last
-      end
-    end
-    # p rows.reverse!
-    num_g = @options[:top_gem] - 1
-    table = Terminal::Table.new :rows => rows[0..num_g], :headings => @arr_of_stats.unshift('GEM').push('Rating')
-    puts table
-    # p @options
+def polish_main_stats(gems, stats)
+  l = gems.length
+  gems.each_index do |i|
+    stats[i] = stats[i].concat(stats[i + l]).concat(stats[i + 2 * l]).concat(stats[i + 3 * l])
+  end
+  # delete element of stats if element include less then 5(amount of stats) elements
+  stats.delete_if { |index| index.length < 5 }
+  stats
 end
 
-rating
+def rating_rows(gems, stats)
+  arr_rate = []
+  rows = []
+
+  gems.each_index do |index|
+    arr_rate[index] = stats[index].sum
+    stats[index].unshift(gems[index]).push(arr_rate[index])
+    rows << stats[index]
+  end
+
+  rows.sort_by! &:last
+  rows.reverse!
+end
+
+def table(rows)
+  stats_id = %i[Gem Watch Star Fork Contributers Issues Used_by Rating]
+  num_top = @options[:top_gem] - 1
+  table = Terminal::Table.new rows: rows[0..num_top], headings: stats_id
+  puts table
+end
+runner
+
+#ruboop:enable Metrics/AbcSize, Metrics/LineLength, Security/Open
