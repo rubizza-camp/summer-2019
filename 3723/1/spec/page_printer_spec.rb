9af2de3864dca4fa6eaef8cebe08edbfbaff8e79
Hawat <gmhawat@gmail.com>
2019-07-17 20:41:29 +0300

    Add spec for PagePrinter class

---
+++ 3723/1/spec/page_printer_spec.rb
@@ -0,0 +1,17 @@
+require './page_printer'
+require './sorted_pages'
+
+RSpec.describe PagePrinter do
+  subject { PagePrinter.new(row) }
+  let(:row) { [['rails', 'forks 111'], ['faraday', '13'], ['rspec', '1']] }
+
+  describe '#call' do 
+    it 'can sort by number from top' do
+      expect { subject.call(2) }.to change { subject.rows.count }.by(-1)
+    end
+    it 'can filter by part of name' do
+      subject.call('far')
+      expect(subject.rows.first.first).to eq('faraday')
+    end
+  end
+end
