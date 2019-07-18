b41b6044bfff5f2382906ecc5356e96225fc1c35
Elizaveta Shved <elishved108@gmail.com>
2019-07-18 16:14:27 +0300

    Modify file to draw final table

--- 3581/1/data_output.rb
+++ 3581/1/data_output.rb
@@ -1,64 +1,28 @@
-require 'nokogiri'
-require 'open-uri'
-require 'rubygems'
 require 'terminal-table'
-require 'optparse'
-require 'yaml'
+require_relative 'sort_gems.rb'
 
 class DataOutput
-  def initialize
-    @hash_gems = {}
-  end
-
-  def read_yml_file
-    top_with_param
-    list_gems = YAML.load_file(@add_args['file'])
-    list_gems['gems'].each do |name_of_gem|
-      @hash_gems[name_of_gem] = Parsing.new.site_search(name_of_gem)
+  class << self
+    def draw_table(hash_gems, add_args)
+      CommandLineParser.find_gem_by_name(hash_gems, add_args) if add_args['name']
+      sorte_gems = SortGems.sort_top(hash_gems, add_args)
+      puts fill_table(sorte_gems)
     end
-  end
 
-  def summ
-    @hash_gems.each do |_, gem_param|
-      sum = gem_param['used_by'] + gem_param['watch'] + gem_param['star'] + gem_param['fork'] + gem_param['contrib']
-      gem_param['sum'] = sum
-    end
-  end
+    private
 
-  def sort_top
-    summ
-    return @hash_gems = @hash_gems.sort_by { |_, val| -val['sum'] }[0..@add_args['top'] - 1].to_h if @add_args['top']
-    @hash_gems = @hash_gems.sort_by { |_, val| -val['sum'] }.to_h
-  end
-
-  def top_with_param
-    @add_args = {}
-    @add_args['file'] = 'gems.yml'
-    OptionParser.new do |arg|
-      arg.on('-t', '--top = t', Integer) { |t| @add_args['top'] = t }
-      arg.on('-n', '--name = n', String) { |n| @add_args['name'] = n }
-      arg.on('-f', '--file = f', String) { |f| @add_args['file'] = f }
-    end.parse!
-  end
-
-  def find_by_name
-    @hash_gems.select! { |name, _| name.include?(@add_args['name']) }
-  end
-
-  def fill_table
-    Terminal::Table.new do |t|
-      @hash_gems.each do |name_of_gem, gems_param|
-        t << [name_of_gem, 'used by ' "#{gems_param['used_by']}", 'watched by ' "#{gems_param['watch']}",
-              "#{gems_param['star']} " 'stars', "#{gems_param['fork']} " 'forks',
-              "#{gems_param['contrib']} " 'contributors', "#{gems_param['issues']} " 'issues']
+    def fill_table(hash_gems)
+      Terminal::Table.new do |top|
+        pass_params(top, hash_gems)
       end
     end
-  end
 
-  def draw_table
-    sort_top
-    find_by_name if @add_args['name']
-    top_with_param
-    puts fill_table
+    def pass_params(top, hash_gems)
+      hash_gems.each do |name_of_gem, gems_param|
+        top << [name_of_gem, 'used by ' "#{gems_param['used_by']}", 'watched by ' "#{gems_param['watch']}",
+                "#{gems_param['star']} " 'stars', "#{gems_param['fork']} " 'forks',
+                "#{gems_param['contrib']} " 'contributors', "#{gems_param['issues']} " 'issues']
+      end
+    end
   end
 end
