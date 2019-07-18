8dc4fdf831def6df4a84e7854bea56837a9deef6
Demeugaliyev Mirosh <39377563+Demeugaliyev@users.noreply.github.com>
2019-07-18 20:39:46 +0600

    3751-1 (#397)
    * Initial
    
    Refactoring in progress
    
    * re-write in OOP style
    
    * fix hound violations
    
    * rebuild relations of GemEntity and GetGemInfo
    
    GetGemInfo.new(self).call in @options for visibility and readibility
    
    * fix hound violations

---
+++ 3751/1/gem_entity.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Gem info
+class GemEntity
+  attr_reader :name, :options
+
+  def initialize(name)
+    @name = name
+    @options = GetGemInfo.new(self).call
+  end
+
+  def rating # rubocop:disable Metrics/AbcSize
+    [
+      options[:users] / 100,
+      options[:watchers] * 2,
+      options[:stars] * 2,
+      options[:forks] * 3,
+      options[:contributors] * 5,
+      options[:issues] * -4,
+      options[:closed_issues]
+    ].sum
+  end
+
+  def row
+    [
+      name,
+      "used by #{options[:users]}",
+      "watched by #{options[:watchers]}",
+      "#{options[:stars]} stars",
+      "#{options[:forks]} forks",
+      "#{options[:contributors]} contributors",
+      "#{options[:issues]} issues"
+    ]
+  end
+end
---
+++ 3751/1/top_gems.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require_relative 'listing_gems.rb'
+require_relative 'get_gem_info.rb'
+require_relative 'gem_entity'
+require 'terminal-table'
+require 'optparse'
+require 'gems'
+require 'open-uri'
+require 'nokogiri'
+require 'yaml'
+require 'pry'
+
+# output table
+# :reek:InstanceVariableAssumption
+class GemAnalyze
+  def call
+    gems = ListingGems.new(parameters).load
+
+    gems.sort_by! { |gem| -1 * gem.rating }
+
+    gems = gems.first(parameters[:top]) if parameters[:top]
+
+    print_table(gems)
+  end
+
+  def print_table(gems)
+    table = Terminal::Table.new rows: gems.map(&:row)
+    puts table
+  end
+
+  # :reek:TooManyStatements
+  def parameters
+    return @parameters if defined?(@parameters)
+
+    params = { file: 'gems.yml' }
+    OptionParser.new do |opts|
+      opts.on('--top=', Integer)
+      opts.on('--name=', String)
+      opts.on('--file=', String)
+    end.parse!(into: params)
+    @parameters = params
+  end
+end
+
+GemAnalyze.new.call
