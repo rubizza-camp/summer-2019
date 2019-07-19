15a9a823f2fbdbe5b33e6ba77d6551c5b28239ad
Anna Kukhareva <anna.kuhkhareva95@gmail.com>
2019-07-19 15:22:04 +0300

    correct sorting error

--- 3725/1/top_gems.rb
+++ 3725/1/top_gems.rb
@@ -6,23 +6,17 @@ require_relative 'lib/reader'
 require_relative 'lib/table'
 
 options = Reader::Shell.load_parameters
-
 gem_names = Reader::File.new(options[:file] || 'gemlist.yml').read
-
 client = Octokit::Client.new(access_token: ENV['KEY_TOKEN'])
-
 repos = gem_names.map do |gem_name|
   Repo::Searcher.call(gem_name: gem_name, client: client)
 end
-
 parsed_data = repos.map do |repo|
   Repo::Parser.new(repo: repo).parse
 end
-
 filter_repos = parsed_data.select { |hash| hash[:name] =~ /#{options[:name]}/ }
-
-sorted_repos = filter_repos.sort do |a|
-  a[:used_by]
+sorted_repos = filter_repos.sort do |a, b|
+  a[:used_by] <=> b[:used_by]
 end
 sorted_repos = sorted_repos.take(options[:top].to_i) if options[:top]
 table = Table::Printer.create(sorted_repos: sorted_repos)
