ad1c6d96cb35ae228099f0ef170bcf54f6b529f6
Likemurvin <romashi12@gmail.com>
2019-07-20 00:54:02 +0300

    Fix methods and namings

--- 3336/1/top_gems.rb
+++ 3336/1/top_gems.rb
@@ -1,5 +1,4 @@
-# rubocop:disable Metrics/AbcSize, Metrics/LineLength, Security/Open
-# rubocop:disable Lint/UselessAssignment, Lint/AmbiguousOperator
+# rubocop:disable Metrics/AbcSize, Security/Open, Lint/AmbiguousOperator
 
 require 'open-uri'
 require 'nokogiri'
@@ -11,112 +10,93 @@ require 'terminal-table'
 require 'optparse'
 
 # :reek:TooManyStatements
-# :reek:NestedIterators
 # :reek:UtilityFunction
-def options_get
-  options = { top_gem: nil, name_gem: nil, file: 'gems.yaml' }
-
+# :reek:NestedIterators
+def fetch_options
+  options = { file: 'gems.yaml' }
   OptionParser.new do |parser|
-    parser.on('--top=', '--t=', Integer) { |args| options[:top_gem] = args }
-    parser.on('--name=', '--n=', String) { |args| options[:name_gem] = args }
-    parser.on('--file=', '--f=') { |args| options[:file] = args }
+    parser.on('--top=', '-t', Integer) { |args| options[:top] = args }
+    parser.on('--name=', '-n', String) { |args| options[:name] = args }
+    parser.on('--file=', '-f') { |args| options[:file] = args }
   end.parse!
   options
 end
 
 # :reek:TooManyStatements
 def runner
-  options = options_get
-  gems = yaml_parse(options)
-  gems = options_check(gems, options)
-  gems = gems.map { |item| item.delete '-' }
-  urls = url_check(gems)
-  html_result = nokogiri_parse(urls)
-  stats = main_stats(html_result)
-  stats = polish_main_stats(gems, stats)
-  rows = rating_rows(gems, stats)
-  table(rows, options)
+  options = fetch_options
+  gems = parse_yaml(options[:file])
+  gems = filter_by_name(gems, options[:name])
+  rows = gems.map do |name|
+    url = pick_url(name)
+    github_pages = pages(url)
+    stats = stats(github_pages)
+    create_row(name, stats)
+  end
+  table(rows, filter_by_top(gems, options[:top]))
 end
 
 # :reek:UtilityFunction
-def yaml_parse(options)
-  gem_name = YAML.load_file(options[:file]).values[0].split(' ')
+def parse_yaml(options)
+  YAML.load_file(options)['gems']
 end
 
 # :reek:NilCheck
-# :reek:TooManyStatements
 # :reek:UtilityFunction
-def options_check(gem_list, options)
-  unless options[:name_gem].nil?
-    gem_list = gem_list.select { |item| item.include?(options[:name_gem]) }
-  end
-  options[:top_gem] = gem_list.length if options[:top_gem].nil?
-  gem_list
+def filter_by_name(gem_list, name)
+  return gem_list if name.nil?
+
+  gem_list.select { |item| item.include?(name) }
 end
 
-# :reek:UtilityFunction
-def url_check(gem_list)
-  gem_list.map { |item| Gems.info(item)['source_code_uri'] || Gems.info(item)['homepage_uri'] }
-  # gems = gems.map { |item| Gems.info(item)['homepage_uri'] if item.nil?}
-end
-
-def nokogiri_parse(url_list)
-  doc = url_list.map { |item| Nokogiri::HTML(open(item)) }
-  second_doc = url_list.map { |item| Nokogiri::HTML(open(item + '/network/dependents')) }
-  main_html = [doc, second_doc]
-end
-
-# :reek:TooManyStatements
-# :reek:UtilityFunction
-def main_stats(main_html)
-  stats_hash = { watch: nil, star: nil, fork: nil, contributers: nil, issues: nil, used_by: nil }
-  watch_star_fork = main_html[0].map { |item| item.css('a.social-count').text.delete(',').scan(/\d+/) }
-  stats_hash[:contributers] = (main_html[0].map { |item| item.css('span.num.text-emphasized')[3].text.delete(',').scan(/\d+/).map(&:to_i) }).flatten
-  stats_hash[:issues] = (main_html[0].map { |item| item.css('span.Counter')[0].text.to_i }).flatten
-  stats_hash[:used_by] = (main_html[1].map { |item| item.css('.btn-link')[1].text.delete(',').scan(/\d+/).map(&:to_i) }).flatten
-  stats_hash[:watch] = (watch_star_fork.map { |item| item.values_at(0).map(&:to_i) }).flatten
-  stats_hash[:star] = (watch_star_fork.map { |item| item.values_at(1).map(&:to_i) }).flatten
-  stats_hash[:fork] = (watch_star_fork.map { |item| item.values_at(2).map(&:to_i) }).flatten
-  stats_hash
-end
-
-# :reek:TooManyStatements
-# :reek:UncommunicativeVariableName
-# :reek:UtilityFunction
-# :reek:NestedIterators
-def polish_main_stats(gems, stats)
-  l = gems.length
-  hash = {}
-  gems.each_with_index do |item, index|
-    hash[item] = stats.values.map { |value| value = value[index] }
-  end
-  hash
-end
-
-# :reek:TooManyStatements
-# :reek:UtilityFunction
 # :reek:NilCheck
-# :reek:NestedIterators
-def rating_rows(gems, stats)
-  rows = []
-  gems.each_with_index do |item, index|
-    rows[index] = stats[item]
-  end
-  rows = rows.map { |arr| arr.map { |item| item || 0 } }
-  stats = stats.each_value { |value| value.each { |item| item = 0 if item.nil? } }
-  rows.map { |item| item.push(item.sum) }
-  rows = rows.map { |item| item.unshift(gems[rows.index(item)]) }
-  rows.sort_by! &:last
-  rows.reverse!
+# :reek:UtilityFunction
+def filter_by_top(gems, top)
+  return gems.length if top.nil?
+
+  top
 end
 
+# :reek:UtilityFunction
+def pick_url(gem)
+  info = Gems.info(gem)
+  info['source_code_uri'] || info['homepage_uri']
+end
+
+def pages(url)
+  main_page = Nokogiri::HTML(open(url))
+  dependents_page = Nokogiri::HTML(open(url + '/network/dependents'))
+  [main_page, dependents_page]
+end
+
+# :reek:TooManyStatements
+# :reek:UtilityFunction
+def stats(pages)
+  main, dependents = pages
+  watch_star_fork = main.css('a.social-count').text.delete(',').scan(/\d+/).map(&:to_i)
+  {
+    contributers: main.css('span.num.text-emphasized')[3].text.delete(',').scan(/\d+/)[0].to_i,
+    issues: main.css('span.Counter')[0].text.to_i,
+    used_by: dependents.css('.btn-link')[1].text.delete(',').scan(/\d+/)[0].to_i,
+    watch: watch_star_fork[0],
+    star: watch_star_fork[1],
+    fork: watch_star_fork[2]
+  }
+end
+
+# :reek:UtilityFunction
+def create_row(gem, stats)
+  row = stats.values.map { |item| item || 0 }
+  row.push(row.sum).unshift(gem)
+end
+
+# :reek:FeatureEnvy
 def table(rows, options)
   stats_id = %i[Gem Watch Star Fork Contributers Issues Used_by Rating]
-  num_top = options[:top_gem] - 1
-  table = Terminal::Table.new rows: rows[0..num_top], headings: stats_id
+  (rows.sort_by! &:last).reverse!
+  table = Terminal::Table.new rows: rows[0..(options - 1)], headings: stats_id
   puts table
 end
 runner
 
-# rubocop:enable Metrics/AbcSize, Metrics/LineLength, Security/Open
-# rubocop:enable Lint/UselessAssignment, Lint/AmbiguousOperator
+# rubocop:enable Metrics/AbcSize, Security/Open, Lint/AmbiguousOperator
