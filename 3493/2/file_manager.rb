7f87e461584724c89cb7b88be431bc5d196fe21b
Maxxx911 <maxrikal@gmail.com>
2019-07-19 15:17:09 +0300

    Add file functional

---
+++ 3493/2/file_manager.rb
@@ -0,0 +1,24 @@
+class FileManager
+  API_URL = 'https://api.telegram.org/'.freeze
+
+  def self.request_file_path(file_id, person_number, folder_name)
+    uri = URI("#{API_URL}bot#{ENV['TOKEN']}/getFile?file_id=#{file_id}")
+    json_response = JSON.parse(Net::HTTP.get(uri))
+    new.save_photo_from_uri(json_response['result']['file_path'], person_number, folder_name)
+  end
+
+  def self.save_location_in_file(location, person_number, folder_name)
+    location_path = "#{person_number}/#{folder_name}/#{Date.today}/#{SecureRandom.hex(4)}"
+    File.write(location_path, location, {mode: 'wb'})
+    return location_path
+  end
+
+  def save_photo_from_uri(path, person_number, folder_name)
+    uri = URI("#{API_URL}file/bot#{ENV['TOKEN']}/#{path}")
+    FileUtils.mkdir_p("#{person_number}/#{folder_name}/#{Date.today}")
+    photo_new_path = "#{person_number}/#{folder_name}/#{Date.today}/#{SecureRandom.hex(4)}.jpg"
+    File.write(photo_new_path, open(uri).read, {mode: 'wb'})
+    return photo_new_path
+  end
+
+end
