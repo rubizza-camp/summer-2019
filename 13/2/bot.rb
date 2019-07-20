41554dda8462ec1b8e904b18871619510a6bf06c
vasilro <mail@askolta.ru>
2019-07-20 23:15:49 +0300

    Migrate from gem, initial files

---
+++ 13/2/bot.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'telegram/bot'
+require 'redis'
+require 'httparty'
+require 'nokogiri'
+require 'date'
+
+# the code will appear in monday
+
+
---
+++ 13/2/modules/githubparser.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+# Tthe code will appear in monday
