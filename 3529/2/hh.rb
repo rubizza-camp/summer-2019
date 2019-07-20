c0d967fee5e73cb3fa02958590e1f9b68c40d3e4
Ganna Goodkevich <gannagoodkevich@gmail.com>
2019-07-20 13:30:14 -0700

    done image loading from telegram

--- 3529/2/hh.rb
+++ 3529/2/hh.rb
@@ -1,12 +1,10 @@
-require 'yaml'
-m = 0
-file = YAML.safe_load(File.read('Data/camp_participants.yaml'))
-file['participents'].each do |participant|
-  puts participant['telegram_id']
-  puts "---"
+require 'httparty'
+require 'open-uri'
+
+file_id = 'AgADAgADuqwxG-XUmEn_C8NMsB4Nyvfetw8ABDXpUpKaAAGHKkZPAQABAg'
+response = HTTParty.get('https://api.telegram.org/bot919190207:AAFfJhW2frNEWYeaSAvxhgwC6I233JVnVBg/getFile?file_id=AgADAgADuqwxG-XUmEn_C8NMsB4Nyvfetw8ABDXpUpKaAAGHKkZPAQABAg')
+puts response['result']['file_path']
+photo_url  = 'https://api.telegram.org/file/bot919190207:AAFfJhW2frNEWYeaSAvxhgwC6I233JVnVBg/photos/file_0.jpg'
+open('image.jpg', 'wb') do |file|
+  file << open(photo_url).read
 end
-File.open('Data/camp_participants.yaml','w') do |h| 
-   h.write file.to_yaml
-end
-puts '--//--'
-puts file['participents'].inspect
