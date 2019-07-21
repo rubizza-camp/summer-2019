a306e418bbabb07de34c3934668329bb176f88ed
Maxxx911 <maxrikal@gmail.com>
2019-07-21 18:07:23 +0300

    Split big command into two small methods (selfi, geoposition)

--- 3493/2/commands/checkin_command/checkin.rb
+++ 3493/2/commands/checkin_command/checkin.rb
@@ -1,35 +1,12 @@
-require_relative '../../models/user.rb'
-require_relative '../../file_manager.rb'
-require 'date'
-require 'time'
-
+require_relative '../../support_methods/user_methods/user_methods.rb'
 module CheckinCommand
   def checkin!(*)
-    if User[from['id']]
-      if payload['photo']
-        respond_with :message, text: "Получила фотку терь кинь гео"
-        FileManager.request_file_path(payload['photo'].last['file_id'], User[from['id']].person_number, 'checkins')
-        save_context :checkin!
-      elsif payload['location']
-        if is_camp?(payload['location']) 
-          respond_with :message, text: "Зачекинила"
-          FileManager.save_location_in_file(payload['location'], User[from['id']].person_number, 'checkins')
-          User[from['id']].update(checkin_datetime: Time.now, is_checkin: true)
-        else
-          respond_with :message, text: "Наебать решил?"
-        end
-      else
-        save_context :checkin!
-        respond_with :message, text: 'Отправь-ка селфи'
-      end
+    if UserMethods.registered?(from['id'])
+      respond_with :message, text: 'Фоточку в студию'
+      session['status'] = 'checkins'
+      save_context :selfi
     else
-      respond_with :message, text: "Сначала start"
+      respond_with :message, text: 'Сначала нужно зарегистрироваться командой /start'
     end
-
-  end 
-
-  def is_camp?(location)
-    (53.914264.. 53.916233).include?(location['latitude'].to_f) && (27.565941..27.571306).include?(location['longitude'].to_f)
-    true
   end
 end
--- 3493/2/commands/checkout_command/checkout.rb
+++ 3493/2/commands/checkout_command/checkout.rb
@@ -1,12 +1,12 @@
-require 'time'
 require_relative '../../support_methods/user_methods/user_methods.rb'
 module CheckoutCommand
   def checkout!(*)
     if UserMethods.registered?(from['id']) && UserMethods.checkin?(from['id'])
       respond_with :message, text: 'Фоточку в студию'
-      save_context :selfi!
+      session['status'] = 'checkouts'
+      save_context :selfi
     else
-      respond_with :message, text: 'Сначала нужно зарегистрироваться командой /start'
+      respond_with :message, text: 'А ты точно сегодня чекинился??'
     end
   end
 end
