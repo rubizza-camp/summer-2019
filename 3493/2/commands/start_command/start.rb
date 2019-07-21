747b3c8cd79e1dfa991a5afb73f28f233c124fe0
Maxxx911 <maxrikal@gmail.com>
2019-07-21 18:10:01 +0300

    Split start command

--- 3493/2/commands/start_command/start.rb
+++ 3493/2/commands/start_command/start.rb
@@ -1,26 +1,26 @@
 require_relative '../../models/user.rb'
+require_relative '../../support_methods/user_methods/user_methods.rb'
+require_relative '../../support_methods/number_methods/number_methods.rb'
 
 module StartCommand
-  def start!(person_number = nil, *)
-    redis = Redis.new(host: "127.0.0.1")
-    numbers = File.open('data/numbers'){|file| file.read}.split("\n")
-    if person_number.to_i != 0
-      User.all.each do |user|
-        if user.person_number == person_number
-          respond_with :message, text: "Наебать решил, этот номер принадлежит #{user.name}"
-          save_context :start!
-          return 0
-        end
-      end
-      if User[from['id']]
-        respond_with :message, text: "Ты уже зарегистрирован как #{User[from['id']].person_number}"
-      elsif numbers.include?(person_number)
-        User.create(id: from['id'], name: from['first_name'], person_number: person_number)
-        respond_with :message, text: 'Зарегистрирован'
-      end
+  def start!(*)
+    if UserMethods.registered?(from['id'])
+      respond_with :message, text: 'Ты уже зарегистрирован!'
     else
+      respond_with :message, text: 'Введи номер!'
+      save_context :enter_number
+    end
+  end
+
+  def enter_number(person_number = nil, *)
+    if UserMethods.find_users_by_person_number(person_number).empty? &&
+       NumberMethods.valid_number?(person_number) && person_number.to_i != 0
+      UserMethods.create_user(from['id'], from['firts_name'], person_number)
+      respond_with :message, text: 'Зарегистрирован!'
+    else
+      respond_with :message, text: 'Неверный номер. Такой номер либо используется,
+                                      либо уже занят.'
       save_context :start!
-      respond_with :message, text: 'Введи-ка свой номерок!!'
     end
   end
 end
