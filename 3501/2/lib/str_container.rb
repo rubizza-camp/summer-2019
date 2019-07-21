31e7655e52334853633cbb0db88c599163635995
IVAN BARYSIUK <Jdjjad89coq5ven5uqoweOOvqw3n@gmail.com>
2019-07-21 23:36:53 +0300

    Coplexy updating StrContainer class

--- 3501/2/lib/str_container.rb
+++ 3501/2/lib/str_container.rb
@@ -1,67 +1,28 @@
 # frozen_string_literal: true
 
+require 'json'
+
 class StrContainer
-  def self.send_yourself_pic
-    'Пришли мне себяшку!'
-  end
+  class << self
+    attr_reader :strings
+    def initialize_strings
+      @strings = JSON.parse(File.read('source/strings.json'))
+    end
 
-  def self.truly_here
-    'Точно на месте?'
-  end
+    def respond_to_missing?(method_name)
+      method_name.to_s.start_with?('user_') || super
+    end
 
-  def self.god_luck
-    'Удачи! '
-  end
+    def method_missing(method_name)
+      strings.fetch(method_name.to_s, 'Undefined string') || super
+    end
 
-  def self.check_out_help
-    'И незабудь /checkout когда закончишь...'
-  end
+    def welcome_old(user_name)
+      "Привет, #{user_name}. Рад видеть вас снова!"
+    end
 
-  def self.good_rest
-    'Удачного отдыха!'
-  end
-
-  def self.welcome_new
-    'Привет незнакомец, введи свой номер Rubizza club.'
-  end
-
-  def self.welcome_old(user_name)
-    "Привет, #{user_name}. Рад видеть вас снова!"
-  end
-
-  def self.registred_new(user_name)
-    "Добро пожаловать, #{user_name} в Rubizza survival camp!"
-  end
-
-  def self.welcome
-    'Привет, . Рад видеть вас снова!'
-  end
-
-  def self.busy_user_name
-    'Вы ошиблись, номер уже занят!'
-  end
-
-  def self.need_number
-    'Необходим ваш номер!'
-  end
-
-  def self.number_not_consist
-    'Номер не найден!'
-  end
-
-  def self.session_unclosed
-    'У вас есть незакрытая сессия! /checkout - для закрытия'
-  end
-
-  def self.session_allclosed
-    'Откройте сессию! /checkin - для открытия'
-  end
-
-  def self.more_data
-    'Неправильный ввод, попробуйте ещё раз!'
-  end
-
-  def self.new_start
-    'Привет, для начала введите /start!'
+    def registred_new(user_name)
+      "Добро пожаловать, #{user_name} в Rubizza survival camp!"
+    end
   end
 end
