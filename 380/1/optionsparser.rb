fa99481d0f8c48953e16fc92bd3c8ec55bc47651
mmmalikov <unitcircle@protonmail.comunitcircle@protonmail.comunitcircle@protonmail.com>
2019-07-22 18:13:30 +0300

    fix last review

--- 380/1/optionsparser.rb
+++ 380/1/optionsparser.rb
@@ -6,18 +6,30 @@ module OptionsParser
   # rubocop:disable Metrics/MethodLength
   def options
     options = {}
-    OptionParser.new do |parse|
-      parse.on('-t', '--top [top]', Integer, 'Enter count of gems in top:') do |top|
-        options[:count] = top if top
-      end
-      parse.on('-f', '--file [file]', String, 'Enter your yml file:') do |file|
-        options[:file] = file if file
-      end
-      parse.on('-n', '--name [STRING]', String, 'Enter gem you need:') do |name|
-        options[:name] = name if name
-      end
+    OptionParser.new do |parser|
+      parse_file(parser)
+      parse_count(parser)
+      parse_name(parser)
     end.parse!
     options
+  end
+
+  def parse_file(parser)
+    parse.on('-t', '--top [top]', Integer, 'Enter count of gems in top:') do |top|
+      options[:count] = top if top
+    end
+  end
+
+  def parse_count(parser)
+    parse.on('-f', '--file [file]', String, 'Enter your yml file:') do |file|
+      options[:file] = file if file
+    end
+  end
+
+  def parse_name(parser)
+    parse.on('-n', '--name [STRING]', String, 'Enter gem you need:') do |name|
+      options[:name] = name if name
+    end
   end
   # rubocop:enable Metrics/MethodLength
 end
