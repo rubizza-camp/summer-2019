0f5c2cb1ce2cee56e2c3f385a7696bd109a13b57
mmmalikov <unitcircle@protonmail.comunitcircle@protonmail.comunitcircle@protonmail.com>
2019-07-22 18:16:04 +0300

    last fix of optionparser

--- 380/1/optionsparser.rb
+++ 380/1/optionsparser.rb
@@ -3,7 +3,6 @@ require 'optparse'
 # include method for parsing options
 module OptionsParser
   # :reek:NestedIterators, :reek:TooManyStatements, :reek:UtilityFunction
-  # rubocop:disable Metrics/MethodLength
   def options
     options = {}
     OptionParser.new do |parser|
@@ -15,21 +14,20 @@ module OptionsParser
   end
 
   def parse_file(parser)
-    parse.on('-t', '--top [top]', Integer, 'Enter count of gems in top:') do |top|
+    parser.on('-t', '--top [top]', Integer, 'Enter count of gems in top:') do |top|
       options[:count] = top if top
     end
   end
 
   def parse_count(parser)
-    parse.on('-f', '--file [file]', String, 'Enter your yml file:') do |file|
+    parser.on('-f', '--file [file]', String, 'Enter your yml file:') do |file|
       options[:file] = file if file
     end
   end
 
   def parse_name(parser)
-    parse.on('-n', '--name [STRING]', String, 'Enter gem you need:') do |name|
+    parser.on('-n', '--name [STRING]', String, 'Enter gem you need:') do |name|
       options[:name] = name if name
     end
   end
-  # rubocop:enable Metrics/MethodLength
 end
