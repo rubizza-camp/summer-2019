04f2cf1b3642a7c06e730af2fe3c57afb0ce30de
AS-AlStar <starovojtov.alexander@gmail.com>
2019-07-23 17:50:55 +0300

    Create ENV variable

--- 3573/2/telegram_bot.rb
+++ 3573/2/telegram_bot.rb
@@ -1,25 +1,27 @@
 require 'telegram/bot'
 require 'logger'
+require 'dotenv'
 require_relative './commands/start'
 require_relative './commands/checkin'
 require_relative './commands/checkout'
 require_relative './commands/additional_methods'
+require_relative './commands/download_image'
+require_relative './commands/location'
 
 class WebhooksController < Telegram::Bot::UpdatesController
-
   include Telegram::Bot::UpdatesController::MessageContext
   include Start
   include Checkin
   include Checkout
   include AddMethods
+  include DownloadImage
+  include DownloadLocation
 
   Telegram::Bot::UpdatesController.session_store = :redis_store, { expires_in: 2_592_000 }
-
 end
 
-TOKEN = '860071708:AAGrfhtaGXROhZJGjP-Bml5tIf0UoKquBM0'
-
-bot = Telegram::Bot::Client.new(TOKEN)
+Dotenv.load
+bot = Telegram::Bot::Client.new(ENV['TOKEN'])
 
 # poller-mode
 logger = Logger.new(STDOUT)
