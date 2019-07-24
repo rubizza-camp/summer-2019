8ba0794b7bb0e287a144eb0103476aea8eebdb6e
MargaretOm <margosha20617@gmail.com>
2019-07-24 11:46:55 +0300

    Delete list of commands. Add handler exception
    Delete list of commands, because use Botfather for create command list
    Add handler exception for empty list of students numbers

--- 3604/2/modules/start_command.rb
+++ 3604/2/modules/start_command.rb
@@ -2,62 +2,49 @@
 
 require 'yaml'
 
-DATA_PATH = './data/camp_numbers.yml'
-IMAGE_DOG_PATH = './modules/dogs.jpg'
-NUMBERS_LIST_KEY = 'camp_numbers'
-LIST_OF_COMMANDS = [
-  '/start',
-  '/remove_account',
-  '/checkin',
-  '/checkout'
-].freeze
-
 module StartCommand
+  DATA_PATH = './data/camp_numbers.yml'
+  IMAGE_DOG_PATH = './modules/dogs.jpg'
+  NUMBERS_LIST_KEY = 'camp_numbers'
+
   def start!(*)
-    print_list_of_command
-    respond_with :message, text: user_name.to_s + I18n.t(:START_RESPONSE)
+    respond_with :message, text: user_name.to_s + I18n.t(:start_response)
     respond_with :photo, photo: File.open(IMAGE_DOG_PATH)
     save_context :message_register
   end
 
-  def message_register(first_word, *)
-    student_number = first_word.to_i
+  def message_register(number, *)
     return response_for_registered_student if user_registered?
-    return response_for_busy_student_number if student_number_busy?(student_number)
-    return response_for_register_student(student_number) if list_of_numbers.include?(student_number)
+    return response_for_busy_student_number if student_number_reserved?(number.to_i)
+    return response_for_register_student(number.to_i) if list_of_numbers.include?(number.to_i)
     respond_for_different_situation
+  rescue NoMethodError
+    respond_with :message, text: I18n.t(:empty_list_of_numbers_response)
   end
 
   private
 
-  def student_number_busy?(student_number)
+  def student_number_reserved?(student_number)
     redis.get(student_number)
   end
 
   def response_for_registered_student
-    respond_with :message, text: I18n.t(:REGISTERED_RESPONSE) + redis.get(user_id_telegram).to_s
+    respond_with :message, text: I18n.t(:registered_response) + redis.get(user_id_telegram).to_s
   end
 
   def response_for_busy_student_number
-    respond_with :message, text: I18n.t(:STUDENT_NUMBER_BUSY_RESPONSE)
+    respond_with :message, text: I18n.t(:student_number_reserved_response)
     start!
   end
 
   def response_for_register_student(student_number)
     register_student(student_number)
-    respond_with :message, text: I18n.t(:SUCCESSFUL_REGISTRATION_RESPONSE)
+    respond_with :message, text: I18n.t(:successful_registration_response)
   end
 
   def respond_for_different_situation
-    respond_with :message, text: I18n.t(:FAILED_REGISTRATION_RESPONSE)
+    respond_with :message, text: I18n.t(:failed_registration_response)
     start!
-  end
-
-  def print_list_of_command
-    respond_with :message, text: I18n.t(:LIST_OF_COMMANDS_RESPONSE)
-    LIST_OF_COMMANDS.each do |name_of_command|
-      respond_with :message, text: name_of_command
-    end
   end
 
   def list_of_numbers
