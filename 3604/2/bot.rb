43549d522af66fb929ccc4be2cd2f96f4df2f764
MargaretOm <margosha20617@gmail.com>
2019-07-24 11:42:39 +0300

    Divide bot.rb into two files

--- 3604/2/bot.rb
+++ 3604/2/bot.rb
@@ -1,24 +1,6 @@
 require 'telegram/bot'
 require 'logger'
-require 'dotenv'
-require 'i18n'
-Dir[File.join('./modules', '*.rb')].each { |file| require_relative file }
-
-I18n.load_path << Dir[File.expand_path('config/locales') + '/*.yml']
-I18n.default_locale = :en
-
-class WebhooksController < Telegram::Bot::UpdatesController
-  Telegram::Bot::UpdatesController.session_store = :redis_store, { expires_in: 2_592_000 }
-
-  include Telegram::Bot::UpdatesController::MessageContext
-  include StartCommand
-  include CheckinCommand
-  include CheckoutCommand
-  include DeleteCommand
-  include PhotoDownloader
-  include GeoDownloader
-  include RedisHelper
-end
+require_relative 'webhooks_controller.rb'
 
 Dotenv.load
 bot = Telegram::Bot::Client.new(ENV['TOKEN'])
---
+++ 3604/2/webhooks_controller.rb
@@ -0,0 +1,19 @@
+require 'dotenv'
+require 'i18n'
+Dir[File.join('./modules', '*.rb')].each { |file| require_relative file }
+
+I18n.load_path << Dir[File.expand_path('config/locales') + '/*.yml']
+I18n.default_locale = :en
+
+class WebhooksController < Telegram::Bot::UpdatesController
+  Telegram::Bot::UpdatesController.session_store = :redis_store, { expires_in: 2_592_000 }
+
+  include Telegram::Bot::UpdatesController::MessageContext
+  include StartCommand
+  include CheckinCommand
+  include CheckoutCommand
+  include DeleteCommand
+  include PhotoDownloader
+  include GeoDownloader
+  include RedisHelper
+end
