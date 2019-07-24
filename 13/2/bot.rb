8c8c5ae0d2d99e2257a738a614c411b694336d27
vasilro <mail@askolta.ru>
2019-07-24 21:05:49 +0300

    work in progress

--- 13/2/bot.rb
+++ 13/2/bot.rb
@@ -7,117 +7,126 @@ require 'httparty'
 require 'nokogiri'
 require 'time'
 require 'json'
+require 'down'
+require 'fileutils'
+require_relative 'modules/githubparser'
 
-TOKEN = '935196824:AAErWgXg1n1IfOVWQFfPnTqyie5aDmZdfsk'
+TOKEN = 'censored'
 
-class GithubParser
+#test mode Redis.new.flushall
+Redis.new.flushall
 
-  attr_reader :list_numbers
+def redis_set(chat_id, stage)
+  Redis.current.set(chat_id.to_i, stage.to_i)
+end
 
-  def parse
-    @list_numbers = []
-    numbers = Nokogiri::HTML(HTTParty.get('https://github.com/rubizza-camp/summer-2019'))
-    numbers = numbers.css('span.css-truncate.css-truncate-target a.js-navigation-open').each do |number| 
-      number = number.text.to_i
-      @list_numbers << number if number != 0
+def redis_get(chat_id)
+  Redis.current.get(chat_id).to_i  
+end
+
+def students(input)
+  students = GithubParser.new
+  list_numbers = students.parse
+  list_numbers.include?(input)
+end
+
+def current_stage(stage)
+  answer = stage
+  case answer
+  when 0 then return 'Hey, push /start'
+  when 1 then return 'You pushed start, lets enter your number'
+  when 2 then return 'This incorrect, enter correct number' 
+  when 3 then return 'Your number is ok, push /checkin'  
+  when 4 then return 'You checked in, need location'
+  when 5 then return 'Your location received, wait for your photo'
+  when 6 then return 'Your photo and location received, you may /checkout'
+  end
+end
+
+def put_message(bot, message, answer)
+  bot.api.send_message(chat_id: message.chat.id, text: "#{answer}")
+end
+
+def create_folder(dirname, path)
+  dirname = File.dirname(path)
+  unless File.directory?(dirname)
+  FileUtils.mkdir_p(dirname)
+  end
+end
+
+def write_in_file(name_file, content)
+  file = File.open("#{name_file}", "w")
+  file.write("#{content}")
+  file.close
+end
+
+def location(message)
+  latitude, longitude = message.location.latitude, message.location.longitude
+  write_in_file("#{timestamp}/geo.txt", "Location: latitude: #{latitude}, longitude: #{longitude}")
+end
+
+#def photo(message, url, destination)
+#  Down.download("#{url}", destination: "#{destination}", max_size: 1 * 300 * 300)
+#end
+
+def start_helper(message, bot)
+  chat_id = message.chat.id.to_i
+  stage = redis_get(chat_id)
+  redis_set(chat_id, 1) if stage <= 1
+  puts "stage start_helper: #{stage}"
+  answer = current_stage(redis_get(chat_id))
+  return put_message(bot, message, answer)
+end
+
+def checkin_helper(message, bot)
+  chat_id = message.chat.id.to_i
+  stage = redis_get(chat_id)
+  redis_set(chat_id, 4) if (3..4).include?(stage)
+  answer = current_stage(redis_get(chat_id))
+  puts "stage in checkin_helper: #{stage}"
+  return put_message(bot, message, answer)
+end
+
+def checkout_helper(message, bot)
+  chat_id = message.chat.id.to_i
+  stage = redis_get(chat_id)
+  if stage == 6
+    redis_set(chat_id, 0) 
+    answer = 'You logged out! Bye!'
+    answer
+  end
+   puts "stage in stage_helper: #{stage}"
+  return put_message(bot, message, answer)
+end
+
+def else_helper(message, bot)
+  chat_id = message.chat.id.to_i
+  stage = redis_get(chat_id)
+  if (1..2).include?(stage)
+    input = message.text.to_i
+    puts "Input text: #{input}"
+    if students(input)
+    redis_set(chat_id, 3)
+    else
+    redis_set(chat_id, 2)
     end
   end
-end
-
-@students = GithubParser.new
-@students.parse
-
-@check_list = []
-@stage = {stage: 0}
-@check_data = {
-                location_longitude: nil,
-                location_latitude: nil,
-                start_time: 0,
-                end_time: 0,
-                photo: nil
-}
-
-def check_number(message)
-  number = message.chomp.to_i
-  if @check_list.include?(number)
-    @stage[:stage] = 1
-    return 'You already registred! Enter command /checkin or /checkout'
-  elsif @students.list_numbers.include?(number)
-    @check_list << number 
-    @stage[:stage] = 1
-    return 'Ok, your ID registered! Next you need to /checkin'
-  else
-    return 'Unfortunally, this number is not in the list. Try another number.'
-  end
-end
-
-def check_status(status_to_compare)
-  @stage[:stage] == status_to_compare ? true : false
-end
-
-def save_photo(message)
-  id_photo = message.photo
-  @check_data[:photo] = id_photo
-end
-
-def save_location(message)
-  longitude = message.location.longitude
-  latitude = message.location.latitude
-  @check_data[:location_longitude] = longitude
-  @check_data[:location_latitude] = latitude
-end
-
-def working_hour(end_time)
-  @check_data[:start_time] - end_time
+  if stage == 4 then redis_set(chat_id, 5) end
+  if stage == 5 then redis_set(chat_id, 6) end
+  #if stage == 4 then location(message) end
+  #if stage == 5 then photo(message) end
+  answer = current_stage(redis_get(chat_id))
+   puts "stage in else_helper: #{stage}"
+  return put_message(bot, message, answer)
 end
 
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
     case message.text
-    when '/start', 'start' ,'s'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hey, input your number, #{message.from.first_name}")
-      bot.listen do |message|
-        bot.api.send_message(chat_id: message.chat.id, text: "Wait, i am searching!")
-        answer = check_number(message.text)
-        bot.api.send_message(chat_id: message.chat.id, text: answer)
-        break if check_status(1)
-      end
-    when '/checkin'
-      bot.listen do |message|
-        break if check_status(2)
-        bot.api.send_message(chat_id: message.chat.id, text: "Ok you checkin starts at #{message.date}, #{message.from.first_name}")
-        bot.api.send_message(chat_id: message.chat.id, text: "Send me location")
-        bot.listen do |message|
-          break if check_status(2)
-          save_location(message) if message.location
-          bot.api.send_message(chat_id: message.chat.id, text: "Location received.") if message.location
-          bot.api.send_message(chat_id: message.chat.id, text: "Then send me photo!")
-          bot.listen do |message| 
-            break if check_status(2)
-            save_photo(message)
-            bot.api.send_message(chat_id: message.chat.id, text: "Photo received") if message.photo
-            @stage[:stage] = 2 if (@check_data[:location] && @check_data[:photo])
-            bot.api.send_message(chat_id: message.chat.id, text: "Have a nice day! Dont forget to make /checkout")
-            @check_data[:start_time] = message.date.to_i
-            puts "#{@stage}"
-          end
-          break if check_status(2)
-        end
-        break if check_status(2)
-        puts "You on stage #{@stage}"
-        puts "#{}"
-      end
-      p @check_data[:start_time]
-    when '/checkout'
-      if @stage[:stage] = 2
-      difference = working_hour(message.date.to_i)
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, today you worked #{difference}")
-
-    end
-
-    else
-      bot.api.send_message(chat_id: message.chat.id, text: "Somthing going wrong, try to /start")  
+    when '/start' then start_helper(message, bot)
+    when '/checkin' then checkin_helper(message, bot)
+    when '/checkout' then checkout_helper(message, bot)
+    else else_helper(message, bot)
     end
   end
 end
-
