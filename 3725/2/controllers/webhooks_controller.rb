001d0f572c55d953ebae9f1b1d234ed249a53ab7
Anna Kukhareva <anna.kuhkhareva95@gmail.com>
2019-07-25 10:51:06 +0300

    remove start method from controller, remove extra line

--- 3725/2/controllers/webhooks_controller.rb
+++ 3725/2/controllers/webhooks_controller.rb
@@ -10,7 +10,6 @@ require_relative '../lib/user_methods'
 require_relative '../lib/validation/location'
 require_relative '../lib/validation/number'
 
-
 class WebhooksController < Telegram::Bot::UpdatesController
   include Telegram::Bot::UpdatesController::MessageContext
   include Telegram::Bot::UpdatesController::Session
@@ -25,24 +24,5 @@ class WebhooksController < Telegram::Bot::UpdatesController
 
   def message(*)
     reply_with :message, text: 'What you mean?'
-  end
-
-  def start!(*)
-    if UserStates.already_registered?(from['id'])
-      respond_with :message, text: 'You are already registered'
-    else
-      respond_with :message, text: 'Enter the number'
-      save_context :user_number
-    end
-  end
-
-  def user_number(person_number = nil, *)
-    if UserStates.find_user(person_number).empty? && NumberMethods.valid_number?(person_number)
-      UserStates.create_user(from['id'], from['firts_name'], person_number)
-      respond_with :message, text: 'Registration is successful'
-    else
-      respond_with :message, text: 'Wrong number'
-      save_context :start!
-    end
   end
 end
