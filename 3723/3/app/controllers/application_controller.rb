35d109ed042272b8e39e4aab0d6ab6bf4006017d
Hawat <gmhawat@gmail.com>
2019-07-25 15:47:00 +0300

    Add secure session key

--- 3723/3/app/controllers/application_controller.rb
+++ 3723/3/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "secure_password"
+    set :session_secret, ENV['SESSION_KEY']
   end
 
   get '/' do
