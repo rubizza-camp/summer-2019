f82202aeef7a3802da00720bc376caffaf6eafdf
Padfod <alfred122496@gmail.com>
2019-07-25 10:26:40 +0300

    Fixed few classes.

--- 3614/2/commands/start.rb
+++ 3614/2/commands/start.rb
@@ -7,5 +7,6 @@ module Start
     save_context :check_list_id
     respond_with :message, text: "Hello, #{from['first_name']}, @#{from['username']}."
     respond_with :message, text: 'U can check list of command using /help'
+    #lalalal
   end
 end
