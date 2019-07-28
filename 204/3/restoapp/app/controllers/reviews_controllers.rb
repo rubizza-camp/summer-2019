744d2e0d45a418907e9ba0f8825107b8d5b0db59
Artur <rutraniloms@icloud.com>
2019-07-28 23:37:29 +0300

    fix controllers

--- 204/3/restoapp/app/controllers/reviews_controllers.rb
+++ 204/3/restoapp/app/controllers/reviews_controllers.rb
@@ -1,19 +1,2 @@
 class ReviewsController < ApplicationController
-  def new; end
-
-  def create
-    @review = Review.new(review_params)
-  end
-
-  def show; end
-
-  def edit; end
-
-  def update; end
-
-  private
-
-  def review_params
-    params.require(:review).permit(:restraunt_id, :user_id, :body, :mark)
-  end
 end
