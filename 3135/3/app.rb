144b7cd77517b607bca44ab46e876e54b22df28e
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-07-28 16:32:25 +0300

    Create a default layout and main view

--- 3135/3/app.rb
+++ 3135/3/app.rb
@@ -1,15 +1,16 @@
 require 'sinatra'
 require 'sinatra/activerecord'
-require 'pry'
+#require 'pry'
 
 set :database, "sqlite3:db/database.sqlite3"
 
 get '/' do
-  'Hello world!'
+  @restaurants = Restaurant.all
+  erb :index, :layout => :layout
 end
 
 require './models/user'
 require './models/restaurant'
 require './models/review'
 
-binding.pry
+#binding.pry
