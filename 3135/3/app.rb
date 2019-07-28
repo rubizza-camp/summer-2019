6cdf547b75a97cc30e8e453e1cbaa6434e29ce67
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-07-28 17:53:17 +0300

    Restaurant in detail done

--- 3135/3/app.rb
+++ 3135/3/app.rb
@@ -4,9 +4,25 @@ require 'sinatra/activerecord'
 
 set :database, "sqlite3:db/database.sqlite3"
 
+# main page
 get '/' do
   @restaurants = Restaurant.all
   erb :index, :layout => :layout
+end
+
+# registration
+get '/register' do
+  erb :registration_form, :layout => :layout
+end
+
+post '/register' do
+  puts params.inspect
+end
+
+# restaurants_in_detail
+get '/:name' do
+  @restaurant = Restaurant.find_by(name: params[:name])
+  erb :restaurant_in_detail, :layout => :layout
 end
 
 require './models/user'
