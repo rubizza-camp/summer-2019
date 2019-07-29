b2daf15d89316896a772a7378eedc1b4668390a4
middaysan <midda7@mail.ru>
2019-07-29 12:21:32 +0300

    Add missing config files and fix minor code style issues
    delete idea files
    
    delete put out, added for check
    
    added .env template file, delete token.env, add .env to gitignore
    
    TOKEN variable added
    
    create env template
    
    add local gitignore
    
    idea
    
    modifie gitignore file
    
    add empty line to giignore file

--- 3741/2/checker_bot.rb
+++ 3741/2/checker_bot.rb
@@ -4,7 +4,10 @@ require 'time'
 require 'redis-activesupport'
 require 'active_support/all'
 require_relative 'webhook'
+require 'dotenv'
 
+Dotenv.load
+TOKEN = ENV['TOKEN']
 bot = Telegram::Bot::Client.new(TOKEN)
 webhook = WebhooksController
 logger = Logger.new(STDOUT)
