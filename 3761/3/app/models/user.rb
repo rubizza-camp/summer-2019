1908f5a2770481e9a0478d840fab13aa97a84d88
Julia <lugina.yulya@mail.ru>
2019-07-29 12:45:49 +0300

    Create models

---
+++ 3761/3/app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  has_many :reviews
+  validates_presence_of :name, :email, :password_hash
+  validates_uniqueness_of :name, :email
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    self.password_hash = Password.create(new_password)
+  end
+end
