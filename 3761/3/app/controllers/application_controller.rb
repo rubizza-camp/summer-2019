6928fb816c75643388056b5bd43e0778417d5328
Julia <lugina.yulya@mail.ru>
2019-07-29 16:42:06 +0300

    Move session_secret in .env

--- 3761/3/app/controllers/application_controller.rb
+++ 3761/3/app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 require 'rack-flash'
+require 'dotenv'
 
 class ApplicationController < Sinatra::Base
   register Sinatra::Session
   register Sinatra::ActiveRecordExtension
 
+  Dotenv.load
+  SESSION_SECRET = ENV['SESSION_SECRET']
+
   use Rack::Flash
   configure do
     set :session_fail, '/sign_up'
+    set :session_secret, SESSION_SECRET
     set views: proc { File.join(root, '../views/') }
   end
 
