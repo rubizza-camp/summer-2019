24a9e9a4457addebc8c0de10771d110860a764b9
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-07-30 21:04:23 +0300

    Change model validations

--- 3135/3/models/restaurant.rb
+++ 3135/3/models/restaurant.rb
@@ -2,6 +2,6 @@
 class Restaurant < ActiveRecord::Base
   has_many :reviews
 
-  validates_uniqueness_of :name, :description, :location
+  validates_uniqueness_of :name
   validates_presence_of :name, :description, :location
 end
