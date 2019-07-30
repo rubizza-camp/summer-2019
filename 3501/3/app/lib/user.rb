e4449c6261f9cb629c85b1fa213533ce1d37103d
IVAN BARYSIUK <Jdjjad89coq5ven5uqoweOOvqw3n@gmail.com>
2019-07-30 12:09:13 +0300

    Adding active record validation

--- 3501/3/app/lib/user.rb
+++ 3501/3/app/lib/user.rb
@@ -5,58 +5,32 @@ require 'date'
 require 'time'
 
 class User < ActiveRecord::Base
-  extend UserHelper
-
   has_many :feed_backs
   has_many :snack_bars
 
-  # :reek:FeatureEnvy
-  def create_new_snackbar(post) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-    snack_bars.create(
-      description: post.params[:description],
-      name: post.params[:name],
-      photo: post.params[:photo],
-      telephone: post.params[:telephone],
-      working_time_opening: post.params[:working_time_opening],
-      working_time_closing: post.params[:working_time_closing],
-      latitude: post.params[:latitude],
-      longitude: post.params[:longitude]
-    )
-  rescue NoMethodError
-    false
-  end
+  validates :mail, uniqueness: true
+  validates :password, confirmation: true, length: { in: 8..64 }, presence: true, on: :create
 
-  def create_new_comment(post)
-    self_create_new_comment(post)
+  extend UserHelper
+
+  def update_snackbar_commnets_count_and_rait(post)
     current_snack_bar = SnackBar.find_by_id(post.params[:id])
-    current_comments_count = current_snack_bar.comments_count
-    update_snackbar_commnets_count_and_rait(current_snack_bar, current_comments_count, post)
-  rescue NoMethodError
-    false
+    calculate_comments_count_and_rait(current_snack_bar, post)
   end
 
   private
 
-  # :reek:FeatureEnvy
-  def self_create_new_comment(post)
-    feed_backs.create(
-      snack_bar_id: post.params[:id],
-      content: post.params[:content],
-      raiting: post.params[:raiting],
-      date: Time.now
-    )
+  def calculate_comments_count_and_rait(current_snack_bar, post)
+    current_comments_count = current_snack_bar.comments_count
+    next_comments_count = current_comments_count ? current_comments_count + 1 : 1
+    calculate_snackbar_modular_raiting(current_snack_bar, next_comments_count, post)
   end
 
-  def update_snackbar_commnets_count_and_rait(current_snack_bar, current_comments_count, post)
-    current_comments_count = current_comments_count ? current_comments_count + 1 : 1
-    current_snack_bar.update_attribute(:comments_count, current_comments_count)
-    update_snackbar_modular_raiting(current_snack_bar, current_comments_count, post)
-  end
-
-  def update_snackbar_modular_raiting(current_snack_bar, current_comments_count, post)
+  def calculate_snackbar_modular_raiting(current_snack_bar, next_comments_count, post)
     olt_raiting = current_snack_bar.modular_raiting ? current_snack_bar.modular_raiting.to_f : 0.0
     new_raiting = (post.params[:raiting].to_f + olt_raiting *
-      (current_comments_count - 1)) / current_comments_count
+      (next_comments_count - 1)) / next_comments_count
     current_snack_bar.update_attribute(:modular_raiting, new_raiting)
+    current_snack_bar.update_attribute(:comments_count, next_comments_count)
   end
 end
