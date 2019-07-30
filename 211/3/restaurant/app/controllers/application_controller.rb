5872ba2f65fe0d5b176847e581191f6dd9a8ce45
Iris The Fox <iris-miz@yandex.ru>
2019-07-30 16:38:30 +0300

    Change BaseController

--- 211/3/restaurant/app/controllers/application_controller.rb
+++ 211/3/restaurant/app/controllers/application_controller.rb
@@ -1,19 +1,11 @@
 require './config/environment'
-require 'sinatra/activerecord'
-require 'sinatra/base'
-require 'sinatra/flash'
 
 require_relative 'base_controller.rb'
-require_relative 'sessions_controller.rb'
 require_relative 'restaurants_controller.rb'
 require_relative 'reviews_controller.rb'
+require_relative 'sessions_controller.rb'
 
-class ApplicationController < Sinatra::Base
-  set views: proc { File.join(root, '../views/') }
-  enable :sessions
-  use Rack::Static, urls: ['/stylesheets', '/javascripts', '/images'], root: 'public'
-  set :static, true
-
+class ApplicationController < BaseController
   use SessionsController
   use RestaurantsController
   use ReviewsController
--- 211/3/restaurant/app/controllers/base_controller.rb
+++ 211/3/restaurant/app/controllers/base_controller.rb
@@ -1,8 +1,13 @@
 require 'sinatra/activerecord'
+require 'sinatra/base'
+require 'sinatra/flash'
 require 'sinatra/form_helpers'
 
 class BaseController < Sinatra::Base
   set views: proc { File.join(root, '../views/') }
+  enable :sessions
+  set :static, true
+  use Rack::Static, urls: ['/images'], root: 'public'
 
   register Sinatra::ActiveRecordExtension
   register Sinatra::Flash
