820d99a94e2b1789de17f4bb043d76f5620c51cd
kos1kov <dima.kosikov00@mail.ru>
2019-07-30 21:28:41 +0300

    Move email validation into user model

--- 3587/3/app/models/user.rb
+++ 3587/3/app/models/user.rb
@@ -1,16 +1,19 @@
-require 'bcrypt'
 class User < ActiveRecord::Base
   include BCrypt
 
   validates :name, presence: true
   validates_uniqueness_of :email
+  validate :valid_email?
 
   def password
     @password ||= Password.new(password_hash)
   end
 
   def password=(new_password)
-    password = Password.create(new_password)
-    self.password_hash = password
+    self.password_hash = Password.create(new_password)
+  end
+
+  def valid_email?
+    errors.add(:email, message: 'Неправильная почта') unless Truemail.valid?(email) && email?
   end
 end
