86ca3f0e37d428877dd395be409dcc4781978eb3
Maxxx911 <maxrikal@gmail.com>
2019-07-30 18:48:19 +0300

    Use ** instead hash

--- 3493/3/helpers/review_helper.rb
+++ 3493/3/helpers/review_helper.rb
@@ -1,6 +1,5 @@
 class ReviewHelper
-  def self.create_review(hash)
-    Review.create(title: hash[:title], description: hash[description], place_id: hash[place_id],
-                  user_id: hash[user_id], rating: hash[rating])
+  def self.create_review(**hash)
+    Review.create(hash)
   end
 end
