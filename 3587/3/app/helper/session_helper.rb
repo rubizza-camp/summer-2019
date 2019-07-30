928af4d2a16d8688374d59b2eea2c77f7033c56b
kos1kov <dima.kosikov00@mail.ru>
2019-07-30 21:26:46 +0300

    Rewrite helper for only for session

--- 3587/3/app/helper/session_helper.rb
+++ 3587/3/app/helper/session_helper.rb
@@ -1,18 +1,7 @@
 module SessionHelper
-  def valid_password?
-    return true unless params[:password].empty?
-    flash[:error] = 'Введите пароль'
-  end
-
-  def valid_email?
-    return true if Truemail.valid?(params[:email])
-    flash[:error] = 'Неправильная почта'
-    redirect '/register'
-  end
-
-  def account_exist?
-    @user = User.find_by_email(params[:email])
-    return true if @user && @user.password == params[:password]
-    flash[:error] = 'Неверно введенные данные'
+  def start_session
+    session_start!
+    session['user_id'] = @user.id
+    redirect '/'
   end
 end
