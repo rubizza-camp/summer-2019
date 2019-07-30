ca625c4d3a6ea8dcebdb917961d710d65cd1070a
Julia <lugina.yulya@mail.ru>
2019-07-30 11:14:06 +0300

    Add validations on db columns

--- 3761/3/db/migrate/20190725084050_create_users_table.rb
+++ 3761/3/db/migrate/20190725084050_create_users_table.rb
@@ -2,9 +2,9 @@
 class CreateUsersTable < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |column|
-      column.string :name
-      column.string :email
-      column.string :password_hash
+      column.string :name, null: false
+      column.string :email, null: false
+      column.string :password_hash, null: false
       column.timestamps
     end
   end
--- 3761/3/db/schema.rb
+++ 3761/3/db/schema.rb
@@ -32,9 +32,9 @@ ActiveRecord::Schema.define(version: 2019_07_26_125759) do
   end
 
   create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.string 'password_hash'
+    t.string 'name', null: false
+    t.string 'email', null: false
+    t.string 'password_hash', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.index ['email'], name: 'index_users_on_email', unique: true
