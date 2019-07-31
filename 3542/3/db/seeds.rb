a69bb6c75937d83e9614f165e76007034fa9b60d
Andrey <andrey.korolko@mail.ru>
2019-07-31 13:39:24 +0300

    fix seeds; fix average mark

--- 3542/3/db/seeds.rb
+++ 3542/3/db/seeds.rb
@@ -1,1 +1,9 @@
+require 'active_record'
+Dir['./models/*.rb'].each { |file| require file }
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3',
+                                        database: 'db/development.sqlite3',
+                                        pool: 15,
+                                        timeout: 5000)
+
 Place.create(name: 'Rubizza', address: 'Masherova, 11', description: 'Extremal ruby courses')
