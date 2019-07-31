429320a8c20e649a4ff7eb550b10de137c3052f8
Андрей <andrey.korolko@mail.ru>
2019-07-31 21:48:31 +0300

    update seeds

--- 3542/3/db/seeds.rb
+++ 3542/3/db/seeds.rb
@@ -6,6 +6,10 @@ ActiveRecord::Base.establish_connection(adapter: 'sqlite3',
                                         pool: 15,
                                         timeout: 5000)
 
-Place.first_or_create(name: 'Rubizza',
-                      address: 'Masherova, 11',
-                      description: 'Extremal ruby courses')
+Place.find_or_create_by(name: 'Rubizza',
+                        address: 'Masherova, 11',
+                        description: 'Extremal ruby courses')
+
+Place.find_or_create_by(name: 'Test',
+                        address: 'Masherova, 11',
+                        description: 'Test description')
