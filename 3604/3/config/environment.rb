3afe28da30284a3c8775c31ff97171bb109ab08c
MargaretOm <margosha20617@gmail.com>
2019-07-31 17:07:57 +0300

    Add environment for database name

--- 3604/3/config/environment.rb
+++ 3604/3/config/environment.rb
@@ -2,9 +2,11 @@ require 'bundler'
 Bundler.require
 require 'rack-flash'
 
+environment = ENV.fetch('RACK_ENV', 'development')
+
 ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
-  database: 'foo.sqlite3'
+  database: "places_minsk_#{environment}.sqlite3"
 )
 
 require_all 'application'
