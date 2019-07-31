a4a7f8095da30cb5ee63f0e787bff2302b2f62e0
kos1kov <dima.kosikov00@mail.ru>
2019-08-01 00:06:47 +0300

    Rewrite account_exist method, add some validation method

--- 3587/3/app/helper/user_helper.rb
+++ 3587/3/app/helper/user_helper.rb
@@ -1,8 +1,35 @@
 module UserHelper
-  def account_exist?
-    @user = User.find_by_email(params[:email])
-    return true if @user && @user.password == params[:password]
-    flash[:error] = 'Неверно введенные данные'
-    redirect '/login'
+  def account_exists?
+    if find_in_db && right_password?
+      true
+    else
+      flash[:error] = I18n.t(:incorrect_password_or_email)
+      false
+    end
+  end
+
+  def find_in_db
+    @user = User.find_by(email: params[:email])
+  end
+
+  def error_message
+    @user.errors.details.dig(:email, 0, :error)
+  end
+
+  def right_password?
+    @user.password == params[:password]
+  end
+
+  def password?
+    if params[:password] == params[:confirm_password]
+      @user = User.create([{ name: params[:name] }, { email: params[:email] },
+                           { password: params[:password] }])
+    else
+      false
+    end
+  end
+
+  def stars
+    @stars = @shop.reviews.average(:grade).round(2)
   end
 end
