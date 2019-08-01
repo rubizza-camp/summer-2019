02f2bf9df85fbf911fe8b259b8026e1185438b21
kos1kov <dima.kosikov00@mail.ru>
2019-08-01 13:50:28 +0300

    Add rating contnatn and public folder to controller

--- 3587/3/app/controllers/application_controller.rb
+++ 3587/3/app/controllers/application_controller.rb
@@ -8,12 +8,14 @@ class ApplicationController < Sinatra::Base
   helpers UserHelper
   helpers SessionHelper
 
+  MAX_RATING = 5
   use Rack::Flash
   Dotenv.load
 
   configure do
     set :session_fail, '/register'
     set :session_secret, ENV['SESSION_SECRET']
+    set public_folder: proc { File.join(root, '../../public/') }
     set views: proc { File.join(root, '../views/') }
     enable :sessions
   end
