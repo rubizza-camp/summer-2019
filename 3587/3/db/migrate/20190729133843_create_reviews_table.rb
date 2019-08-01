4a91f65c6ad051014f7ed9550ade0f1277aa81dd
kos1kov <dima.kosikov00@mail.ru>
2019-08-01 13:46:36 +0300

    Use hash instead of array

--- 3587/3/db/migrate/20190729133843_create_reviews_table.rb
+++ 3587/3/db/migrate/20190729133843_create_reviews_table.rb
@@ -2,6 +2,8 @@
 # :reek:TooManyStatements
 class CreateReviewsTable < ActiveRecord::Migration[5.2]
   def change
+    add_foreign_key :reviews, :shops
+    add_foreign_key :reviews, :users
     create_table :reviews do |column|
       column.integer :grade
       column.text :text
