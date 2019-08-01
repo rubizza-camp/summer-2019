66f67cf00ae237750c8fd60b78454b1590563f8c
Andrey <andrey.korolko@mail.ru>
2019-08-01 14:49:10 +0300

    add loading data from csv file

--- 3542/3/db/seeds.rb
+++ 3542/3/db/seeds.rb
@@ -1,4 +1,5 @@
 require 'active_record'
+require 'csv'
 Dir['./models/*.rb'].each { |file| require file }
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3',
@@ -6,10 +7,12 @@ ActiveRecord::Base.establish_connection(adapter: 'sqlite3',
                                         pool: 15,
                                         timeout: 5000)
 
-Place.find_or_create_by(name: 'Rubizza',
-                        address: 'Masherova, 11',
-                        description: 'Extremal ruby courses')
+NAME = 0
+ADDRESS = 1
+DESCRIPTION = 2
 
-Place.find_or_create_by(name: 'Test',
-                        address: 'Masherova, 11',
-                        description: 'Test description')
+CSV.foreach(File.realpath("db/data/data.csv")) do |row|
+  Place.find_or_create_by(name: row[NAME],
+                          address: row[ADDRESS],
+                          description: row[DESCRIPTION])
+end
