c9719cecd1088ddc646a03fceaeacfdafacc1296
xxkvl <xxkvl@yandex.ru>
2019-08-02 11:57:36 +0300

    Refactor seeds.rb x2

--- 3648/3/db/seeds.rb
+++ 3648/3/db/seeds.rb
@@ -6,10 +6,7 @@ DESCRIPTION = 2
 RATING = 3
 
 CSV.foreach(File.realpath('db/data/places.csv')) do |row|
-  Place.find_or_create_by(
-    place_name: row[NAME],
-    location: row[LOCATION],
-    description: row[DESCRIPTION],
-    place_rating: row[RATING]
-  )
+  Place.find_or_create_by(place_name: row[NAME]).update(location: row[LOCATION],
+                                                        description: row[DESCRIPTION],
+                                                        place_rating: row[RATING])
 end
