f1f76cce8df72aa7e51577afc98dc020cf3a386c
Elizaveta Shved <elishved108@gmail.com>
2019-08-05 20:44:38 +0300

    Move styles to a separate file. Remove cdn link

--- 3581/3/app/controllers/application_controller.rb
+++ 3581/3/app/controllers/application_controller.rb
@@ -2,17 +2,18 @@ require 'rack-flash'
 require 'dotenv'
 
 class ApplicationController < Sinatra::Base
-  register Sinatra::Session
-  register Sinatra::ActiveRecordExtension
 
   Dotenv.load
   SESSION_SECRET = ENV['SESSION_SECRET']
+  register Sinatra::Session
+  register Sinatra::ActiveRecordExtension
 
   use Rack::Flash
   configure do
     set :session_fail, '/sign_up'
     set :session_secret, SESSION_SECRET
     set views: proc { File.join(root, '../views/') }
+    set :public_folder, 'app/assets'
   end
 
   I18n.load_path << Dir[File.expand_path('config/locales') + '/*.yml']
