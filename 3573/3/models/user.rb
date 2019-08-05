f98b786338e9b083eb0a9efb5f73f9200db2e16c
AS-AlStar <starovojtov.alexander@gmail.com>
2019-08-05 20:49:18 +0300

    Fix regexp for email

--- 3573/3/models/user.rb
+++ 3573/3/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
 
   validates :name, :email, :password_hash, presence: true
   validates_uniqueness_of :email
-  validates_format_of :email, with: /.+@.+\..+/
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
   def password
     Password.new(password_hash)
