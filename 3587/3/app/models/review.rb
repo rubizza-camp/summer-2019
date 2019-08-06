87302208a470ead3615438acde3837eceda39f22
kos1kov <dima.kosikov00@mail.ru>
2019-08-06 20:24:52 +0300

    Add max rating constant

--- 3587/3/app/models/review.rb
+++ 3587/3/app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :shop
   validates_presence_of :text, :grade
-  validates_inclusion_of :grade, in: 1..5
+  validates_inclusion_of :grade, in: 1..ApplicationController::MAX_RATING
 end
