591d9aea6b785c86f8dc2641b8276ecfae2eb89a
kos1kov <dima.kosikov00@mail.ru>
2019-08-06 20:26:23 +0300

    Remove bad valid method

--- 3587/3/app/models/user.rb
+++ 3587/3/app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   include BCrypt
 
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates_uniqueness_of :email
-  validate :valid_email?
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: I18n.t(:invalid_email) }
 
   def password
     @password ||= Password.new(password_hash)
@@ -12,9 +12,5 @@ class User < ActiveRecord::Base
 
   def password=(new_password)
     self.password_hash = Password.create(new_password)
-  end
-
-  def valid_email?
-    Truemail.valid?(email) && email?
   end
 end
