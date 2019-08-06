921283ea820546c5eb5f2dac9218832e2a3a1f92
kos1kov <dima.kosikov00@mail.ru>
2019-08-06 20:23:59 +0300

    Redirect user to main page after sign_up

--- 3587/3/app/controllers/sessions_controller.rb
+++ 3587/3/app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
   get '/register' do
     return redirect '/' if session?
+
     erb :register
   end
 
   post '/register' do
-    if password? && @user.valid? && @user.save
-      redirect '/login'
+    if params[:password] == params[:confirm_password] && @user.save
+      @user = User.create(name: params[:name], email: params[:email], password: params[:password])
+      login
     else
       flash[:error] = I18n.t(:incorrect_password_or_email)
     end
@@ -15,6 +17,7 @@ class SessionsController < ApplicationController
 
   get '/login' do
     return redirect '/' if session?
+
     erb :login
   end
 
