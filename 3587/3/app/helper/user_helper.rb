8553ce058ecf924a47976e16187463af1eb8bc55
kos1kov <dima.kosikov00@mail.ru>
2019-08-06 20:29:05 +0300

    Simplify class logic by remove bad method

--- 3587/3/app/helper/user_helper.rb
+++ 3587/3/app/helper/user_helper.rb
@@ -1,20 +1,11 @@
 module UserHelper
-  def account_exists?
-    if find_in_db && right_password?
-      true
-    else
-      flash[:error] = I18n.t(:incorrect_password_or_email)
-      false
-    end
-  end
-
   def logout
     session[:user_id] = nil
   end
 
   def login
     logout if login?
-    session['user_id'] = @user.id if account_exists?
+    session['user_id'] = @user.id if find_in_db && right_password?
     redirect '/'
   end
 
@@ -22,20 +13,8 @@ module UserHelper
     @user = User.find_by(email: params[:email])
   end
 
-  def error_message
-    @user.errors.details.dig(:email, 0, :error)
-  end
-
   def right_password?
     @user.password == params[:password]
-  end
-
-  def password?
-    if params[:password] == params[:confirm_password]
-      @user = User.create(name: params[:name], email: params[:email], password: params[:password])
-    else
-      false
-    end
   end
 
   def stars
@@ -44,5 +23,9 @@ module UserHelper
 
   def login?
     session[:user_id]
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id])
   end
 end
