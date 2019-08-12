434d2b77a2a022d8f1ab22cc48815a155123d388
Elizaveta Shved <elishved108@gmail.com>
2019-08-12 15:42:52 +0300

    Fix hound errors

--- 3581/3/app/controllers/reviews_controller.rb
+++ 3581/3/app/controllers/reviews_controller.rb
@@ -6,13 +6,11 @@ class ReviewController < ApplicationController
   post '/reviews/' do
     @review = Review.new(text: params['text'], grade: params['grade'].to_i,
                          place_id: params[:id], user_id: session[:user_id])
-    if @review.save
-      redirect "/places/#{params[:id]}"
-    else
+    unless @review.save
       @reviews = @place.reviews.order(id: :desc)
       @error = true
       flash[:error] = I18n.t(:blank_review)
-      redirect "/places/#{params[:id]}"
     end
+      redirect "/places/#{params[:id]}"
   end
 end
